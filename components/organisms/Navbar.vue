7d96ac447aa17a204f2bd2c5dcb193ca21484a96
Florent Delerue <flo@nuxtjs.com>
2022-02-02 17:12:23 +0100

    feat(ProfileDropdown): profile dropdown on navbar (#6)

--- components/organisms/Navbar.vue
+++ components/organisms/Navbar.vue
@@ -4,15 +4,15 @@
       <div class="flex items-center justify-between h-16">
         <div class="flex items-center">
           <NuxtLink :to="`/${$route.params.team ? $route.params.team : 'dashboard'}`" class="block">
-            <Logo class="h-8 w-auto text-black dark:text-white" />
+            <Logo class="w-auto h-8 text-black dark:text-white" />
           </NuxtLink>
 
-          <!-- <SvgIcon name="custom/separator" class="flex-shrink-0 w-8 h-8 text-tw-gray-300 ml-2" /> -->
+          <!-- <SvgIcon name="custom/separator" class="flex-shrink-0 w-8 h-8 ml-2 text-tw-gray-300" /> -->
 
           <!-- <TeamsDropdown /> -->
         </div>
 
-        <!-- <ProfileDropdown /> -->
+        <ProfileDropdown />
       </div>
     </UContainer>
   </nav>
