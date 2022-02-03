e7a28816a47f2ce21163eb43ac0118a99ccb7cdd
Florent <flo@nuxtjs.com>
2022-02-03 14:13:39 +0100

    refacto: add types

--- pages/teams/new.vue
+++ pages/teams/new.vue
@@ -15,12 +15,12 @@
 
     <Page>
       <div class="flex flex-col-reverse lg:flex-row gap-10 lg:mt-[-72.5px]">
-        <div class="w-full lg:w-2/3 space-y-6">
+        <div class="w-full space-y-6 lg:w-2/3">
           <UCard variant="black" ring-class="ring-1 u-ring-gray-200" border-color-class="u-border-gray-200" @submit.prevent="onSubmit">
             <div class="space-y-6">
               <UFormGroup name="slug" label="Slug" :help="form.slug !== slug ? `Your team slug will be renamed to “${slug}”` : 'This is your team\'s URL namespace on Nuxt.'" required class="relative">
                 <div class="flex items-center">
-                  <span class="u-bg-gray-50 border border-r-0 u-border-gray-300 rounded-l-md px-2 inline-flex items-center u-textgray-500 text-sm py-2">
+                  <span class="inline-flex items-center px-2 py-2 text-sm border border-r-0 u-bg-gray-50 u-border-gray-300 rounded-l-md u-textgray-500">
                     nuxt.com/
                   </span>
 
@@ -60,8 +60,8 @@
 
             <template #footer>
               <div class="flex items-center justify-between">
-                <p class="u-text-gray-500 text-sm">
-                  <!-- Continuing will initiate a 14 day trial of the <NuxtLink to="/pricing" class="text-primary-500 hover:underline font-medium">
+                <p class="text-sm u-text-gray-500">
+                  <!-- Continuing will initiate a 14 day trial of the <NuxtLink to="/pricing" class="font-medium text-primary-500 hover:underline">
                     Pro plan
                   </NuxtLink>. -->
                 </p>
@@ -83,6 +83,8 @@
 <script setup lang="ts">
 import { omit } from 'lodash-es'
 import slugify from '@sindresorhus/slugify'
+import type { Ref } from 'vue'
+import type { Team, User } from '~/types'
 
 definePageMeta({
   middleware: 'auth'
@@ -90,7 +92,7 @@ definePageMeta({
 
 const router = useRouter()
 const client = useStrapiClient()
-const user = useStrapiUser()
+const user = useStrapiUser() as Ref<User>
 
 const file = ref(null)
 const form = reactive({ name: '', slug: '', avatar: null })
@@ -108,7 +110,7 @@ const onSubmit = async () => {
     const team = await client('/teams', {
       method: 'POST',
       body: formData
-    })
+    }) as Team
 
     user.value.teams.push(team)
 
