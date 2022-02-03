0681bddebef67ae6447030b344db12eb988d55c6
Sylvain Marroufin <marroufin.sylvain@gmail.com>
2022-02-03 11:05:14 +0100

    feat(teams): `new` page `slug` input auto-format

--- pages/teams/new.vue
+++ pages/teams/new.vue
@@ -3,12 +3,12 @@
     <div class="bg-white dark:bg-black">
       <UContainer padded>
         <div class="py-16 sm:py-24">
-          <h2 class="text-4xl font-extrabold u-text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl">
-            Create a new team
-          </h2>
-          <p class="mt-5 text-xl u-text-gray-400">
-            A Nuxt team will allow you to collaborate with others on your projects and enjoy additional optional features.
-          </p>
+          <h2
+            class="text-4xl font-extrabold u-text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl"
+          >Create a new team</h2>
+          <p
+            class="mt-5 text-xl u-text-gray-400"
+          >A Nuxt team will allow you to collaborate with others on your projects and enjoy additional optional features.</p>
         </div>
       </UContainer>
     </div>
@@ -16,13 +16,24 @@
     <Page>
       <div class="flex flex-col-reverse lg:flex-row gap-10 lg:mt-[-72.5px]">
         <div class="w-full lg:w-2/3 space-y-6">
-          <UCard variant="black" ring-class="ring-1 u-ring-gray-200" border-color-class="u-border-gray-200" @submit.prevent="onSubmit">
+          <UCard
+            variant="black"
+            ring-class="ring-1 u-ring-gray-200"
+            border-color-class="u-border-gray-200"
+            @submit.prevent="onSubmit"
+          >
             <div class="space-y-6">
-              <UFormGroup name="slug" label="Slug" :help="form.slug !== slug ? `Your team slug will be renamed to “${slug}”` : 'This is your team\'s URL namespace on Nuxt.'" required class="relative">
+              <UFormGroup
+                name="slug"
+                label="Slug"
+                :help="form.slug !== slug ? `Your team slug will be renamed to “${slug}”` : 'This is your team\'s URL namespace on Nuxt.'"
+                required
+                class="relative"
+              >
                 <div class="flex items-center">
-                  <span class="u-bg-gray-50 border border-r-0 u-border-gray-300 rounded-l-md px-2 inline-flex items-center u-textgray-500 text-sm py-2">
-                    nuxt.com/
-                  </span>
+                  <span
+                    class="u-bg-gray-50 border border-r-0 u-border-gray-300 rounded-l-md px-2 inline-flex items-center u-textgray-500 text-sm py-2"
+                  >nuxt.com/</span>
 
                   <UInput
                     v-model="form.slug"
@@ -36,7 +47,12 @@
                 </div>
               </UFormGroup>
 
-              <UFormGroup name="name" label="Name" help="This is your team's visible name within Nuxt. For example, the name of your company or department." required>
+              <UFormGroup
+                name="name"
+                label="Name"
+                help="This is your team's visible name within Nuxt. For example, the name of your company or department."
+                required
+              >
                 <UInput
                   v-model="form.name"
                   name="name"
@@ -47,11 +63,15 @@
                 />
               </UFormGroup>
 
-              <UFormGroup name="avatar" label="Avatar" help="An avatar is optional but strongly recommended.">
+              <UFormGroup
+                name="avatar"
+                label="Avatar"
+                help="An avatar is optional but strongly recommended."
+              >
                 <div class="flex items-center gap-3">
                   <UAvatar :src="avatar" gradient />
 
-                  <input ref="file" name="avatar" type="file" class="hidden" @change="onFileUpload">
+                  <input ref="file" name="avatar" type="file" class="hidden" @change="onFileUpload" />
 
                   <UButton label="Change" size="sm" variant="white" @click="$refs.file.click()" />
                 </div>
@@ -63,14 +83,10 @@
                 <p class="u-text-gray-500 text-sm">
                   <!-- Continuing will initiate a 14 day trial of the <NuxtLink to="/pricing" class="text-primary-500 hover:underline font-medium">
                     Pro plan
-                  </NuxtLink>. -->
+                  </NuxtLink>.-->
                 </p>
 
-                <UButton
-                  type="submit"
-                  :loading="loading"
-                  label="Create"
-                />
+                <UButton type="submit" :loading="loading" label="Create" />
               </div>
             </template>
           </UCard>
@@ -83,6 +99,7 @@
 <script setup lang="ts">
 import { omit } from 'lodash-es'
 import slugify from '@sindresorhus/slugify'
+import { nextTick } from 'vue'
 
 definePageMeta({
   middleware: 'auth'
@@ -96,6 +113,16 @@ const file = ref(null)
 const form = reactive({ name: '', slug: '', avatar: null })
 const avatar = ref(null)
 const loading = ref(false)
+
+watch(() => form.slug, () => {
+  const newSlug = slugify(form.slug)
+  if (newSlug !== form.slug) {
+    // nextTick to avoid no changes when pasting twice the same value
+    nextTick(() => {
+      form.slug = newSlug
+    })
+  }
+})
 
 const onSubmit = async () => {
   loading.value = true
@@ -113,7 +140,7 @@ const onSubmit = async () => {
     user.value.teams.push(team)
 
     router.push(`/${team.slug}`)
-  } catch (e) {}
+  } catch (e) { }
 
   loading.value = false
 }
