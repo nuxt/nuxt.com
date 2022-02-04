3d3a2fd365b19001fd16392e5f40f15b67b8cbbf
Benjamin Canac <canacb1@gmail.com>
2022-02-04 14:41:26 +0100

    chore: update

--- pages/index.vue
+++ pages/index.vue
@@ -4,47 +4,75 @@ definePageMeta({
 })
 
 useMeta({
+  htmlAttrs: {
+    class: 'bg-black'
+  },
   bodyAttrs: {
-    class: ['bg-black']
+    class: 'bg-black antialiased font-sans'
   }
 })
 </script>
 
 <template>
-  <div class="container mx-auto">
-    <LogoFull class="text-white" />
+  <UContainer class="h-screen flex flex-col justify-center">
+    <Logo class="logo" />
+    <h1 class="title">
+      A better way to create<br>modern websites.
+    </h1>
+    <p class="description">
+      Our suite of developer-friendly solutions helps you create,<br>secure and deliver enterprise-grade modern website.
+    </p>
     <div class="links">
-      <a href="https://nuxtjs.org">Framework</a>
       <a href="https://nuxtlabs.com">Company</a>
+      <a href="https://nuxtjs.org">Framework</a>
     </div>
     <!-- Work in progress... 👀 -->
-  </div>
+  </UContainer>
 </template>
 
 <style scoped>
 .container {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
-  width: 100%;
+  padding: 48px 16px;
+}
+.logo {
+  height: 64px;
+  color: #00DC82;
+  margin-bottom: 20px;
+}
+.title {
+  color: #fff;
+  font-weight: 500;
+  font-size: 92px;
+  line-height: 1.1;
+  margin: 20px 0;
+}
+.description {
+  color: #aaa;
+  font-weight: 300;
+  font-size: 28px;
+  line-height: 1.5;
+  letter-spacing: 0.75px;
+  padding-bottom: 20px;
 }
 .links {
   display: flex;
-  margin-top: 5px;
-  gap: 10px;
+  gap: 20px;
 }
 .links a {
-  font-size: 15px;
-  color: #888;
+  font-size: 18px;
+  font-weight: 400;
+  letter-spacing: 1px;
+  color: #ccc;
   text-decoration: none;
   transition: all 0.2s;
-  padding-bottom: 2px;
-  border-bottom: 1px transparent solid;
+  padding: 16px 32px;
+  border: 2px #aaa solid;
+  background-color: transparent;
+  border-radius: 8px;
 }
 .links a:hover {
-  border-bottom: 1px white solid;
-  color: white;
+  border-color: #fff;
+  color: #000;
+  background-color: #fff;
 }
 </style>
