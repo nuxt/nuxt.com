def2500e53a3628f6f2c873afa4a1ed9a83d7142
Benjamin Canac <canacb1@gmail.com>
2022-02-04 14:44:54 +0100

    fix(nuxt): add missing metas

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,14 @@ export default defineNuxtConfig({
     title: 'Nuxt: Intuitive Web Development',
     meta: [
       { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1' },
-      { name: 'description', content: 'Nuxt makes web development intuitive and fun. The open source framework NuxtJS makes the development of modern web applications simple.' }
+      { name: 'description', content: 'Nuxt makes web development intuitive and fun. The open source framework NuxtJS makes the development of modern web applications simple.' },
+      { name: 'og:site_name', content: 'Nuxt' },
+      { name: 'og:type', content: 'website' },
+      { name: 'og:image', content: 'https://nuxt.com/social.png' },
+      { name: 'og:image:alt', content: 'Nuxt' },
+      { name: 'twitter:image', content: 'https://nuxt.com/social-twitter.png' },
+      { name: 'twitter:card', content: 'summary_large_image' },
+      { name: 'twitter:site', content: '@nuxt_js' }
     ],
     link: [
       { rel: 'icon', href: '/icon.png' }
