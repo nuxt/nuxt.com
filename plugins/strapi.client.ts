88e03e72fb82cfa2f756410e4cfa7c5b47b66078
Sylvain Marroufin <marroufin.sylvain@gmail.com>
2022-02-07 11:46:19 +0100

    fix(strapi): secure toast display on `strapi:error` hook (#30)

--- plugins/strapi.client.ts
+++ plugins/strapi.client.ts
@@ -3,6 +3,6 @@ import { defineNuxtPlugin } from '#imports'
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.hooks.hook('strapi:error' as any, (e: Strapi4Error) => {
-    nuxtApp.$toast.error({ title: e.error.name, description: e.error.message })
+    nuxtApp.$toast.error({ title: e.error?.name, description: e.error?.message })
   })
 })
