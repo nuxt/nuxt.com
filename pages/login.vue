8c095bfa83abab00ce960c80b1c6f7e0e5bf88ac
Sylvain Marroufin <marroufin.sylvain@gmail.com>
2022-02-08 15:18:58 +0100

    feat(auth): email login

--- pages/login.vue
+++ pages/login.vue
@@ -25,14 +25,81 @@
           <span class="font-medium text-sm">{{ user.username }}</span>
         </div>
       </div>
-      <UButton
-        v-else
-        block
-        label="Connect with GitHub"
-        size="xl"
-        icon="fa-brands:github"
-        @click="onClick"
-      />
+      <div v-else class="space-y-8">
+        <div>
+          <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
+            Sign in to your account
+          </h2>
+          <p class="mt-2 text-center text-sm text-gray-600">
+            Or
+            {{ ' ' }}
+            <NuxtLink to="/register" class="font-medium text-gray-800 hover:underline">
+              create a new account
+            </NuxtLink>
+          </p>
+        </div>
+
+        <UCard body-class="px-4 py-5 sm:px-6 space-y-6" padded @submit.prevent="onSubmit">
+          <div class="-space-y-px">
+            <div>
+              <label for="email" class="sr-only">Email address</label>
+              <UInput
+                v-model="form.identifier"
+                name="email"
+                type="email"
+                required
+                autofocus
+                placeholder="Email address"
+                custom-class="relative focus:z-10 !rounded-b-none"
+                size="lg"
+              />
+            </div>
+            <div>
+              <label for="password" class="sr-only">Password</label>
+              <UInput
+                v-model="form.password"
+                name="password"
+                type="password"
+                required
+                placeholder="Password"
+                custom-class="relative focus:z-10 !rounded-t-none"
+                size="lg"
+              />
+            </div>
+          </div>
+          <div class="flex items-center justify-end">
+            <div class="text-sm">
+              <NuxtLink :to="forgotLink" class="font-medium text-gray-800 hover:underline">
+                Forgot your password?
+              </NuxtLink>
+            </div>
+          </div>
+          <UButton
+            :loading="loading"
+            type="submit"
+            icon="heroicons-outline:arrow-right"
+            trailing
+            size="lg"
+            label="Sign in"
+            block
+          />
+          <div class="relative">
+            <div class="absolute inset-0 flex items-center">
+              <div class="w-full border-t border-gray-300" />
+            </div>
+            <div class="relative flex justify-center text-sm">
+              <span class="px-2 bg-white text-gray-500"> Or continue with </span>
+            </div>
+          </div>
+          <UButton
+            block
+            label="GitHub"
+            size="lg"
+            icon="fa-brands:github"
+            @click="onClick"
+          />
+        </UCard>
+      </div>
     </div>
   </div>
 </template>
@@ -42,21 +109,52 @@ import type { Ref } from 'vue'
 import type { User } from '~/types'
 
 const { getProviderAuthenticationUrl } = useStrapiAuth()
+const { login } = useStrapiAuth()
+const user = useStrapiUser() as Ref<User>
+const router = useRouter()
+const route = useRoute()
+const { $toast } = useNuxtApp()
 
 definePageMeta({
   layout: false
 })
 
-const user = useStrapiUser() as Ref<User>
-const router = useRouter()
+const loading = ref(false)
+const form = reactive({ identifier: '', password: '' })
+const forgotLink = computed(() => `/forgot${form.identifier ? `?email=${form.identifier}` : ''}`)
 
 const onClick = () => {
   window.location = getProviderAuthenticationUrl('github') as unknown as Location
 }
 
+const onSubmit = async () => {
+  loading.value = true
+
+  try {
+    await login(form)
+
+    const redirect = useCookie('redirect').value
+    if (redirect) {
+      router.push(redirect)
+      useCookie('redirect').value = null
+    } else {
+      router.push('/dashboard')
+    }
+  } catch (e) {}
+
+  loading.value = false
+}
+
 onMounted(() => {
   if (user.value && user.value.beta) {
     router.push('/dashboard')
+  }
+
+  if (route.query.confirmed) {
+    $toast.success({
+      title: 'Email confirmed!',
+      description: 'You can now login to your account.'
+    })
   }
 })
 </script>
