3b313c98983f0fa89cb4b9d5a47453d9c5bc2a8e
Benjamin Canac <canacb1@gmail.com>
2022-02-09 15:39:42 +0100

    chore(index): improve design

--- pages/index.vue
+++ pages/index.vue
@@ -5,74 +5,35 @@ definePageMeta({
 
 useMeta({
   htmlAttrs: {
-    class: 'bg-black'
+    class: 'bg-black dark'
   },
   bodyAttrs: {
-    class: 'bg-black antialiased font-sans'
+    class: 'antialiased font-sans text-gray-700 dark:text-gray-200'
   }
 })
 </script>
 
 <template>
-  <UContainer class="h-screen flex flex-col justify-center">
-    <Logo class="logo" />
-    <h1 class="title">
-      A better way to create<br>modern websites.
-    </h1>
-    <p class="description">
-      Our suite of developer-friendly solutions helps you create,<br>secure and deliver enterprise-grade modern website.
-    </p>
-    <div class="links">
-      <a href="https://nuxtlabs.com">Company</a>
-      <a href="https://nuxtjs.org">Framework</a>
+  <div class="h-screen flex flex-col justify-center">
+    <div class="px-4 sm:px-6 lg:px-8 w-6xl mx-auto">
+      <Logo class="w-20 h-20 logo mb-6" />
+      <h1 class="text-4xl sm:text-6xl font-extrabold u-text-gray-900 mb-6">
+        A better way to create<br>modern websites.
+      </h1>
+      <p class="text-lg sm:text-2xl u-text-gray-500">
+        Our suite of developer-friendly solutions helps you create,<br>secure and deliver enterprise-grade modern website.
+      </p>
+      <div class="flex items-center gap-6 mt-12">
+        <UButton to="https://nuxtlabs.com" label="Company" size="xl" />
+        <UButton to="https://nuxtjs.org" label="Framework" size="xl" />
+      </div>
+      <!-- Work in progress... 👀 -->
     </div>
-    <!-- Work in progress... 👀 -->
-  </UContainer>
+  </div>
 </template>
 
 <style scoped>
-.container {
-  padding: 48px 16px;
-}
 .logo {
-  height: 64px;
   color: #00DC82;
-  margin-bottom: 20px;
-}
-.title {
-  color: #fff;
-  font-weight: 500;
-  font-size: 92px;
-  line-height: 1.1;
-  margin: 20px 0;
-}
-.description {
-  color: #aaa;
-  font-weight: 300;
-  font-size: 28px;
-  line-height: 1.5;
-  letter-spacing: 0.75px;
-  padding-bottom: 20px;
-}
-.links {
-  display: flex;
-  gap: 20px;
-}
-.links a {
-  font-size: 18px;
-  font-weight: 400;
-  letter-spacing: 1px;
-  color: #ccc;
-  text-decoration: none;
-  transition: all 0.2s;
-  padding: 16px 32px;
-  border: 2px #aaa solid;
-  background-color: transparent;
-  border-radius: 8px;
-}
-.links a:hover {
-  border-color: #fff;
-  color: #000;
-  background-color: #fff;
 }
 </style>
