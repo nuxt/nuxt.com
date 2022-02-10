942cf64cb1ffc174b9a61ee1c25b535fde4538c3
Benjamin Canac <canacb1@gmail.com>
2022-02-10 17:54:11 +0100

    chore(Navbar): clean code

--- components/organisms/Navbar.vue
+++ components/organisms/Navbar.vue
@@ -3,10 +3,6 @@
     <UContainer padded>
       <div class="flex items-center justify-between h-16">
         <div class="flex items-center gap-6">
-          <!-- <NuxtLink :to="`/${$route.params.team ? $route.params.team : 'dashboard'}`" class="block">
-            <Logo class="h-8 p-0.5 w-auto u-text-black" />
-          </NuxtLink> -->
-
           <TeamsDropdown />
         </div>
 
@@ -15,10 +11,3 @@
     </UContainer>
   </nav>
 </template>
-
-<script setup lang="ts">
-import type { Ref } from 'vue'
-import type { User } from '~/types'
-
-const user = useStrapiUser() as Ref<User>
-</script>
