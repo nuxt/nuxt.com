32f82ff7aaedfd9cef193e5dce377876854391cf
Benjamin Canac <canacb1@gmail.com>
2022-02-10 12:02:21 +0100

    fix(pages): dark mode support for public pages

--- components/organisms/Navbar.vue
+++ components/organisms/Navbar.vue
@@ -4,7 +4,7 @@
       <div class="flex items-center justify-between h-16">
         <div class="flex items-center gap-6">
           <!-- <NuxtLink :to="`/${$route.params.team ? $route.params.team : 'dashboard'}`" class="block">
-            <Logo class="h-8 p-0.5 w-auto text-black dark:text-white" />
+            <Logo class="h-8 p-0.5 w-auto u-text-black" />
           </NuxtLink> -->
 
           <TeamsDropdown />
