4f34e9e9a432d7c04689f39151e11d6652e23a10
Florent <florentdelerue@hotmail.com>
2022-02-15 16:42:01 +0100

    lint: class sort

--- components/templates/projects/ProjectsList.vue
+++ components/templates/projects/ProjectsList.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full p-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+  <div class="grid w-full grid-cols-1 gap-4 p-8 md:grid-cols-2 lg:grid-cols-3">
     <ProjectsItem v-for="project of projects" :key="project.id" :project="project" />
   </div>
 </template>
