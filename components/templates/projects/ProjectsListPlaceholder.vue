f34406a97a6a1f5f3bd4937811e8021cacc7b1c9
Benjamin Canac <canacb1@gmail.com>
2022-02-17 15:21:04 +0100

    up

--- components/templates/projects/ProjectsListPlaceholder.vue
+++ components/templates/projects/ProjectsListPlaceholder.vue
@@ -1,32 +1,31 @@
 <template>
-  <div class="flex flex-col space-y-4">
-    <h2 class="text-2xl font-semibold leading-6 u-text-gray-900">
+  <UCard body-class="p-12 text-center">
+    <h2 class="text-2xl font-semibold u-text-gray-900">
       No projects yet!
     </h2>
-    <p class="u-text-gray-500">
-      Create a project from a Template, or import a Git repository.
+    <p class="mt-3 u-text-gray-500">
+      Create a project from a template, or import a git repository.
     </p>
-    <div class="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+    <div class="-mx-12 sm:mx-0 grid sm:gap-6 sm:grid-cols-2 lg:grid-cols-4 my-6">
       <TemplateCard
-        v-for="(template, index) of templates.slice(0, 4)"
+        v-for="(template, index) of templates"
         :key="index"
         class="my-4"
         :template="template"
         :to="{ name: 'team-new-clone', query: { template: template.slug } }"
       />
     </div>
-    <div>
-      <UButton to="/projects/new/" label="Create a new project" size="lg" />
-    </div>
-    <ULink to="/new/templates" class="text-sm u-text-gray-900 hover:underline">
-      Browse Templates &rarr;
+
+    <UButton to="/projects/new" label="Create a new project" size="lg" />
+    <ULink to="/new/templates" class="text-sm u-text-gray-900 hover:underline block mt-3">
+      Browse templates &rarr;
     </ULink>
-  </div>
+  </UCard>
 </template>
 
 <script setup lang="ts">
 import type { Template } from '~/types'
 const { find } = useStrapi4()
 
-const { data: templates } = await useAsyncData('templates', () => find<Template[]>('templates', { populate: ['screenshot'] }))
+const { data: templates } = await useAsyncData('templates', () => find<Template[]>('templates', { populate: 'screenshot', pagination: { start: 0, limit: 4 } }))
 </script>
