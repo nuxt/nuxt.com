f3b180bc1e9175458897239cc6e9b0e00a793101
Florent <florentdelerue@hotmail.com>
2022-02-21 17:32:29 +0100

    feat: theme selector

---
+++ components/atoms/ThemeSelect.vue
@@ -0,0 +1,45 @@
+<template>
+  <USelect
+    v-model="value"
+    name="theme"
+    :options="options"
+  />
+</template>
+<script setup>
+const colorMode = useColorMode()
+const options = [
+  {
+    text: 'System',
+    value: 'system',
+    icon: 'heroicons-outline:desktop-computer'
+  },
+  {
+    text: 'Dark',
+    value: 'dark',
+    icon: 'heroicons-outline:moon'
+  },
+  {
+    text: 'Light',
+    value: 'light',
+    icon: 'heroicons-outline:sun'
+  }
+]
+
+const value = computed({
+  get () {
+    return colorMode.preference || 'system'
+  },
+  set (value) {
+    switch (value) {
+      case 'dark':
+        colorMode.preference = 'dark'
+        break
+      case 'light':
+        colorMode.preference = 'light'
+        break
+      default:
+        colorMode.preference = 'system'
+    }
+  }
+})
+</script>
