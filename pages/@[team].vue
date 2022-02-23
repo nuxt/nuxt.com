f66b824994ea3fbb92364b9a634e6530d52c1c0a
Benjamin Canac <canacb1@gmail.com>
2022-02-23 18:22:21 +0100

    chore(team): improve data fetching and errors

--- pages/@[team].vue
+++ pages/@[team].vue
@@ -14,11 +14,15 @@ definePageMeta({
 
 const user = useStrapiUser() as Ref<User>
 const route = useRoute()
+const router = useRouter()
 const { findOne } = useStrapi4()
 
 const team: Ref<Team> = ref(null)
 if (route.params.team !== user.value.username) {
-  const { data } = await useAsyncData('team', () => findOne<Team>('teams', route.params.team as string))
+  const { data, error } = await useAsyncData('team', () => findOne<Team>('teams', route.params.team as string))
+  if (error.value) {
+    router.push({ name: '@team', params: { team: user.value.username } })
+  }
 
   team.value = data.value
 }
