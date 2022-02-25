27bf3bd19b26e24fd1dccf63572820a5602cbf43
Benjamin Canac <canacb1@gmail.com>
2022-02-25 18:29:38 +0100

    fix(index): dark mode

--- pages/index.vue
+++ pages/index.vue
@@ -2,12 +2,12 @@
   <Page>
     <main class="mt-16 mx-auto px-4 sm:mt-24">
       <div class="text-center">
-        <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+        <h1 class="text-4xl tracking-tight font-extrabold u-text-gray-900 sm:text-5xl md:text-6xl">
           <span class="block xl:inline">A better way to create<br></span>
           {{ ' ' }}
           <span class="block text-[#00DC82] xl:inline">modern websites</span>
         </h1>
-        <p class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
+        <p class="mt-3 max-w-md mx-auto text-base u-text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
           Our suite of developer-friendly solutions helps you create,<br>secure and deliver enterprise-grade modern website.
         </p>
         <div class="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
