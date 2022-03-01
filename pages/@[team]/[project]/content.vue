d74a8b281f0794e046cfaded6e8db3e51e608e0b
Sylvain Marroufin <marroufin.sylvain@gmail.com>
2022-03-01 14:01:44 +0100

    feat(project): call `/tree` to fetch files

--- pages/@[team]/[project]/content.vue
+++ pages/@[team]/[project]/content.vue
@@ -4,7 +4,7 @@
       <FilesTree :files="files" :selected-file="selectedFile" @select-file="selectFile" />
     </template>
 
-    Editor
+    <pre class="whitespace-pre break-words text-sm"><code>{{ selectedFile }}</code></pre>
   </ProjectPage>
 </template>
 
@@ -12,7 +12,7 @@
 import type { PropType, Ref } from 'vue'
 import type { Team, Project, File } from '~/types'
 
-defineProps({
+const props = defineProps({
   team: {
     type: Object as PropType<Team>,
     default: null
@@ -23,42 +23,10 @@ defineProps({
   }
 })
 
-const files: Ref<File[]> = ref([
-  {
-    type: 'directory',
-    path: 'content/blog',
-    name: 'blog',
-    children: [
-      {
-        type: 'file',
-        path: 'content/blog/1.molestie-ac-feugiat-sed-lectus.md',
-        name: '1.molestie-ac-feugiat-sed-lectus.md',
-        isDraft: true
-      },
-      {
-        type: 'file',
-        path: 'content/blog/2.commodo-sed-egestas-egestas-fringilla-phasellus.md',
-        name: '2.commodo-sed-egestas-egestas-fringilla-phasellus.md',
-        isDraft: true
-      },
-      {
-        type: 'file',
-        path: 'content/blog/3.praesent-semper-feugiat-nibh-sed-pulvinar-proin.md',
-        name: '3.praesent-semper-feugiat-nibh-sed-pulvinar-proin.md'
-      },
-      {
-        type: 'file',
-        path: 'content/blog/4.molestie-ac-feugiat-sed-lectus.md',
-        name: '4.molestie-ac-feugiat-sed-lectus.md'
-      }
-    ]
-  },
-  {
-    type: 'file',
-    path: 'content/index.md',
-    name: 'index.md'
-  }
-])
+const client = useStrapiClient()
+
+const { data: files } = await useAsyncData('files', () => client<File[]>(`/projects/${props.project.id}/tree`))
+
 const selectedFile: Ref<File> = ref(files.value.find(file => file.path.endsWith('index.md')))
 
 function selectFile (file) {
