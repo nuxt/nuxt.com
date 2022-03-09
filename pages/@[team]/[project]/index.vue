af2b77240d9805367f9d66bd13111105167ff9ac
Benjamin Canac <canacb1@gmail.com>
2022-03-09 16:52:10 +0100

    chore(project): fix links

--- pages/@[team]/[project]/index.vue
+++ pages/@[team]/[project]/index.vue
@@ -1,36 +1,38 @@
 <template>
   <ProjectPage>
-    <div class="lg:flex lg:items-center lg:justify-between">
-      <div class="flex-1 min-w-0">
-        <h2 class="text-2xl font-bold leading-7 u-text-gray-900 sm:text-3xl sm:truncate">
-          {{ team?.slug || user.username }}/{{ project.name }}
-        </h2>
-        <div class="mt-1 flex flex-col sm:flex-row sm:flex-wrap sm:mt-0 sm:space-x-6">
-          <div class="mt-2 flex items-center text-sm u-text-gray-500">
-            <UIcon name="heroicons-outline:clock" class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" aria-hidden="true" />
-            <time>
-              Updated {{ useTimeAgo(new Date(project.updatedAt)).value }}
-            </time>
+    <div class="flex-1 py-4 sm:py-6 lg:py-8">
+      <div class="lg:flex lg:items-center lg:justify-between">
+        <div class="flex-1 min-w-0">
+          <h2 class="text-2xl font-bold leading-7 u-text-gray-900 sm:text-3xl sm:truncate">
+            {{ team?.slug || user.username }}/{{ project.name }}
+          </h2>
+          <div class="flex flex-col mt-1 sm:flex-row sm:flex-wrap sm:mt-0 sm:space-x-6">
+            <div class="flex items-center mt-2 text-sm u-text-gray-500">
+              <UIcon name="heroicons-outline:clock" class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" aria-hidden="true" />
+              <time>
+                Updated {{ useTimeAgo(new Date(project.updatedAt)).value }}
+              </time>
+            </div>
           </div>
         </div>
-      </div>
-      <div class="mt-5 flex items-center lg:mt-0 lg:ml-4 gap-3">
-        <UButton label="Edit" icon="heroicons-outline:pencil" variant="secondary" />
+        <div class="flex items-center gap-3 mt-5 lg:mt-0 lg:ml-4">
+          <UButton label="Edit" :to="{ name: '@team-project-content' }" icon="heroicons-outline:pencil" variant="secondary" />
 
-        <UButton
-          v-if="project.repository.url"
-          :to="project.repository.url"
-          target="_blank"
-          label="View"
-          icon="heroicons-outline:link"
-          variant="secondary"
-        />
+          <UButton
+            v-if="project.repository.url"
+            :to="project.repository.url"
+            target="_blank"
+            label="View"
+            icon="heroicons-outline:link"
+            variant="secondary"
+          />
 
-        <UButton label="Publish" icon="heroicons-outline:check" />
+          <UButton label="Open on GitHub" :to="`https://github.com/${project.repository.owner}/${project.repository.name}`" target="_blank" icon="fa-brands:github" />
+        </div>
       </div>
+
+      <UCard class="mt-8" />
     </div>
-
-    <UCard class="mt-8" />
   </ProjectPage>
 </template>
 
