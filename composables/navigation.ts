0dd0c2ecf1045f1823f64837855453f42453172e
Florent <florentdelerue@hotmail.com>
2022-03-10 17:00:06 +0100

    refactor(navigation): remove transition and fix collapse animation

--- composables/navigation.ts
+++ composables/navigation.ts
@@ -2,111 +2,36 @@ import { useMotion } from '@vueuse/motion'
 import type { MotionInstance } from '@vueuse/motion'
 import type { Ref } from 'vue'
 
-export const useNavigationMotion = (navigationContainer: Ref<HTMLElement>) => {
-  // Navigation motion instance
-  const navigationMotion = useMotion(navigationContainer)
-
-  function onEnter (el: HTMLElement, done: any) {
-    /* if (isMobileLayout) {
-      setTimeout(() => {
-        navigationMotion.apply({
-          height: el.clientHeight
-        })
-      }, 0)
-    } */
-
-    // Slide-in
-    useMotion(el, {
-      initial: {
-        opacity: 0,
-        x: -Math.abs(el.offsetWidth) / 2,
-        zIndex: 10
-      },
-      enter: {
-        opacity: 1,
-        x: 0,
-        transition: {
-          delay: 0,
-          duration: 200,
-          ease: 'circOut',
-          onComplete: done
-        }
-      }
-    })
-  }
-
-  function onLeave (el: HTMLElement, done: any) {
-    /* if (isMobileLayout) {
-      setTimeout(() => {
-        navigationMotion.set({
-          height: el.clientHeight
-        })
-      }, 0)
-    } */
-
-    // Slide-out
-    useMotion(el, {
-      initial: {
-        opacity: 1,
-        x: 0
-      },
-      enter: {
-        opacity: 0,
-        x: 0,
-        // TODO: Fix this
-        // x: backDirection.value ? 0 : 50,
-        transition: {
-          duration: 100,
-          ease: 'linear',
-          onComplete: done
-        }
-      }
-    })
-  }
-
-  /* function updateHeight () {
-    if (isMobileLayout) {
-      navigationMotion.set({
-        height: 'auto'
-      })
-    }
-  } */
-
-  return {
-    onEnter,
-    onLeave
-    // updateHeight
-  }
-}
-
 export const useNavigationItemMotion = (
   emit: (...props: any) => void,
   collapsible: Ref<HTMLElement>,
-  content: Ref<HTMLElement>
+  asideItem: Ref<HTMLElement>,
+  asideLinks: Ref<HTMLElement>,
+  openedPadding: Number
 ) => {
-  const button = ref(null)
-  const buttonHeight = ref()
-  const contentHeight = ref()
   const opened = ref(true)
+  const asideItemHeight = ref()
+  const asideLinksHeight = ref()
 
   let collapsibleMotion: MotionInstance<any>
+
   function initCollapsible (): void {
-    buttonHeight.value = button.value?.clientHeight
-    contentHeight.value = content.value?.clientHeight
+    asideItemHeight.value = asideItem.value?.clientHeight
+    asideLinksHeight.value = asideLinks.value?.clientHeight
 
     collapsibleMotion = useMotion(collapsible, {
       initial: {
-        height: buttonHeight.value + contentHeight.value
+        height: asideItemHeight.value + asideLinksHeight.value + openedPadding
       },
       open: {
-        height: buttonHeight.value + contentHeight.value,
+        height: asideItemHeight.value + asideLinksHeight.value + openedPadding,
         transition: {
-          ease: 'circOut',
-          duration: 250
+          ease: 'ease-in',
+          duration: 500
         }
       },
       close: {
-        height: buttonHeight.value,
+        height: asideItemHeight.value,
         transition: {
           ease: 'circOut',
           duration: 250
@@ -138,10 +63,10 @@ export const useNavigationItemMotion = (
   return {
     collapse,
     collapsible,
-    button,
-    content,
-    buttonHeight,
-    contentHeight,
+    asideItem,
+    asideLinks,
+    asideItemHeight,
+    asideLinksHeight,
     opened
   }
 }
