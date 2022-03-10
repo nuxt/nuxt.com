bc1e8249dbded572763593ab6b654909ca764e77
Florent <florentdelerue@hotmail.com>
2022-03-10 16:58:27 +0100

    fix(docsasideitemlink): update props type

--- components/organisms/docs/DocsAsideItemLink.vue
+++ components/organisms/docs/DocsAsideItemLink.vue
@@ -3,6 +3,7 @@
     :to="link.to"
     active-class="font-semibold u-text-gray-900 u-border-gray-900"
     inactive-class="font-medium u-text-gray-400 u-border-gray-200"
+    exact
   >
     {{ link.label }}
   </ULink>
@@ -11,8 +12,8 @@
 <script setup lang="ts">
 defineProps({
   link: {
-    type: Array,
-    default: () => []
+    type: Object,
+    required: true
   }
 })
 </script>
