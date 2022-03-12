1999a10f7b40dbe4f1f16b4c4aa5347b466e0006
Benjamin Canac <canacb1@gmail.com>
2022-03-12 23:21:10 +0100

    chore(community): improve page style

--- pages/community.vue
+++ pages/community.vue
@@ -5,48 +5,48 @@ const { data: framework } = await useFetch('/api/community/framework')
 <template>
   <Page>
     <div>
-      <div class="sm:flex-auto">
-        <h1 class="text-2xl font-semibold text-gray-900">
+      <div class="px-4 sm:px-0">
+        <h1 class="text-2xl font-semibold u-text-gray-900">
           Nuxt Community
         </h1>
-        <p class="mt-2 text-sm text-gray-700">
+        <p class="mt-2 text-sm u-text-gray-700">
           Discover the community and metrics around Nuxt.
         </p>
       </div>
-      <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
-        <div class="px-4 py-5 bg-white shadow rounded-lg overflow-hidden sm:p-6">
-          <dt class="text-sm font-medium text-gray-500 truncate">
+      <dl class="grid grid-cols-1 gap-5 mt-5 sm:grid-cols-3">
+        <UCard>
+          <dt class="text-sm font-medium truncate u-text-gray-500">
             GitHub Stars
           </dt>
-          <dd class="mt-1 text-3xl font-semibold text-gray-900">
+          <dd class="mt-1 text-3xl font-semibold u-text-gray-900">
             {{ framework.stats.stars }}
           </dd>
-        </div>
-        <div class="px-4 py-5 bg-white shadow rounded-lg overflow-hidden sm:p-6">
-          <dt class="text-sm font-medium text-gray-500 truncate">
+        </UCard>
+        <UCard>
+          <dt class="text-sm font-medium truncate u-text-gray-500">
             Members
           </dt>
-          <dd class="mt-1 text-3xl font-semibold text-gray-900">
+          <dd class="mt-1 text-3xl font-semibold u-text-gray-900">
             {{ framework.stats.members }}
           </dd>
-        </div>
-        <div class="px-4 py-5 bg-white shadow rounded-lg overflow-hidden sm:p-6">
-          <dt class="text-sm font-medium text-gray-500 truncate">
+        </UCard>
+        <UCard>
+          <dt class="text-sm font-medium truncate u-text-gray-500">
             Activities
           </dt>
-          <dd class="mt-1 text-3xl font-semibold text-gray-900">
+          <dd class="mt-1 text-3xl font-semibold u-text-gray-900">
             {{ framework.stats.activities }}
           </dd>
-        </div>
+        </UCard>
       </dl>
     </div>
     <div class="mt-10">
-      <div class="sm:flex sm:items-center">
+      <div class="px-4 sm:flex sm:items-center sm:px-0">
         <div class="sm:flex-auto">
-          <h1 class="text-xl font-semibold text-gray-900">
+          <h1 class="text-xl font-semibold u-text-gray-900">
             Contributors
           </h1>
-          <p class="mt-2 text-sm text-gray-700">
+          <p class="mt-2 text-sm u-text-gray-700">
             The people helping the framework getting better, everyday.
           </p>
         </div>
@@ -54,53 +54,86 @@ const { data: framework } = await useFetch('/api/community/framework')
           <!-- CTA -->
         </div>
       </div>
-      <div class="mt-8 flex flex-col">
-        <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
-            <div class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
-              <table class="min-w-full divide-y divide-gray-300">
-                <thead class="bg-gray-50">
+      <UCard class="mt-8" body-class>
+        <ul role="list" class="divide-y divide-gray-200">
+          <li v-for="c in framework.contributors" :key="c.github">
+            <div class="flex items-center px-4 py-4 sm:px-6">
+              <div class="flex items-center flex-1 min-w-0">
+                <UAvatar :src="`https://github.com/${c.github}.png`" :alt="c.github" class="flex-shrink-0" />
+                <div class="grid flex-1 min-w-0 grid-cols-12 gap-4 px-4">
+                  <div class="col-span-9">
+                    <p class="text-sm font-medium text-indigo-600 truncate">
+                      {{ c.name }}
+                    </p>
+                    <p class="flex items-center mt-2 text-sm text-gray-500">
+                      <span class="truncate">{{ c.bio }}</span>
+                    </p>
+                  </div>
+                  <div class="flex items-center justify-center gap-3">
+                    <a :href="`https://github.com/${c.github}`" class="hover:u-text-gray-900" target="_blank" rel="noopener"><UIcon name="fa-brands:github" class="w-5 h-5" /></a>
+                    <a v-if="c.twitter" :href="`https://twitter.com/${c.twitter}`" class="hover:u-text-gray-900" target="_blank" rel="noopener"><UIcon name="fa-brands:twitter" class="w-5 h-5" /></a>
+                  </div>
+                  <div class="flex items-center justify-center gap-3">
+                    <span v-if="c.roles.length" class="inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-green-100 rounded-full">{{ c.roles[0] }}</span>
+                  </div>
+                  <div class="flex items-center justify-end gap-3">
+                    <a :href="`https://github.com/nuxt/framework/pulls?q=is%3Apr+author%3A${c.github}`" target="_blank" rel="noopener">
+                      {{ c.activities }}
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </UCard>
+      <!-- <div class="inline-block max-w-full min-w-full py-2 align-middle md:px-6 lg:px-8">
+            <div class="overflow-hidden shadow ring-1 u-ring-gray-200 md:rounded-lg">
+              <table class="min-w-full divide-y table-auto u-divide-gray-300">
+                <thead class="u-bg-gray-50">
                   <tr>
-                    <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">
+                    <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold u-text-gray-900 sm:pl-6">
                       Name
                     </th>
-                    <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                    <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold u-text-gray-900">
                       Social
                     </th>
-                    <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                    <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold u-text-gray-900">
                       Role
                     </th>
-                    <th scope="col" class="px-3 py-3.5 text-right text-sm font-semibold text-gray-900">
+                    <th scope="col" class="px-3 py-3.5 text-right text-sm font-semibold u-text-gray-900">
                       Activities
                     </th>
                   </tr>
                 </thead>
-                <tbody class="divide-y divide-gray-200 bg-white">
+                <tbody class="divide-y u-bg-white u-divide-gray-200">
                   <tr v-for="c in framework.contributors" :key="c.github">
-                    <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6">
+                    <td class="py-4 pl-4 pr-3 text-sm whitespace-nowrap sm:pl-6">
                       <div class="flex items-center">
-                        <div class="h-10 w-10 flex-shrink-0">
-                          <img class="h-10 w-10 rounded-full" :src="`https://github.com/${c.github}.png`" :alt="c.github">
+                        <div class="flex-shrink-0 w-10 h-10">
+                          <img class="w-10 h-10 rounded-full" :src="`https://github.com/${c.github}.png`" :alt="c.github">
                         </div>
                         <div class="ml-4">
-                          <div class="font-medium text-gray-900">
+                          <div class="font-medium u-text-gray-900">
                             {{ c.name }}
                           </div>
-                          <div class="text-gray-500">
+                          <div class="truncate u-text-gray-500">
                             {{ c.bio }}
                           </div>
                         </div>
                       </div>
                     </td>
-                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                      <a :href="`https://github.com/${c.github}`" target="_blank" rel="noopener"><UIcon name="fa-brands:github" class="flw-4 h-4" /></a>
-                      <a v-if="c.twitter" :href="`https://twitter.com/${c.twitter}`" target="_blank" rel="noopener"><UIcon name="fa-brands:twitter" class="w-4 h-4" /></a>
+                    <td class="px-3 py-4 text-sm u-text-gray-500 whitespace-nowrap">
+                      <div class="flex items-center justify-center flex-shrink-0 gap-3">
+                        <a :href="`https://github.com/${c.github}`" class="hover:u-text-gray-900" target="_blank" rel="noopener"><UIcon name="fa-brands:github" class="h-4 flw-4" /></a>
+                        <a v-if="c.twitter" :href="`https://twitter.com/${c.twitter}`" class="hover:u-text-gray-900" target="_blank" rel="noopener"><UIcon name="fa-brands:twitter" class="w-4 h-4" /></a>
+                      </div>
                     </td>
-                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                      <span v-if="c.roles.length" class="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800">{{ c.roles[0] }}</span>
+                    <td class="px-3 py-4 text-sm u-text-gray-500 whitespace-nowrap">
+                      <span v-if="c.roles.length" class="inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-green-100 rounded-full">{{ c.roles[0] }}</span>
                     </td>
-                    <td class="whitespace-nowrap px-3 py-4 text-sm text-right font-medium">
-                      <a :href="`https://github.com/nuxt/framework/pulls?q=is%3Apr+author%3A${c.username}`" target="_blank" rel="noopener">
+                    <td class="px-3 py-4 text-sm font-medium text-right whitespace-nowrap">
+                      <a :href="`https://github.com/nuxt/framework/pulls?q=is%3Apr+author%3A${c.github}`" target="_blank" rel="noopener">
                         {{ c.activities }}
                       </a>
                     </td>
@@ -108,9 +141,7 @@ const { data: framework } = await useFetch('/api/community/framework')
                 </tbody>
               </table>
             </div>
-          </div>
-        </div>
-      </div>
+          </div> -->
     </div>
   </Page>
 </template>
