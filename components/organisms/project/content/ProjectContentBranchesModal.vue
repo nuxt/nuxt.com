c640985b0e4ba43ca68d903dfd6a0c3259b71c96
Benjamin Canac <canacb1@gmail.com>
2022-03-14 17:37:09 +0100

    chore(content): improve modals

--- components/organisms/project/content/ProjectContentBranchesModal.vue
+++ components/organisms/project/content/ProjectContentBranchesModal.vue
@@ -1,60 +1,50 @@
 <template>
-  <UModal v-model="isOpen" header-class body-class="flex-1 h-80 lg:overflow-y-auto">
-    <template #header>
-      <div class="flex items-center gap-1.5">
-        <UInput
-          v-model="q"
-          name="q"
-          placeholder="Search branch..."
-          icon="heroicons-outline:search"
-          appearance="none"
-          autocomplete="off"
-          class="w-full pl-1"
-          size="xl"
-          autofocus
-        />
-
-        <UButton
-          :loading="pending"
-          icon="heroicons-outline:refresh"
-          variant="transparent"
-          size="sm"
-          class="mr-2"
-          @click="$emit('refreshBranches')"
-        />
+  <UModal v-model="isOpen" width-class="max-w-xl" body-class="relative flex flex-col overflow-hidden h-80">
+    <Combobox as="div" class="flex flex-col flex-1 min-h-0 divide-y u-divide-gray-100" @update:modelValue="onSelect">
+      <div class="relative">
+        <UIcon name="heroicons-outline:search" class="pointer-events-none absolute top-3.5 left-5 h-5 w-5 u-text-gray-400" aria-hidden="true" />
+        <ComboboxInput class="w-full h-12 pr-4 placeholder-gray-400 bg-transparent border-0 pl-[3.25rem] u-text-gray-900 focus:ring-0 sm:text-sm" placeholder="Search..." @change="query = $event.target.value" />
       </div>
-    </template>
 
-    <ul class="divide-y u-divide-gray-200">
-      <li class="group flex items-center justify-between gap-3 px-4 py-2.5 cursor-pointer hover:u-bg-gray-50" @click="onCreateBranchClick">
-        <div class="flex items-center gap-3 truncate">
-          <UIcon name="heroicons-outline:plus" class="flex-shrink-0 w-4 h-4 u-text-gray-400" />
-          <span class="text-sm font-medium truncate u-text-gray-700">Create new branch {{ q && !branchExists ? `"${q}"` : '' }}</span>
-        </div>
+      <ComboboxOptions static class="relative flex-1 overflow-y-auto divide-y u-divide-gray-100 scroll-py-2">
+        <li class="p-2">
+          <h2 v-if="query === ''" class="px-3 my-2 text-xs font-semibold text-gray-900">
+            Actions
+          </h2>
 
-        <UIcon
-          name="heroicons-outline:chevron-right"
-          class="flex-shrink-0 invisible w-5 h-5 group-hover:visible u-text-gray-400"
-        />
-      </li>
-      <li v-for="branch in filteredBranches" :key="branch.name" class="group flex items-center justify-between gap-3 px-4 py-2.5 cursor-pointer hover:u-bg-gray-50" @click="onBranchClick(branch)">
-        <div class="flex items-center gap-3 truncate">
-          <UIcon name="mdi:source-branch" class="flex-shrink-0 w-4 h-4 u-text-gray-400" />
-          <span class="text-sm font-medium truncate u-text-gray-700">{{ branch.name }}</span>
-          <UIcon v-if="selectedBranch?.name === branch.name" name="heroicons-outline:check" class="flex-shrink-0 w-4 h-4 text-primary-500" />
-        </div>
-
-        <UIcon
-          name="heroicons-outline:chevron-right"
-          class="flex-shrink-0 invisible w-5 h-5 group-hover:visible u-text-gray-400"
-        />
-      </li>
-    </ul>
+          <ul class="text-sm u-text-gray-700">
+            <ComboboxOption v-for="(action, index) in actions" :key="index" v-slot="{ active }" :value="action" as="template">
+              <li :class="['flex cursor-pointer select-none items-center rounded-md px-3 py-2', active && 'u-bg-gray-100 u-text-gray-900']">
+                <UIcon :name="action.icon" :class="['h-5 w-5 flex-none u-text-gray-400', active && 'u-text-gray-900', action.iconClass]" aria-hidden="true" />
+                <span class="flex-auto ml-3 truncate">{{ action.label }}</span>
+              </li>
+            </ComboboxOption>
+          </ul>
+        </li>
+        <li v-if="filteredBranches.length" class="p-2">
+          <ul class="text-sm u-text-gray-700">
+            <ComboboxOption v-for="branch of filteredBranches" :key="branch.name" v-slot="{ active }" :value="branch" as="template">
+              <li :class="['flex cursor-pointer select-none items-center rounded-md px-3 py-2', active && 'u-bg-gray-100 u-text-gray-900']">
+                <UIcon name="mdi:source-branch" :class="['h-5 w-5 flex-none u-text-gray-400', active && 'u-text-gray-900']" aria-hidden="true" />
+                <span class="flex-auto ml-3 truncate">{{ branch.name }}</span>
+                <span v-if="active" class="flex-none ml-3 u-text-gray-500">Jump to...</span>
+              </li>
+            </ComboboxOption>
+          </ul>
+        </li>
+      </ComboboxOptions>
+    </Combobox>
   </UModal>
 </template>
 
 <script setup lang="ts">
 import type { PropType } from 'vue'
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxOptions,
+  ComboboxOption
+} from '@headlessui/vue'
 import type { Branch } from '~/types'
 
 const props = defineProps({
@@ -75,7 +65,6 @@ const props = defineProps({
     default: false
   }
 })
-
 const emit = defineEmits(['update:modelValue', 'selectBranch', 'refreshBranches', 'createBranch'])
 
 const isOpen = computed({
@@ -87,22 +76,41 @@ const isOpen = computed({
   }
 })
 
-const q = ref('')
+const query = ref('')
+const branchExists = computed(() => query.value && props.branches.some(b => b.name === query.value))
 const filteredBranches = computed(() => {
-  return props.branches.filter(b => b.name.search(new RegExp(q.value, 'i')) !== -1)
+  let branches = [...props.branches]
+  if (query.value) {
+    branches = branches.filter(b => b.name.search(new RegExp(query.value, 'i')) !== -1)
+  }
+  branches = branches.filter(b => b.name !== props.selectedBranch?.name)
+  return branches
 })
+const actions = computed(() => ([
+  { label: `Create new branch ${query.value && !branchExists.value ? `"${query.value}"` : ''}`, icon: 'heroicons-outline:plus', click: onCreateBranchClick },
+  { label: 'Refresh branches', icon: 'heroicons-outline:refresh', iconClass: props.pending ? 'animate-spin' : '', click: onRefreshBranchesClick }
+]))
 
-const branchExists = computed(() => q.value && props.branches.some(b => b.name === q.value))
-
-function onBranchClick (b: Branch) {
+function onBranchSelect (b: Branch) {
   emit('selectBranch', b)
   isOpen.value = false
-  q.value = ''
+  query.value = ''
 }
 
 function onCreateBranchClick () {
-  emit('createBranch', !branchExists.value ? q.value : '')
+  emit('createBranch', !branchExists.value ? query.value : '')
   isOpen.value = false
-  q.value = ''
+  query.value = ''
+}
+function onRefreshBranchesClick () {
+  emit('refreshBranches')
+}
+
+function onSelect (option) {
+  if (option.click) {
+    option.click()
+  } else {
+    onBranchSelect(option)
+  }
 }
 </script>
