de5c54f29a6e2d320e091209e8c2a3488ba4b75b
Sylvain Marroufin <marroufin.sylvain@gmail.com>
2022-03-18 17:38:00 +0100

    chore: update modals CTA wording

--- components/organisms/project/content/ProjectContentCreateBranchModal.vue
+++ components/organisms/project/content/ProjectContentCreateBranchModal.vue
@@ -36,7 +36,7 @@
       />
     </div>
     <div class="gap-3 mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
-      <UButton type="submit" label="Save" variant="green" class="justify-center flex-shrink-0 w-full sm:w-auto" />
+      <UButton type="submit" label="Create" variant="green" class="justify-center flex-shrink-0 w-full sm:w-auto" />
       <UButton type="button" label="Cancel" variant="secondary" class="justify-center flex-shrink-0 w-full mt-3 sm:w-auto sm:mt-0" @click="close" />
     </div>
   </UModal>
