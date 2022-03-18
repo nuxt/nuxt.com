4319c09aa5dfa4d55272bbc8d922126fa7e778f4
Sylvain Marroufin <marroufin.sylvain@gmail.com>
2022-03-18 17:44:28 +0100

    feat(project): add icons to modals (#183)

--- components/organisms/project/content/ProjectContentRevertFileModal.vue
+++ components/organisms/project/content/ProjectContentRevertFileModal.vue
@@ -1,13 +1,26 @@
 <template>
   <UModal v-model="isOpen" appear @close="onClose" @submit.prevent="onSubmit">
     <div class="sm:flex sm:items-start">
-      <div class="mt-3 text-center sm:mt-0 sm:text-left">
+      <div class="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-amber-100 dark:bg-amber-500 sm:mx-0 sm:h-10 sm:w-10">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="text-amber-500 dark:text-white h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          stroke-width="2"
+        >
+          <path stroke-linecap="round" stroke-linejoin="round" d="M3 10h10a8 8 0 018 8v2M3 10l6 6m-6-6l6-6" />
+        </svg>
+      </div>
+
+      <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
         <h3 class="text-lg font-medium leading-6 u-text-gray-900">
           Revert file
         </h3>
         <div class="mt-2">
           <p class="text-sm break-all u-text-gray-500">
-            Are you sure you want to revert changes you made on “{{ name }}”?
+            Are you sure you want to revert changes on “{{ name }}”?
           </p>
         </div>
       </div>
@@ -33,14 +46,13 @@ const emit = defineEmits(['submit', 'close'])
 
 const isOpen = ref(true)
 
-function onSubmit () {
-  emit('submit', props.path)
-  isOpen.value = false
-  onClose()
-}
 function close () {
   isOpen.value = false
   onClose()
+}
+function onSubmit () {
+  emit('submit', props.path)
+  close()
 }
 function onClose () {
   setTimeout(() => {
