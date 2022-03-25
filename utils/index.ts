69da7ee02c4f616aa946780d77cee2c254abc7ee
Sylvain Marroufin <marroufin.sylvain@gmail.com>
2022-03-25 14:58:19 +0100

    feat(project): format file size display (#213)

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,12 @@
 export const capitalize = function (str) {
   return str.charAt(0).toUpperCase() + str.slice(1)
 }
+
+export const toFormattedBytes = (bytes?: number) => {
+  const units = ['bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+  let l = 0; let n = bytes || 0
+  while (n >= 1024 && ++l) {
+    n = n / 1024
+  }
+  return (n.toFixed(n < 10 && l > 0 ? 1 : 0) + ' ' + units[l])
+}
