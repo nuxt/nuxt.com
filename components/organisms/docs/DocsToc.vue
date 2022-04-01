f7d6c33ed8e32d5b18c6ec0416d23b1154b46695
Yaël Guilloux <yael.guilloux@gmail.com>
2022-04-01 17:45:19 +0200

    feat(toc): update table of contents implementation (scrollSpy, scrollTo, prev/next)

--- components/organisms/docs/DocsToc.vue
+++ components/organisms/docs/DocsToc.vue
@@ -1,27 +1,32 @@
 <template>
   <div class="flex flex-col text-sm font-medium text-right gap-y-2 u-text-gray-500">
-    <NuxtLink to="#" class="flex justify-end hover:font-semibold hover:u-text-gray-900 gap-x-3">
+    <NuxtLink :to="previous?.slug || '#'" :disabled="!previous?.slug" class="flex justify-end hover:font-semibold hover:u-text-gray-900 gap-x-3">
       <span>Previous page</span>
-      <img src="/docs/toc-back.svg" alt="previous page">
+      <IconTocBack />
     </NuxtLink>
-    <div class="flex justify-end font-semibold u-text-gray-900 gap-x-3">
-      <span>On this page</span>
-      <img src="/docs/toc-current.svg" alt="on this page">
-    </div>
-    <ul class="flex flex-col pr-2.5">
-      <li v-for="link in links" :key="link.label" class="py-1 overflow-hidden truncate border-r-2" :class="route.hash === link.to ? 'u-border-gray-900' : 'u-border-gray-300'">
-        <NuxtLink
-          :to="{ hash: link.to }"
-          class="hover:font-semibold hover:u-text-gray-900"
-          :class="[ { 'u-text-gray-900 font-semibold': route.hash === link.to }, link.level === '2' ? 'pr-6 text-xs' : 'mr-2']"
-        >
-          {{ link.label }}
-        </NuxtLink>
-      </li>
-    </ul>
-    <NuxtLink to="#" class="flex justify-end hover:font-semibold hover:u-text-gray-900 gap-x-3">
+    <template v-if="toc.length">
+      <div class="flex justify-end font-semibold u-text-gray-900 gap-x-3">
+        <span>On this page</span>
+        <IconTocCurrent />
+      </div>
+      <ul class="flex flex-col pr-2.5">
+        <li v-for="link in toc" :key="link.text" class="py-1 overflow-hidden truncate border-r-2" :class="activeHeadings.includes(link.id) ? 'u-border-gray-900' : 'u-border-gray-300'">
+          <a
+            :to="{ hash: link.id }"
+            class="hover:font-semibold hover:u-text-gray-900 mr-2"
+            :class="{
+              'u-text-gray-900': activeHeadings.includes(link.id),
+            }"
+            @click.prevent="scrollToHeading(link.id, '--docs-scroll-margin-block')"
+          >
+            {{ link.text }}
+          </a>
+        </li>
+      </ul>
+    </template>
+    <NuxtLink :to="next?.slug || '#'" :disabled="!next?.slug" class="flex justify-end hover:font-semibold hover:u-text-gray-900 gap-x-3">
       <span>Next page</span>
-      <img src="/docs/toc-next.svg" alt="next page">
+      <IconTocNext />
     </NuxtLink>
   </div>
 </template>
@@ -34,31 +39,27 @@ defineProps({
   }
 })
 
-const { updateHeadings } = useScrollspy()
 const route = useRoute()
 
-const links = [
-  { to: '#usage', label: 'Usage', level: '1' },
-  { to: '#example', label: 'Example', level: '1' },
-  { to: '#options', label: 'Options', level: '1' },
-  { to: '#maxage--expired', label: 'maxAge / expires', level: '2' },
-  { to: '#httponly', label: 'Http Only', level: '2' },
-  { to: '#secure', label: 'Secure', level: '2' },
-  { to: '#domain', label: 'Domain', level: '2' },
-  { to: '#path', label: 'Path', level: '2' },
-  { to: '#samesite', label: 'Same site', level: '2' },
-  { to: '#encore', label: 'Encode', level: '2' },
-  { to: '#decode', label: 'Decode', level: '2' },
-  { to: '#handling-cookies-in-api-routes', label: 'Handling cookies in API routes', level: '1' }
-]
+const { activeHeadings, updateHeadings } = useScrollspy()
 
-onMounted(() =>
-  setTimeout(() => {
-    updateHeadings([
-      ...document.querySelectorAll('#docs-content h1'),
-      ...document.querySelectorAll('#docs-content h2'),
-      ...document.querySelectorAll('#docs-content h3')
-    ])
-  }, 200)
-)
+const { toc, previous, next } = useDocs()
+
+watch(route, () => {
+  if (process.client) {
+    setTimeout(() => {
+      updateHeadings([
+        ...document.querySelectorAll('.prose h1'),
+        ...document.querySelectorAll('.prose h2'),
+        ...document.querySelectorAll('.prose h3')
+      ])
+    }, 200)
+  }
+}, {
+  immediate: true
+})
+
+function scrollToHeading (id: string, scrollMarginCssVar: string) {
+  useScrollToHeading(id, scrollMarginCssVar)
+}
 </script>
