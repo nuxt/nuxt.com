e0f66ccac16cc67b0e712d2dbc8c5d3c923c3019
Benjamin Canac <canacb1@gmail.com>
2022-04-04 17:50:18 +0200

    chore(404): remove team link

--- pages/[...404].vue
+++ pages/[...404].vue
@@ -2,7 +2,7 @@
   <div class="flex-1 px-4 py-16 bg-white dark:bg-black sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
     <div class="mx-auto max-w-max">
       <main class="sm:flex">
-        <p class="text-4xl font-extrabold text-primary-500 sm:text-5xl">
+        <p class="text-4xl font-extrabold u-text-gray-400 sm:text-5xl">
           {{ error.statusCode }}
         </p>
         <div class="sm:ml-6">
@@ -15,8 +15,7 @@
             </p>
           </div>
           <div class="flex gap-3 mt-10 sm:border-l sm:border-transparent sm:pl-6">
-            <UButton v-if="user" :to="{ name: '@team-projects', params: { team: user.username } }" label="Go back home" variant="primary" />
-            <UButton v-else to="/" label="Go back home" variant="primary" />
+            <UButton to="/" label="Go back home" variant="primary" />
           </div>
         </div>
       </main>
@@ -25,11 +24,6 @@
 </template>
 
 <script setup lang="ts">
-import type { Ref } from 'vue'
-import type { User } from '~/types'
-
-const user = useStrapiUser() as Ref<User>
-
 const error = {
   name: 'Page not found',
   message: 'This is not the page you\'re looking for.',
