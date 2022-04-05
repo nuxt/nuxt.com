01ee94967d078858bce820ae615ae5a678912d67
Yaël Guilloux <yael.guilloux@gmail.com>
2022-04-05 16:07:39 +0200

    fix(router): update router config

--- app/router.options.ts
+++ app/router.options.ts
@@ -1,11 +1,8 @@
-import type { RouterOptions } from '@nuxt/schema'
+import type { RouterConfig } from '@nuxt/schema'
 
 // https://router.vuejs.org/api/#routeroptions
-export default <RouterOptions>{
-  scrollBehavior: (to, from, savedPosition) => {
-    // Apply this scrollBehavior only on `docs/` pages
-    if (!to.path.includes('/docs/')) { return }
-
+export default <RouterConfig>{
+  scrollBehavior: () => {
     // Scroll to top of window
     window.scrollTo({
       top: 0
