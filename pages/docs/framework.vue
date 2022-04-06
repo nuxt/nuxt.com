0250fa6eeff41a96e50b30d5613cae959a7db7c8
Yaël Guilloux <yael.guilloux@gmail.com>
2022-04-07 01:31:44 +0200

    fix(framework): update get started link

--- pages/docs/framework.vue
+++ pages/docs/framework.vue
@@ -46,7 +46,7 @@ const buttons = [
     size: 'xl',
     variant: 'primary',
     label: 'Get Started',
-    to: '/docs/framework/guides'
+    to: '/docs/framework/getting-started/introduction'
   }
 ]
 
