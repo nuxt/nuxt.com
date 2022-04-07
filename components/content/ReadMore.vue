78dcf8e28361da063b9775edbf4436a065fb939e
Yaël Guilloux <yael.guilloux@gmail.com>
2022-04-07 12:44:29 +0200

    feat(content): add @nuxt/content documentation ; improve content pages (#283)

---
+++ components/content/ReadMore.vue
@@ -0,0 +1,24 @@
+<template>
+  <Alert icon="👉">
+    Read more in <ULink :to="link" v-html="computedTitle" />.
+  </Alert>
+</template>
+
+<script setup lang="ts">
+import { splitByCase, upperFirst } from 'scule'
+
+const props = defineProps({
+  link: {
+    type: String,
+    required: true
+  },
+  title: {
+    type: String,
+    required: false,
+    default: ''
+  }
+})
+
+// Guess title from link!
+const computedTitle = computed(() => (props.title || props.link).split('/').filter(Boolean).map(part => splitByCase(part).map(p => upperFirst(p)).join(' ')).join(' > '))
+</script>
