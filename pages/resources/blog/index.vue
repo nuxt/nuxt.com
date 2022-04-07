10a4cdfddad33039f4f38dfdd46c8ac331ddf561
Florent Delerue <florentdelerue@hotmail.com>
2022-04-07 17:39:05 +0200

    feat(blog): add pages (#295)
    Co-authored-by: Yaël Guilloux <yael.guilloux@gmail.com>
    Co-authored-by: Benjamin Canac <canacb1@gmail.com>

---
+++ pages/resources/blog/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <UContainer padded>
+      <div class="text-left py-12 md:py-20">
+        <h1 class="text-4xl tracking-tight font-extrabold u-text-gray-900 sm:text-5xl">
+          Blog
+        </h1>
+      </div>
+
+      <ul role="list" class="flex flex-col gap-y-4 pb-16">
+        <li v-for="(page, index) in pages" :key="page.slug" :class="{ 'border-b last:border-none': index !== 0 }">
+          <!-- TODO: order by date, main article is the most recent -->
+          <ResourcesBlogPost :page="page" :highlight="index === 0" />
+        </li>
+      </ul>
+    </UContainer>
+  </div>
+</template>
+
+<script setup>
+const { fetchDir } = useContent()
+
+const { data: pages } = await useAsyncData('resources-blog', () => fetchDir('/resources/blog'))
+</script>
+
+<style scoped>
+._ellipse {
+  position: absolute;
+  width: 480.14px;
+  height: 161.22px;
+  left: 0;
+  top: 0;
+  background: linear-gradient(97.62deg, rgba(0, 71, 225, 1) 2.27%, rgba(26, 214, 255, 1) 50.88%, rgba(0, 220, 130, 1) 98.48%);
+  filter: blur(150px);
+  transform: matrix(-0.97, 0.24, 0.24, 0.97, 0, 0);
+}
+</style>
