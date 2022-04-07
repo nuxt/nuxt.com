dfb2f11fca5d8d4b45f135c6db57af7458a2af30
Florent Delerue <florentdelerue@hotmail.com>
2022-04-07 17:39:05 +0200

    feat(blog): add pages (#295)
    Co-authored-by: Yaël Guilloux <yael.guilloux@gmail.com>
    Co-authored-by: Benjamin Canac <canacb1@gmail.com>

---
+++ components/organisms/resources/blog/ResourcesBlogPost.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="flex flex-col gap-y-4 py-8" :class="{ 'rounded-lg border-2 u-border-gray-200 px-8': highlight }">
+    <div class="font-semibold u-text-gray-400" :class="{ 'text-sm': !highlight }">
+      <time>{{ formatDateByLocale('en', page.date) }}</time> - {{ page.category }}
+    </div>
+    <h2 class="u-text-gray-900 font-bold" :class="highlight ? 'text-3xl' : 'text-2xl'">
+      {{ page.title }}
+    </h2>
+    <p class="font-medium u-text-gray-500">
+      {{ page.description }}
+    </p>
+    <div class="flex justify-between items-center">
+      <NuxtLink :to="page.slug" class="relative inline-flex items-center group flex-nowrap max-w-max mb-4">
+        <span class="flex items-center font-semibold text-xl gap-x-2">
+          Read article<UIcon name="heroicons-solid:arrow-right" class="w-4 h-4 mt-1" />
+        </span>
+        <span
+          class="rounded absolute left-0 font-extrabold -bottom-1 h-0.5 w-4 group-hover:w-full transition-all"
+          :class="highlight ? 'bg-gradient-to-r from-[#00DC82] via-[#34CDFE] to-[#0047E1]' : 'u-bg-gray-700'"
+        />
+      </NuxtLink>
+      <UAvatarGroup :group="authors" size="sm" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+const props = defineProps({
+  page: {
+    type: Object,
+    required: true
+  },
+  highlight: {
+    type: Boolean,
+    default: false
+  }
+})
+
+const authors = computed(
+  () => {
+    return (props?.page?.authors || []).map(author => ({ src: author.avatarUrl, ...author }))
+  }
+)
+
+const formatDateByLocale = (locale, d) => {
+  return new Date(d).toLocaleDateString(locale, {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  })
+}
+</script>
