a89010f550ff6ced4313b8ff618bcf8212d7bc75
Benjamin Canac <canacb1@gmail.com>
2022-04-07 16:52:21 +0200

    up

--- pages/resources/case-studies/index.vue
+++ pages/resources/case-studies/index.vue
@@ -10,7 +10,7 @@
         </p>
       </div>
 
-      <ul role="list" class="grid sm:grid-cols-2 gap-4 md:grid-cols-3 sm:gap-6 md:gap-x-12 md:gap-y-10">
+      <ul role="list" class="grid sm:grid-cols-2 gap-4 lg:grid-cols-3 sm:gap-6 md:gap-x-12 md:gap-y-10">
         <li v-for="page in pages" :key="page.slug" class="relative">
           <div class="ring-1 u-ring-gray-200 lg:hover:u-ring-gray-900 lg:hover:ring-2 group block w-full aspect-w-16 aspect-h-7 rounded-lg u-bg-gray-100 overflow-hidden">
             <img :src="page.imgUrl" alt="" class="group-hover:scale-110 duration-300 transition-all object-contain object-center pointer-events-none bg-white">
