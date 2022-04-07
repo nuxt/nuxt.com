5630629cf97bcb9c380c4fe7a8f6269092d61cad
Benjamin Canac <canacb1@gmail.com>
2022-04-07 18:31:26 +0200

    chore(docs): fix sticky on mobile

--- components/organisms/docs/DocsPageContent.vue
+++ components/organisms/docs/DocsPageContent.vue
@@ -4,20 +4,15 @@
       <slot />
     </div>
 
-    <div class="col-span-2">
-      <div class="overflow-x-hidden sticky top-16 -mx-4 sm:-mx-6 px-4 sm:px-6 lg:mx-0 lg:px-0 lg:pt-8 lg:-mt-8  lg:max-h-[calc(100vh-64px)] bg-white/75 dark:bg-black/75 backdrop-blur-md">
-        <div class="border-b border-dashed u-border-gray-200 lg:border-none py-4 lg:py-0">
-          <button
-            class="flex lg:hidden items-center gap-3"
-            @click="isOpen = !isOpen"
-          >
-            <span class="u-text-gray-900 font-semibold text-sm">Table of Contents</span>
+    <div class="lg:col-span-2 lg:self-start overflow-x-hidden sticky top-16 -mx-4 sm:-mx-6 px-4 sm:px-6 lg:mx-0 lg:px-0 lg:pt-8 lg:-mt-8 lg:max-h-[calc(100vh-64px)] bg-white/75 dark:bg-black/75 backdrop-blur-md">
+      <div class="border-b border-dashed u-border-gray-200 lg:border-none py-4 lg:py-0">
+        <button class="flex lg:hidden items-center gap-3" @click="isOpen = !isOpen">
+          <span class="u-text-gray-900 font-semibold text-sm">Table of Contents</span>
 
-            <UIcon name="heroicons-outline:chevron-right" class="w-4 h-4 u-text-gray-400 transition-transform duration-100 transform" :class="[isOpen ? 'rotate-90' : 'rotate-0']" />
-          </button>
+          <UIcon name="heroicons-outline:chevron-right" class="w-4 h-4 u-text-gray-400 transition-transform duration-100 transform" :class="[isOpen ? 'rotate-90' : 'rotate-0']" />
+        </button>
 
-          <DocsToc class="mt-4 lg:mt-0" :class="[isOpen ? 'lg:block' : 'hidden lg:block']" />
-        </div>
+        <DocsToc class="mt-4 lg:mt-0" :class="[isOpen ? 'lg:block' : 'hidden lg:block']" />
       </div>
     </div>
   </div>
