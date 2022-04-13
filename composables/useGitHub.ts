f532c70a2db5934ba39b7bcd8b7ffad07603879b
Benjamin Canac <canacb1@gmail.com>
2022-04-13 16:49:48 +0200

    chore(deps): bump nuxt3

--- composables/useGitHub.ts
+++ composables/useGitHub.ts
@@ -1,5 +1,5 @@
 export const useGitHub = () => {
-  const config = useRuntimeConfig()
+  const config = useRuntimeConfig().public
 
   return {
     githubAppUrl: `https://github.com/apps/${config.github.appSlug}/installations/new`
