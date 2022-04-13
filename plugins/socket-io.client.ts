eb61b41c87e639bfa0522c96f64390cb17a421fe
Benjamin Canac <canacb1@gmail.com>
2022-04-13 15:25:25 +0200

    feat: add `socket-io` plugin

---
+++ plugins/socket-io.client.ts
@@ -0,0 +1,62 @@
+import { io } from 'socket.io-client'
+
+class SocketIO {
+  socket = null
+
+  connect () {
+    const url = useStrapiUrl()
+    const token = useStrapiToken()
+
+    if (!token.value) {
+      return
+    }
+
+    if (this.socket && this.socket.connected) {
+      return
+    }
+
+    this.socket = io(url, {
+      auth: {
+        token: `Bearer ${token.value}`
+      },
+      transports: ['websocket', 'polling']
+    })
+  }
+
+  disconnect () {
+    if (this.socket) {
+      this.socket.disconnect()
+    }
+  }
+
+  emit (event, data) {
+    return this.socket.emit(event, data)
+  }
+
+  on (event, fn) {
+    return this.socket.on(event, fn)
+  }
+
+  off (event, fn) {
+    return this.socket.off(event, fn)
+  }
+}
+
+export default defineNuxtPlugin(() => {
+  const user = useStrapiUser()
+  const socket = new SocketIO()
+
+  watch(user, () => {
+    if (!user.value) {
+      return socket.disconnect()
+    }
+
+    socket.connect()
+  }, { immediate: true })
+
+  return {
+    provide: {
+      socket: new SocketIO()
+    }
+  }
+})
