9244407468ea6bdfce47b7b203692c75a9b0bbf4
Benjamin Canac <canacb1@gmail.com>
2022-04-15 16:19:58 +0200

    up

--- components/content/resources/live-events/ResourcesLiveEventsCard.vue
+++ components/content/resources/live-events/ResourcesLiveEventsCard.vue
@@ -46,7 +46,7 @@
 </template>
 
 <script setup lang="ts">
-const props = defineProps({
+defineProps({
   page: {
     type: Object,
     default: () => {}
--- components/content/resources/live-events/ResourcesLiveEventsList.vue
+++ components/content/resources/live-events/ResourcesLiveEventsList.vue
@@ -9,7 +9,5 @@
 </template>
 
 <script setup lang="ts">
-
 const { data: liveEventsData } = await useAsyncData('live-events', () => queryContent('/resources/live-events/collections').find())
-
 </script>
