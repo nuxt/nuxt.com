3bbb094bc146a1d42f04a3dbe4b84fa87a3e20c8
Florent Delerue <florentdelerue@hotmail.com>
2022-04-15 16:23:38 +0200

    refactor(blog): index with markdown only (#340)
    Co-authored-by: Benjamin Canac <canacb1@gmail.com>

---
+++ components/content/resources/blog/ResourcesBlogHero.vue
@@ -0,0 +1,9 @@
+<template>
+  <UContainer padded>
+    <div class="text-left py-12 md:py-20">
+      <h1 class="text-4xl tracking-tight font-extrabold u-text-gray-900 sm:text-5xl">
+        <Markdown use="title" unwrap="p" />
+      </h1>
+    </div>
+  </UContainer>
+</template>
--- pages/resources/blog/index.vue
+++ pages/resources/blog/index.vue
@@ -1,24 +1,9 @@
 <template>
-  <div>
-    <UContainer padded>
-      <div class="text-left py-12 md:py-20">
-        <h1 class="text-4xl tracking-tight font-extrabold u-text-gray-900 sm:text-5xl">
-          Blog
-        </h1>
-      </div>
-
-      <ul role="list" class="flex flex-col gap-y-4 pb-16">
-        <li v-for="(page, index) in pages" :key="page.slug" :class="{ 'border-b last:border-none': index !== 0 }">
-          <!-- TODO: order by date, main article is the most recent -->
-          <ResourcesBlogPost :page="page" :highlight="index === 0" />
-        </li>
-      </ul>
-    </UContainer>
-  </div>
+  <Content :document="page" />
 </template>
 
-<script setup>
-const { fetchDir } = useContent()
+<script setup lang="ts">
+const { page, fetchPage } = useContent()
 
-const { data: pages } = await useAsyncData('resources-blog', () => fetchDir('/resources/blog'))
+await fetchPage()
 </script>
