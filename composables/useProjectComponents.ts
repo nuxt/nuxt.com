628c7da73886d09723f5fc94d121b7eaf94b48cf
Benjamin Canac <canacb1@gmail.com>
2022-04-19 11:49:04 +0200

    feat(project): fetch components

---
+++ composables/useProjectComponents.ts
@@ -0,0 +1,48 @@
+import type { Ref } from 'vue'
+import type { Project } from '~/types'
+
+export const useProjectComponents = (project: Project) => {
+  const { $toast } = useNuxtApp()
+
+  const components: Ref<object[]> = useState(`project-${project.id}-components`, () => null)
+
+  const pending = ref(false)
+
+  // Http
+
+  async function fetch () {
+    if (!project.url) {
+      return
+    }
+
+    if (components.value !== null) {
+      return
+    }
+
+    pending.value = true
+
+    try {
+      components.value = await $fetch(`${project.url}/api/component-meta`, {
+        retry: false
+      })
+    } catch (e) {
+      components.value = []
+
+      $toast.error({
+        title: 'Could not load components',
+        description: 'Make sure to install @nuxthq/admin module in your Nuxt project.'
+      })
+    }
+
+    pending.value = false
+  }
+
+  return {
+    // Http
+    fetch,
+    // Refs
+    pending,
+    // Data
+    components
+  }
+}
