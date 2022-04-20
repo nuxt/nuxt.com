6dd02aca40d4b788eb04f0b1f8517cb893b738dd
Benjamin Canac <canacb1@gmail.com>
2022-04-20 15:54:22 +0200

    chore(deps): move to `nuxt` package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,4 @@
-import { defineNuxtConfig } from 'nuxt3'
+import { defineNuxtConfig } from 'nuxt'
 import preset from './presets'
 import colors from './presets/colors'
 import typography from './presets/typography'
