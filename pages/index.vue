c5befba263788e0d10ce5f0976fdf7d145d2dd28
Benjamin Canac <canacb1@gmail.com>
2022-04-26 14:09:29 +0200

    chore(index): init page with content

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,9 @@
 <template>
-  <Page>
-    <main class="px-4 mx-auto mt-16 sm:mt-24">
-      <div class="text-center">
-        <h1 class="text-4xl font-extrabold tracking-tight u-text-gray-900 sm:text-5xl md:text-6xl">
-          <span class="block xl:inline">A better way to create<br></span>
-          {{ ' ' }}
-          <span class="block text-green-400 xl:inline">modern websites</span>
-        </h1>
-        <p class="max-w-md mx-auto mt-3 text-base u-text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
-          Our suite of developer-friendly solutions helps you create,<br>secure and deliver enterprise-grade modern website.
-        </p>
-        <div class="max-w-md mx-auto mt-5 sm:flex sm:justify-center md:mt-8">
-          <UButton to="/docs/framework" label="Get started" size="xl" />
-        </div>
-      </div>
-    </main>
-  </Page>
+  <Content :document="page" />
 </template>
+
+<script setup lang="ts">
+const { page, fetchPage } = useContent()
+
+await fetchPage()
+</script>
