7592b2427a0686f48cfbec28186abd4ae94a74ad
Benjamin Canac <canacb1@gmail.com>
2022-04-26 14:08:34 +0200

    chore(community): init page with content

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -157,8 +157,7 @@ const links = computed(() => {
     icon: 'heroicons-outline:collection'
   }, {
     title: 'Community',
-    slug: team ? `/@${team}` : '/community',
-    exact: true,
+    slug: '/community',
     icon: 'heroicons-outline:globe'
   }]
 })
