e9eee9468ca679fd0ca9814345480c5eaec462ac
Kevin Marrec <kevin@marrec.io>
2022-04-27 11:07:31 +0200

    chore: define theme (#410)

---
+++ editor/theme/font.ts
@@ -0,0 +1,12 @@
+export const typography = [
+  'Roboto',
+  'HelveticaNeue-Light',
+  'Helvetica Neue Light',
+  'Helvetica Neue',
+  'Helvetica',
+  'Arial',
+  'Lucida Grande',
+  'sans-serif'
+]
+
+export const code = ['Consolas', 'Monaco', 'Andale Mono', 'Ubuntu Mono', 'monospace']
