e0dadd7875559cb4508c5fe5e3b0d928a7b77db9
Benjamin Canac <canacb1@gmail.com>
2022-04-28 15:27:49 +0200

    fix(project): combobox update with headlessui 1.6

--- components/project/ProjectCombobox.vue
+++ components/project/ProjectCombobox.vue
@@ -1,23 +1,25 @@
 <template>
-  <Combobox as="div" class="flex flex-col flex-1 min-h-0 divide-y u-divide-gray-100" @update:modelValue="onSelect">
-    <div class="relative">
-      <UIcon name="heroicons-outline:search" class="pointer-events-none absolute top-3.5 left-5 h-5 w-5 u-text-gray-400" aria-hidden="true" />
-      <ComboboxInput ref="comboboxInput" :value="query" class="w-full h-12 pr-4 placeholder-gray-400 dark:placeholder-gray-500 bg-transparent border-0 pl-[3.25rem] u-text-gray-900 focus:ring-0 sm:text-sm" placeholder="Search..." @change="query = $event.target.value" />
-    </div>
+  <Combobox @update:modelValue="onSelect">
+    <div class="flex flex-col flex-1 min-h-0 divide-y u-divide-gray-100">
+      <div class="relative">
+        <UIcon name="heroicons-outline:search" class="pointer-events-none absolute top-3.5 left-5 h-5 w-5 u-text-gray-400" aria-hidden="true" />
+        <ComboboxInput ref="comboboxInput" :value="query" class="w-full h-12 pr-4 placeholder-gray-400 dark:placeholder-gray-500 bg-transparent border-0 pl-[3.25rem] u-text-gray-900 focus:ring-0 sm:text-sm" placeholder="Search..." @change="query = $event.target.value" />
+      </div>
 
-    <ComboboxOptions v-if="hasOptions" static hold class="relative flex-1 overflow-y-auto divide-y u-divide-gray-100 scroll-py-2">
-      <ProjectComboboxOption v-if="recentItems.length && !query" type="recentItems" :items="recentItems" :label="recentItemsLabel" />
+      <ComboboxOptions v-if="hasOptions" static hold class="relative flex-1 overflow-y-auto divide-y u-divide-gray-100 scroll-py-2">
+        <ProjectComboboxOption v-if="recentItems.length && !query" type="recentItems" :items="recentItems" :label="recentItemsLabel" />
 
-      <ProjectComboboxOption v-if="filteredItems.length" type="items" :items="filteredItems" :label="itemsLabel" />
+        <ProjectComboboxOption v-if="filteredItems.length" type="items" :items="filteredItems" :label="itemsLabel" />
 
-      <ProjectComboboxOption v-if="filteredActions.length" type="actions" :items="filteredActions" :label="actionsLabel" />
-    </ComboboxOptions>
+        <ProjectComboboxOption v-if="filteredActions.length" type="actions" :items="filteredActions" :label="actionsLabel" />
+      </ComboboxOptions>
 
-    <div v-else class="py-14 px-6 flex-1 flex flex-col items-center justify-center sm:px-14">
-      <UIcon name="heroicons-outline:search" class="mx-auto h-6 w-6 text-gray-400" aria-hidden="true" />
-      <p class="mt-4 text-sm text-gray-900">
-        {{ query ? "We couldn't find any items with that term. Please try again." : "We couldn't find any items." }}
-      </p>
+      <div v-else class="py-14 px-6 flex-1 flex flex-col items-center justify-center sm:px-14">
+        <UIcon name="heroicons-outline:search" class="mx-auto h-6 w-6 text-gray-400" aria-hidden="true" />
+        <p class="mt-4 text-sm text-gray-900">
+          {{ query ? "We couldn't find any items with that term. Please try again." : "We couldn't find any items." }}
+        </p>
+      </div>
     </div>
   </Combobox>
 </template>
@@ -87,21 +89,13 @@ const filteredActions = computed(() => {
 
 const hasOptions = computed(() => (props.recentItems.length && !query) || filteredItems.value.length || filteredActions.value.length)
 
-// Watch
-
-watch(() => query.value, (value, oldValue) => {
-  if (value !== oldValue) {
-    activateFirstOption()
-  }
-})
-
 // Methods
 
 function activateFirstOption () {
   // hack combobox by using keyboard event
   // https://github.com/tailwindlabs/headlessui/blob/main/packages/%40headlessui-vue/src/components/combobox/combobox.ts#L692
   setTimeout(() => {
-    comboboxInput.value?.$el.dispatchEvent(new KeyboardEvent('keydown', { key: 'PageUp' }))
+    comboboxInput.value?.$el.dispatchEvent(new KeyboardEvent('keydown', { key: 'ArrowDown' }))
   }, 0)
 }
 
