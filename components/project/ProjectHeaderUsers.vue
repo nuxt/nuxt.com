063382667d2d099a23c84deff4a79a4065196d96
Benjamin Canac <canacb1@gmail.com>
2022-05-02 18:38:00 +0200

    chore(project): refactor composables so `file` / `branch` refs are strings (#440)

--- components/project/ProjectHeaderUsers.vue
+++ components/project/ProjectHeaderUsers.vue
@@ -16,7 +16,7 @@
           {{ activeUser.id === user.id ? '(you)' : '' }}
         </p>
         <div v-if="canJump(activeUser)">
-          <p v-if="activeUser.branch !== branch.name" class="u-text-gray-200 italic">
+          <p v-if="activeUser.branch !== branch" class="u-text-gray-200 italic">
             Jump to {{ activeUser.branch }} branch
           </p>
           <p v-else-if="!!activeUser.file" class="u-text-gray-200 italic">
@@ -40,11 +40,11 @@ const project: Project = inject('project')
 const activeUsers: Ref<SocketUser[]> = inject('activeUsers')
 
 const { branch, select: selectBranch } = useProjectBranches(project)
-const { file, select: selectContentFile, refresh: refreshContentFiles } = useProjectFiles(project, 'content')
+const { computedFile: contentFile, select: selectContentFile, refresh: refreshContentFiles } = useProjectFiles(project, 'content')
 const { refresh: refreshMediaFiles } = useProjectFiles(project, 'public')
 
 function canJump (activeUser) {
-  return activeUser.id !== user.value?.id && (activeUser.branch !== branch.value.name || !!activeUser.file)
+  return activeUser.id !== user.value?.id && (activeUser.branch !== branch.value || !!activeUser.file)
 }
 
 async function jumpTo (activeUser: SocketUser) {
@@ -54,15 +54,15 @@ async function jumpTo (activeUser: SocketUser) {
 
   const { file: f, branch: b } = activeUser
 
-  if (b !== branch.value?.name) {
-    selectBranch({ name: b })
+  if (b !== branch.value) {
+    selectBranch(b)
 
     await refreshContentFiles()
     refreshMediaFiles()
   }
 
-  if (file.value && f !== file.value.path) {
-    selectContentFile({ path: f, type: 'blob' })
+  if (contentFile.value && f !== contentFile.value.path) {
+    selectContentFile(f)
   }
 
   router.push({ name: '@team-project-content' })
