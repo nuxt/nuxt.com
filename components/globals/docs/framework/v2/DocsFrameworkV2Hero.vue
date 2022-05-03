b4f8c0e258e7610701d3174c8e3a00f397e6c2df
Florent Delerue <florentdelerue@hotmail.com>
2022-05-03 14:11:12 +0200

    feat(framework): landing page v2 (#430)
    Co-authored-by: Pooya Parsa <pyapar@gmail.com>

---
+++ components/globals/docs/framework/v2/DocsFrameworkV2Hero.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="not-prose relative">
+    <img src="/docs/framework/v2/gems-big.png" class="absolute opacity-20 lg:opacity-100 top-72 -left-14 h-1/2" alt="Gem illustration">
+    <img src="/docs/framework/v2/gems.png" class="absolute opacity-20 lg:opacity-100 top-0 right-0 h-1/5 top-48" alt="Gem illustration">
+    <div class="flex flex-col items-center justify-center pb-48 gap-y-8 pt-36 md:pt-44 lg:pb-56 lg:pt-36 relative">
+      <div v-if="$slots.badge" class="flex gap-x-2">
+        <UBadge rounded label="Warning" />
+        <span class="u-text-gray-500">
+          <slot name="badge" />
+        </span>
+      </div>
+      <h1 v-if="$slots.title" class="font-bold text-5xl sm:text-7xl text-center u-text-gray-900 max-w-lg">
+        <Markdown use="title" unwrap="p" />
+      </h1>
+      <p v-if="$slots.description" class="sm:w-1/2 text-md sm:text-lg text-center u-text-gray-500">
+        <Markdown use="description" unwrap="p" />
+      </p>
+      <div class="flex flex-col sm:flex-row gap-x-8">
+        <UButton
+          v-for="button of buttons"
+          :key="button.label"
+          class="flex justify-center"
+          :size="button.size || 'xl'"
+          :variant="button.variant || 'transparent'"
+          :icon="button.icon || undefined"
+          :label="button.label || ''"
+          :to="button.to || '#'"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { PropType } from 'vue'
+
+defineProps({
+  buttons: {
+    type: Array as PropType<{ label?: string, variant?: string, to?: string, size?: string, icon?: string }[]>,
+    default: () => []
+  }
+})
+</script>
---
+++ components/globals/docs/framework/v2/DocsFrameworkV2Need.vue
@@ -0,0 +1,29 @@
+<template>
+  <ul class="grid grid-cols-1 sm:grid-cols-2 gap-12 pt-12">
+    <li v-for="need in needData[0].needs" :key="need.title">
+      <NuxtLink :to="`/docs/framework/v2${need.to}`">
+        <UCard shadow-class="shadow-none hover:shadow-md hover:drop-shadow-md hover:shadow-inner-md transition-shadow duration-300" class="group p-2">
+          <div class="flex justify-between pb-4">
+            <div class="h-8 w-8 relative">
+              <img :src="`/docs/framework/v2/${need.icon}`" class="h-full w-full opacity-100 group-hover:opacity-0 transition-opacity duration-300 absolute top-0" :alt="`${need.title} illustration`">
+              <img :src="`/docs/framework/v2/${need.iconColor}`" class="h-full w-full opacity-0 group-hover:opacity-100 transition-opacity duration-300 absolute top-0" :alt="`${need.title} green illustration`">
+            </div>
+            <UIcon name="heroicons-solid:arrow-right" alt="arrow" class="mr-4 opacity-0 group-hover:translate-x-4 group-hover:opacity-100 h-6 w-6 transition duration-300" />
+          </div>
+          <h5 class="font-semibold text-2xl pb-4">
+            {{ need.title }}
+          </h5>
+          <p class="u-text-gray-500">
+            {{ need.description }}
+          </p>
+        </UCard>
+      </NuxtLink>
+    </li>
+  </ul>
+</template>
+
+<script setup lang="ts">
+
+const { data: needData } = await useAsyncData('need', () => queryContent('/docs/framework/v2/collections/need').find())
+
+</script>
