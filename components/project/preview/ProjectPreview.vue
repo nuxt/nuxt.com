303ad92fa161eebb06d543ff99b991bd7744fca3
Benjamin Canac <canacb1@gmail.com>
2022-05-11 16:44:26 +0200

    chore(project): dynamic scaling on preview

--- components/project/preview/ProjectPreview.vue
+++ components/project/preview/ProjectPreview.vue
@@ -2,26 +2,26 @@
   <div>
     <div v-show="isOpen" class="relative z-50">
       <Transition
-        enter-active-class="ease-out duration-300"
+        enter-active-class="duration-300 ease-out"
         enter-from-class="opacity-0"
         enter-to-class="opacity-100"
-        leave-active-class="ease-in duration-200"
+        leave-active-class="duration-200 ease-in"
         leave-from-class="opacity-100"
         leave-to-class="opacity-0"
       >
-        <div v-if="isExpand" class="fixed inset-0 bg-gray-500/75 dark:bg-gray-600/75 transition-opacity cursor-pointer" @click="isExpand = false" />
+        <div v-if="isExpand" class="fixed inset-0 transition-opacity cursor-pointer bg-gray-500/75 dark:bg-gray-600/75" @click="isExpand = false" />
       </Transition>
 
       <div
         ref="el"
-        class="fixed group ring-1 u-ring-gray-200 rounded-lg shadow-lg z-50 overflow-hidden"
+        class="fixed z-50 overflow-hidden rounded-lg shadow-lg group ring-1 u-bg-white"
         :class="{
-          '!left-16 !top-16 !w-auto !h-auto inset-16': isExpand,
-          'aspect-[16/9] select-none resize min-w-[160px] min-h-[90px] max-w-[640px] max-h-[360px]': !isExpand
+          '!left-16 !top-16 !w-auto !h-auto inset-16 ring-transparent': isExpand,
+          'aspect-[16/9] select-none resize min-w-[160px] min-h-[90px] max-w-[640px] max-h-[360px] u-ring-gray-200': !isExpand
         }"
         :style="style"
       >
-        <div v-if="!isExpand" class="absolute inset-0 group-hover:bg-gray-900/75 flex items-center justify-center dark z-50 cursor-move p-2">
+        <div v-if="!isExpand" class="absolute inset-0 z-50 flex items-center justify-center p-2 cursor-move group-hover:bg-gray-900/75 dark">
           <UButton
             v-show="isDiff"
             rounded
@@ -54,10 +54,8 @@
 
         <iframe
           id="iframe"
-          width="1920"
-          :class="{ 'collapsed': !isExpand }"
-          height="1080"
           :src="previewUrl"
+          :style="iframeStyle"
           class="w-full h-full"
         />
       </div>
@@ -71,14 +69,5 @@ import type { Project } from '~/types'
 const project: Project = inject('project')
 
 const { previewUrl } = useProjectFiles(project, 'content')
-const { el, style, isOpen, isExpand, isDiff, reset } = useProjectPreview()
+const { el, style, iframeStyle, isOpen, isExpand, isDiff, reset } = useProjectPreview()
 </script>
-
-<style scoped>
-iframe.collapsed {
-  transform: scale(0.25);
-  width: 400%;
-  height: 400%;
-  transform-origin: top left;
-}
-</style>
