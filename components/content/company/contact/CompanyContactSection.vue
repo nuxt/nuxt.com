08e6a55dddd41d5a731fdb98029007e34fb42647
Sylvain Marroufin <marroufin.sylvain@gmail.com>
2022-05-13 12:51:28 +0200

    feat(company): `support` / `contact` pages (#499)
    Co-authored-by: Benjamin Canac <canacb1@gmail.com>

---
+++ components/content/company/contact/CompanyContactSection.vue
@@ -0,0 +1,9 @@
+<template>
+  <UContainer padded class="pt-12 pb-36">
+    <img src="/company/contact/gradient.svg" class="absolute right-0 hidden lg:block">
+
+    <div class="relative z-[1] flex flex-col gap-12 lg:grid lg:grid-cols-2 lg:gap-20">
+      <Markdown :use="$slots.default" />
+    </div>
+  </UContainer>
+</template>
