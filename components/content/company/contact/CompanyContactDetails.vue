9efcd524dfe8a096cdefe7e12aaa9a5b5ef7e55a
Sylvain Marroufin <marroufin.sylvain@gmail.com>
2022-05-13 12:51:28 +0200

    feat(company): `support` / `contact` pages (#499)
    Co-authored-by: Benjamin Canac <canacb1@gmail.com>

---
+++ components/content/company/contact/CompanyContactDetails.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="space-y-8">
+    <div
+      v-for="(item, index) in items"
+      :key="index"
+      class="relative flex gap-4 p-5 border rounded-lg u-bg-gray-50 u-border-gray-200"
+    >
+      <div class="flex items-center justify-center w-12 h-12 border rounded-lg u-bg-white u-border-gray-200">
+        <img :src="item.icon" class="w-6 h-6">
+      </div>
+
+      <div class="flex flex-col flex-1 gap-2">
+        <span class="text-lg font-semibold u-text-gray-900">{{ item.title }}</span>
+        <span class="u-text-gray-700">{{ item.description }}</span>
+      </div>
+
+      <UIcon name="heroicons-outline:chevron-right" class="self-center w-5 h-5 u-text-gray-900" />
+
+      <NuxtLink :to="item.to" class="absolute inset-0" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { PropType } from 'vue'
+
+defineProps({
+  items: {
+    type: Array as PropType<Object>,
+    default: () => []
+  }
+})
+</script>
---
+++ components/content/company/support/CompanySupportCarousel.vue
@@ -0,0 +1,38 @@
+<template>
+  <UContainer padded class="pt-24 pb-36">
+    <Swiper
+      :modules="modules"
+      :slides-per-view="5"
+      :space-between="68"
+      :loop="true"
+      :autoplay="autoplay"
+    >
+      <SwiperSlide v-for="(item, index) in items" :key="index" class="!h-8">
+        <div class="relative flex items-center h-full">
+          <img :src="`/brands/${item.name}.png`" :alt="item.name">
+          <NuxtLink :to="item.to" target="_blank" class="absolute inset-0" rel="noopener noreferrer nofollow" />
+        </div>
+      </SwiperSlide>
+    </Swiper>
+  </UContainer>
+</template>
+
+<script setup lang="ts">
+import type { PropType } from 'vue'
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import { Autoplay } from 'swiper'
+
+// Import Swiper styles
+import 'swiper/css'
+
+defineProps({
+  items: {
+    type: Array as PropType<Object>,
+    default: () => []
+  }
+})
+
+const modules = [Autoplay]
+const autoplay = { delay: 4000, pauseOnMouseEnter: true, disableOnInteraction: false }
+</script>
