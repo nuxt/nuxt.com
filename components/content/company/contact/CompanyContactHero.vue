fcdd0448975e621a852c7632ea917de2ce906411
Sylvain Marroufin <marroufin.sylvain@gmail.com>
2022-05-13 10:53:35 +0200

    feat(company): add contact page

---
+++ components/content/company/contact/CompanyContactHero.vue
@@ -0,0 +1,37 @@
+<template>
+  <UContainer padded class="pb-12">
+    <div class="flex flex-col grid-cols-1 gap-8 md:grid sm:grid-cols-2 lg:grid-cols-3 md:h-[475px]">
+      <div class="relative md:absolute right-0 flex items-center justify-center lg:col-span-1 h-[475px] md:h-auto">
+        <img src="/company/contact/gems.svg">
+        <div class="h-[170px] absolute top-0 inset-x-0 bg-gradient-to-b from-white dark:from-black to-transparent" />
+        <div class="w-[170px] absolute right-0 inset-y-0 bg-gradient-to-l from-white dark:from-black to-transparent" />
+      </div>
+
+      <div class="relative flex flex-col justify-center py-8 space-y-5 lg:col-span-2">
+        <h1 class="text-5xl font-semibold u-text-gray-900">
+          <Markdown use="title" unwrap="p" />
+        </h1>
+        <p class="max-w-xl text-lg font-medium u-text-gray-500">
+          <Markdown use="description" unwrap="p" />
+        </p>
+
+        <div class="flex gap-8">
+          <UButton
+            v-for="(button, index) in buttons"
+            :key="index"
+            v-bind="button"
+          />
+        </div>
+      </div>
+    </div>
+  </UContainer>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  buttons: {
+    type: Array,
+    default: () => []
+  }
+})
+</script>
--- content/company/5.contact/index.md
+++ content/company/5.contact/index.md
@@ -1,3 +1,51 @@
 ---
 title: Contact
 ---
+
+::company-contact-hero
+---
+buttons:
+  -
+    label: GitHub Discussions
+    to: https://github.com/nuxt/framework/discussions
+    target: _blank
+  -
+    label: Follow us on
+    to: https://twitter.com/nuxt_js
+    target: _blank
+    variant: secondary
+    icon: fa-brands:twitter
+    trailing: true
+---
+#title
+How can we help you?
+#description
+Read the latest news about all Nuxt solutions, from framework announcements to integration tutorials.
+::
+
+::company-contact-section
+#content
+:::company-contact-details
+---
+items:
+  -
+    title: Documentation
+    icon: /company/contact/book.svg
+    description: Nuxt server renders your pages to bring your website the full visibility it deserves so.
+    to: /docs/framework
+  -
+    title: Partners
+    icon: /company/contact/nuxt.svg
+    description: Nuxt server renders your pages to bring your website the full visibility it deserves so.
+    to: /community/partners
+  -
+    title: Support
+    icon: /company/contact/support.svg
+    description: Nuxt server renders your pages to bring your website the full visibility it deserves so.
+    to: /company/support
+---
+:::
+
+:::company-contact-contact
+:::
+::
