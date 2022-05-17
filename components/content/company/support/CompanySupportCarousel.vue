06a821ad70feaa44a27bf91c188ce4fd5c920b65
Sylvain Marroufin <marroufin.sylvain@gmail.com>
2022-05-17 12:42:49 +0200

    up

--- components/content/company/support/CompanySupportCarousel.vue
+++ components/content/company/support/CompanySupportCarousel.vue
@@ -1,20 +1,18 @@
 <template>
   <UContainer padded class="pt-24 pb-36">
-    <ClientOnly>
-      <Swiper
-        :modules="modules"
-        :slides-per-view="slidesPerView"
-        :loop="true"
-        :autoplay="autoplay"
-      >
-        <SwiperSlide v-for="(item, index) in items" :key="index" class="!h-8">
-          <div class="relative flex items-center justify-center h-full">
-            <img :src="`/brands/${item.name}.png`" :alt="item.name">
-            <NuxtLink :to="item.to" target="_blank" class="absolute inset-0" rel="noopener noreferrer nofollow" />
-          </div>
-        </SwiperSlide>
-      </Swiper>
-    </ClientOnly>
+    <Swiper
+      :modules="modules"
+      :slides-per-view="slidesPerView"
+      :loop="true"
+      :autoplay="autoplay"
+    >
+      <SwiperSlide v-for="(item, index) in items" :key="index" class="!h-8">
+        <div class="relative flex items-center justify-center h-full">
+          <img :src="`/brands/${item.name}.png`" :alt="item.name">
+          <NuxtLink :to="item.to" target="_blank" class="absolute inset-0" rel="noopener noreferrer nofollow" />
+        </div>
+      </SwiperSlide>
+    </Swiper>
   </UContainer>
 </template>
 
@@ -23,6 +21,7 @@ import type { PropType } from 'vue'
 // Import Swiper Vue.js components
 import { Swiper, SwiperSlide } from 'swiper/vue'
 import { Autoplay } from 'swiper'
+import { breakpointsTailwind, useBreakpoints } from '@vueuse/core'
 
 // Import Swiper styles
 import 'swiper/css'
@@ -34,18 +33,21 @@ defineProps({
   }
 })
 
-const { $mq } = useNuxtApp()
+const { smaller } = useBreakpoints(breakpointsTailwind)
+
+const xs = smaller('sm')
+const sm = smaller('md')
+const md = smaller('lg')
 
 const slidesPerView = computed(() => {
-  switch ($mq.current) {
-    case 'xs':
-      return 1
-    case 'sm':
-      return 2
-    case 'md':
-      return 3
-    default:
-      return 5
+  if (xs.value) {
+    return 1
+  } else if (sm.value) {
+    return 2
+  } else if (md.value) {
+    return 3
+  } else {
+    return 5
   }
 })
 const modules = [Autoplay]
