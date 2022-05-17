450173809768f7053b085a99fdc8f2ef3bfeb195
Sylvain Marroufin <marroufin.sylvain@gmail.com>
2022-05-17 16:20:00 +0200

    feat(company): about page (#508)
    Co-authored-by: Benjamin Canac <canacb1@gmail.com>

---
+++ components/content/company/about/CompanyAboutMission.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="flex flex-col-reverse items-start pt-24 overflow-hidden xl:pb-24 xl:pb-36 xl:block xl:relative">
+    <div class="self-end translate-x-8 xl:translate-x-0 xl:absolute xl:-right-8 xl:top-24">
+      <img v-if="image" :src="image" class="relative">
+    </div>
+
+    <UContainer padded class="relative pb-24 mx-0 xl:mx-auto">
+      <div class="flex flex-col max-w-md gap-5">
+        <h2 class="text-xl font-semibold uppercase u-text-gray-300">
+          <Markdown use="title" unwrap="p" />
+        </h2>
+        <h3 class="text-4xl font-semibold u-text-gray-900">
+          <Markdown use="subtitle" unwrap="p" />
+        </h3>
+        <p class="text-2xl font-semibold u-text-gray-700">
+          <Markdown use="description" unwrap="p" />
+        </p>
+      </div>
+    </UContainer>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  image: {
+    type: String,
+    default: ''
+  }
+})
+</script>
