8b9d7ded82c02b2087ed4e52a417c9af3f42ae1e
Baptiste Leproux <leproux.baptiste@gmail.com>
2022-05-18 16:41:18 +0200

    feat(company): careers pages (#523)
    Co-authored-by: Benjamin Canac <canacb1@gmail.com>

--- components/content/resources/case-studies/ResourcesCaseStudiesList.vue
+++ components/content/resources/case-studies/ResourcesCaseStudiesList.vue
@@ -2,12 +2,12 @@
   <UContainer padded class="pb-16 sm:pb-32">
     <ul role="list" class="grid gap-4 sm:grid-cols-2 lg:grid-cols-3 sm:gap-6 md:gap-x-12 md:gap-y-10">
       <li v-for="data in caseStudiesData" :key="data.path" class="relative">
-        <div class="block w-full overflow-hidden rounded-lg ring-1 u-ring-gray-200 lg:hover:u-ring-gray-900 lg:hover:ring-2 group aspect-w-16 aspect-h-7 u-bg-gray-100">
+        <UCard shadow-class="" class="relative transition duration-200 aspect-w-16 aspect-h-7 group hover:ring-2 hover:u-ring-gray-900" body-class="">
           <img :src="data.imgUrl" :alt="data.path" class="object-contain object-center transition-all duration-300 bg-white pointer-events-none group-hover:scale-110">
           <NuxtLink :to="data.path" tabindex="-1" class="focus:outline-none">
             <span class="absolute inset-0" aria-hidden="true" />
           </NuxtLink>
-        </div>
+        </UCard>
       </li>
     </ul>
   </UContainer>
