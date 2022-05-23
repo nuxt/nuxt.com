c9084908363ff63d5abe4b4b58b1cf15ce309b73
Sylvain Marroufin <marroufin.sylvain@gmail.com>
2022-05-23 11:20:24 +0200

    refactor(content): convert slug pages as layout components

--- pages/company/careers/[slug]/index.vue
+++ pages/company/careers/[slug]/index.vue
@@ -1,41 +1,9 @@
 <template>
-  <UContainer constrained-class="max-w-4xl" padded>
-    <div class="flex flex-col items-center py-10 sm:py-20">
-      <NuxtLink to="/company/careers" class="flex items-center justify-center font-semibold u-text-gray-500 hover:u-text-gray-900">
-        <UIcon name="heroicons-outline:chevron-left" />
-        <span class="ml-3">Back to Careers</span>
-      </NuxtLink>
-      <h1 class="my-6 text-5xl font-semibold u-text-gray-900">
-        {{ page.role }}
-      </h1>
-      <ul class="flex items-center gap-6">
-        <li v-for="(info, index) in infos" :key="index" class="flex items-center u-text-gray-600">
-          <UIcon :name="info.icon" class="mr-2" />
-          <span> {{ info.label }}</span>
-        </li>
-      </ul>
-    </div>
-
-    <ContentRenderer class="prose dark:prose-invert max-w-none" :value="page" />
-
-    <CompanyCareersForm :offer="page.role" class="mt-24 mb-36" />
-  </UContainer>
+  <CompanyCareersOfferLayout :page="page" />
 </template>
 
 <script setup lang="ts">
 const { page, fetchPage } = useContent()
 
 await fetchPage()
-
-const infos = computed(() => [{
-  icon: 'uil:users-alt',
-  label: page.value.department
-}, {
-  icon: 'uil:globe',
-  label: page.value.location
-}, {
-  icon: 'uil:clock-ten',
-  label: page.value.frequency
-}])
-
 </script>
--- pages/resources/case-studies/[slug]/index.vue
+++ pages/resources/case-studies/[slug]/index.vue
@@ -1,33 +1,5 @@
 <template>
-  <div v-if="page" class="pb-16 sm:pb-32">
-    <div class="relative py-10 overflow-hidden sm:py-20">
-      <img :src="page.gradientUrl" alt="" class="absolute inset-x-0 bottom-0 z-0 w-full translate-y-1/4">
-
-      <UContainer padded constrained-class="max-w-4xl" class="z-[1] relative text-center flex flex-col items-center">
-        <h1 class="text-4xl font-semibold tracking-tight u-text-gray-900 sm:text-5xl">
-          {{ page.title }}
-        </h1>
-        <p class="mt-3 text-base u-text-gray-600 sm:text-lg md:mt-5">
-          {{ page.description }}
-        </p>
-        <NuxtLink :to="page.url" target="_blank" class="relative inline-flex items-center mt-3 text-lg font-semibold md:mt-5 group flex-nowrap max-w-max">
-          See website
-          <span
-            class="rounded absolute u-bg-gray-700 left-0 font-semibold -bottom-1 h-0.5 w-0 group-hover:w-full transition-all"
-          />
-          <UIcon name="heroicons-outline:arrow-sm-right" class="w-5 h-5 ml-1 mt-0.5" />
-        </NuxtLink>
-      </UContainer>
-    </div>
-
-    <ContentRenderer :value="page" />
-
-    <UContainer constrained-class="max-w-4xl">
-      <ResourcesCaseStudyCta />
-
-      <ResourcesCaseStudyPrevNext :prev="prev" :next="next" />
-    </UContainer>
-  </div>
+  <ResourcesCaseStudiesPostLayout :page="page" :prev="prev" :next="next" />
 </template>
 
 <script setup lang="ts">
