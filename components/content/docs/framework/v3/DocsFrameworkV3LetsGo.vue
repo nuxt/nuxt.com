edbaf4fdd43c0d7f6cdccb5c49a0b176c1e16932
Florent <florentdelerue@hotmail.com>
2022-05-24 16:37:26 +0200

    feat(docsframeworkv3): lets go section

---
+++ components/content/docs/framework/v3/DocsFrameworkV3LetsGo.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex gap-x-8">
+    <UButton
+      v-for="button of buttons"
+      :key="button.label"
+      :size="button.size || 'xl'"
+      :variant="button.variant || 'transparent'"
+      :icon="button.icon || undefined"
+      :label="button.label || ''"
+      :to="button.to || '#'"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  buttons: {
+    type: Array as PropType<{ label?: string, variant?: string, to?: string, size?: string, icon?: string }[]>,
+    default: () => []
+  }
+})
+</script>
--- components/content/docs/framework/v3/DocsFrameworkV3Section.vue
+++ components/content/docs/framework/v3/DocsFrameworkV3Section.vue
@@ -1,11 +1,20 @@
 <template>
-  <div class="flex flex-col py-20 gap-y-8 not-prose">
+  <div class="flex flex-col py-20 gap-y-8 not-prose" :class="{ 'items-center': center }">
     <h1 class="text-4xl font-semibold text-left u-text-gray-900">
       <Markdown use="title" unwrap="p" />
     </h1>
-    <p class="w-full text-lg text-left lg:w-1/2 u-text-gray-500">
+    <p class="w-full text-lg text-left lg:w-1/2 u-text-gray-500" :class="center ? 'text-center' : 'text-left'">
       <Markdown use="description" unwrap="p" />
     </p>
     <Markdown use="content" unwrap="p" />
   </div>
 </template>
+
+<script setup lang="ts">
+defineProps({
+  center: {
+    type: Boolean,
+    default: false
+  }
+})
+</script>
