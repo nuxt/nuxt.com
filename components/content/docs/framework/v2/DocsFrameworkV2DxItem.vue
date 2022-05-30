dd70c4fb5252a196ba9049c5356d612e3e680ffe
Florent Delerue <florentdelerue@hotmail.com>
2022-05-30 12:29:55 +0200

    fix(docs): replace `v2` `_collections` with slots and components (#574)

---
+++ components/content/docs/framework/v2/DocsFrameworkV2DxItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <li class="flex flex-col items-center gap-y-4">
+    <div class="relative">
+      <img src="/assets/docs/framework/v2/hexagon.svg" alt="hexagon background">
+      <div class="absolute top-0 flex items-center justify-center w-full h-full">
+        <img :src="`/assets/docs/framework/v2/${icon}`" class="w-8 h-8" :alt="`${icon} illustration`">
+      </div>
+    </div>
+    <h6 class="text-lg font-semibold text-center u-text-gray-900">
+      <Markdown :use="$slots.title" unwrap="p" />
+    </h6>
+    <p class="text-center u-text-gray-400">
+      <Markdown :use="$slots.description" unwrap="p" />
+    </p>
+  </li>
+</template>
+<script setup lang="ts">
+defineProps({
+  icon: {
+    type: String,
+    default: ''
+  }
+})
+</script>
---
+++ components/content/docs/framework/v2/DocsFrameworkV2DxList.vue
@@ -0,0 +1,5 @@
+<template>
+  <ul class="grid grid-cols-1 gap-8 pt-12 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+    <Markdown :use="$slots.list" unwrap="p" />
+  </ul>
+</template>
