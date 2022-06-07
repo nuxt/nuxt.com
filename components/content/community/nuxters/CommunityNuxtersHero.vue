7a101caae153a8fde250873dc7b361e1186caa27
Sylvain Marroufin <marroufin.sylvain@gmail.com>
2022-06-07 12:41:32 +0200

    feat: update hero picture (WIP)

--- components/content/community/nuxters/CommunityNuxtersHero.vue
+++ components/content/community/nuxters/CommunityNuxtersHero.vue
@@ -18,7 +18,7 @@
       </div>
     </template>
     <template #background>
-      <img src="/assets/resources/live-events/hero.svg" alt="World map" class="object-cover h-full opacity-30">
+      <CommunityNuxtersHeroMap class="object-cover h-full" />
       <div class="h-[100px] absolute top-0 inset-x-0 bg-gradient-to-b from-white dark:from-black to-transparent" />
       <div class="w-[100px] absolute left-0 inset-y-0 bg-gradient-to-r from-white dark:from-black to-transparent" />
       <div class="h-[100px] absolute bottom-0 inset-x-0 bg-gradient-to-t from-white dark:from-black to-transparent" />
