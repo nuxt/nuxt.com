49961a2f3be9699fda37809e370b8727ad378efa
Sylvain Marroufin <marroufin.sylvain@gmail.com>
2022-06-07 12:03:33 +0200

    feat: update small nuxter responsiveness

--- components/content/community/nuxters/CommunityNuxtersNuxterSmall.vue
+++ components/content/community/nuxters/CommunityNuxtersNuxterSmall.vue
@@ -1,15 +1,13 @@
 <template>
-  <li class="relative flex items-center justify-between h-32 col-span-6 overflow-hidden border md:col-span-3 xl:col-span-2 u-border-gray-200 rounded-xl group">
+  <li class="relative flex items-center justify-between h-32 col-span-6 gap-1 overflow-hidden border md:col-span-3 xl:col-span-2 u-border-gray-200 rounded-xl group">
     <div class="flex items-center h-full">
       <img :src="nuxter.avatar_url" :alt="nuxter.github" class="w-16 h-16 mx-2 transition-all rounded-full grayscale group-hover:grayscale-0">
-      <div class="flex flex-col justify-between h-full py-5">
+      <div class="flex flex-col justify-between h-full py-5 truncate">
         <div>
           <h3 class="text-lg font-semibold leading-none u-text-gray-900 line-clamp-1">
             @{{ nuxter.github }}
           </h3>
-          <div class="flex items-center gap-3 mt-1">
-            <span class="font-medium u-text-gray-500 line-clamp-1">{{ nuxter.name }}</span>
-          </div>
+          <span class="mt-1 font-medium u-text-gray-500 line-clamp-1">{{ nuxter.name }}</span>
         </div>
         <div class="flex items-center gap-3">
           <CommunityNuxtersNuxterBadge :role="nuxter.role" />
