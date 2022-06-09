963abf26e205f618a98e4cfae08b6b11bb37ed27
Florent <florentdelerue@hotmail.com>
2022-06-09 16:23:08 +0200

    refactor(communityabouttestimonialslist): improve opacity for responsive

--- components/content/community/about/CommunityAboutTestimonialsList.vue
+++ components/content/community/about/CommunityAboutTestimonialsList.vue
@@ -1,13 +1,17 @@
 <template>
-  <div class="relative">
-    <ul class="grid grid-cols-1 overflow-hidden list-none gap-y-8 gap-x-36 sm:grid-cols-2 lg:grid-cols-3" :class="show ? 'h-full' : 'h-[320px]'">
-      <Markdown :use="$slots.items" unwrap="p" />
-    </ul>
+  <div class="flex flex-col">
+    <div class="relative">
+      <ul class="z-[-1] grid grid-cols-1 overflow-hidden list-none gap-y-8 gap-x-36 sm:grid-cols-2 lg:grid-cols-3" :class="show ? 'h-full' : 'h-[320px]'">
+        <Markdown :use="$slots.items" unwrap="p" />
+      </ul>
 
-    <div class="absolute inset-x-0 bottom-0 flex justify-center pb-8 pointer-events-none bg-gradient-to-t from-white pt-52 dark:from-black" :class="show ? 'opacity-0' : 'opacity-100'" />
-
-    <div class="flex justify-center w-full" :class="show ? 'pt-12' : 'pt-8'">
-      <UButton variant="secondary" size="lg" class="relative flex justify-center w-40 u-border-gray-900" @click="show = !show">
+      <div
+        class="absolute inset-x-0 bottom-0 flex justify-center pt-16 pb-8 pointer-events-none bg-gradient-to-t from-white dark:from-black"
+        :class="show ? 'opacity-0' : 'opacity-100'"
+      />
+    </div>
+    <div class="flex justify-center">
+      <UButton variant="secondary" size="lg" class="flex justify-center w-40 mt-12 u-border-gray-900" @click="show = !show">
         <span class="px-4 py-1 text-center">
           {{ show ? hideText : showText }}
         </span>
