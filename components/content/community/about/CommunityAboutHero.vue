c120a2cafcf8014c967b35c922ae2b674642bed0
Florent <florentdelerue@hotmail.com>
2022-06-09 13:56:53 +0200

    refactor(communityabouthero): remove unused code

--- components/content/community/about/CommunityAboutHero.vue
+++ components/content/community/about/CommunityAboutHero.vue
@@ -52,30 +52,6 @@
 
 <script setup lang="ts">
 const colorMode = useColorMode()
-
-const { $toast } = useNuxtApp()
-
-const form = reactive({
-  email: ''
-})
-
-const loading = ref(false)
-
-async function onSubmit () {
-  loading.value = true
-
-  // TODO: handle already subscribed case
-  // FIXME: cannot retry call (uses fetch caching) (waiting for module update)
-  const { error } = await useNewsletterSubscribe(form.email)
-
-  if (!error.value) {
-    $toast.success({ title: 'Subscription succeed', description: 'You have been successfully subscribed to Nuxt newsletter. Please check your emails to confirm your subscription.' })
-  } else {
-    $toast.error({ title: 'Subscription failed', description: 'Something went wrong. Please try again later.' })
-  }
-
-  loading.value = false
-}
 </script>
 
 <style scoped>
