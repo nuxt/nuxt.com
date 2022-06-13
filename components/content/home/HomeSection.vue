c356ee695c7946afa0d3855f32884d8c0f812a6e
Florent <florentdelerue@hotmail.com>
2022-06-13 17:35:33 +0200

    feat(home): sections (wip)

--- components/content/home/HomeSection.vue
+++ components/content/home/HomeSection.vue
@@ -1,36 +1,41 @@
 <template>
-  <UContainer class="grid items-center justify-center grid-cols-1 py-24 sm:grid-cols-2">
-    <div :class="sectionTitleColorClass" class="pb-2">
+  <UContainer :constrained="false" class="relative flex flex-col items-center justify-center py-24">
+    <div :class="sectionTitleColorClass" class="pb-2 font-semibold">
       <Markdown :use="$slots.sectionTitle" unwrap="p" />
     </div>
-    <h2 class="pb-24 text-6xl font-semibold u-text-gray-900">
+    <h2 class="max-w-[52rem] pb-24 text-6xl font-semibold text-center ">
       <Markdown :use="$slots.title" unwrap="p" />
     </h2>
-    <p v-if="$slots.description" class="text-2xl u-text-gray-600">
+    <p v-if="$slots.description" class="text-2xl text-center u-text-gray-600">
       <Markdown :use="$slots.description" unwrap="p" />
     </p>
     <div
       class="items-center justify-center"
       :class="[
-        { 'grid grid-cols-1 sm:grid-cols-2 gap-x-[180px]': ['left', 'right'].includes(bodyPlacement) },
+        { 'grid grid-cols-1 lg:grid-cols-2 gap-x-[180px] items-center justify-center gap-y-8': ['left', 'right'].includes(bodyPlacement) },
       ]"
     >
-      <div v-if="withBodyText" class="flex flex-col text-center sm:text-left gap-y-6" :class="{ 'order-last': bodyPlacement === 'right' }">
-        <div v-if="$slots.bodyTitle" class="font-semibold u-text-gray-900">
+      <div v-if="withBodyText" padded class="flex flex-col max-w-xl px-4 text-left gap-y-6 sm:px-0" :class="[{ 'order-last': bodyPlacement === 'left' }]">
+        <div v-if="$slots.bodyTitle" class="text-4xl font-semibold u-text-gray-900">
           <Markdown :use="$slots.bodyTitle" unwrap="p" />
         </div>
-        <div v-if="$slots.bodyDescription" class="text-2xl u-text-gray-600">
+        <div v-if="$slots.bodyDescription" class="items-center text-2xl u-text-gray-600">
           <Markdown :use="$slots.bodyDescription" unwrap="p" />
         </div>
-        <NuxtLink v-if="$slots.bodyLink" :to="to" class="flex gap-x-4">
-          <span class="w-4 h-4 text-2xl u-text-gray-900">
+        <NuxtLink v-if="$slots.bodyLink" :to="to" class="flex items-center w-full font-semibold gap-x-4">
+          <span class="text-2xl u-text-gray-900">
             <Markdown :use="$slots.bodyLink" unwrap="p" />
           </span>
-          <UIcon name="heroicons-solid:arrow-right" class="w-4 h-4 text-2xl u-text-gray-900" />
+          <UIcon name="heroicons-solid:arrow-right" class="w-5 h-5 u-text-gray-900" />
         </NuxtLink>
+        <UButton v-if="$slots.bodyButton" :to="to" variant="secondary" size="lg" class="flex justify-center w-[224px]">
+          <Markdown :use="$slots.bodyButton" unwrap="p" />
+        </UButton>
       </div>
-      <img v-if="withImage" :src="image" :alt="`${image} illustration`" :class="imageClass">
-      <Markdown :use="$slots.default" unwrap="p" />
+
+      <img v-if="image" :src="`/assets/home/${image}`" :alt="`${image} illustration`" :class="imageClass" class="px-4 sm:px-0">
+
+      <!-- Markdown :use="$slots.default" unwrap="p" /-->
     </div>
   </UContainer>
 </template>
@@ -57,10 +62,6 @@ defineProps({
     default: ''
   },
   withBodyText: {
-    type: Boolean,
-    default: true
-  },
-  withImage: {
     type: Boolean,
     default: true
   },
--- content/index.md
+++ content/index.md
@@ -1,11 +1,68 @@
 ::home-hero
+---
+buttons:
+  - label: 'Watch video'
+    size: 'xl'
+    variant: 'green'
+    to: '/docs/framework/v3/getting-started/quick-start'
+  - label: 'Get Started'
+    size: 'xl'
+    variant: 'secondary'
+    to: '/docs/framework/v3/getting-started/quick-start'
+    target: '_blank'
+    icon: 'heroicons-solid:chevron-right'
+    trailing: true
+---
+
+#badgeLabel
+New
+
+#news
+Nuxt 3.0 is out. [Read about it here](https://v3.nuxtjs.org/){.underline}
 
 #title
-
-The Hybrid Vue Framework
+Develop a modern website today.
 
 #description
-
-Build your next application with Vue 3 and experience hybrid rendering, powerful data fetching and new features. Nuxt 3 is an open source framework making web development simple and powerful.
+Nuxt provides the best developer experience for web teams to deliver the best end-user experience.
 ::
 
+::home-section
+---
+bodyPlacement: 'left'
+withImage: true
+image: 'place-holder.png'
+to: 'https://v3.nuxtjs.org/'
+imageClass: 'w-full lg:min-w-lg lg:-translate-x-8'
+---
+#sectionTitle
+START WITH
+#title
+The Intuitive Web Framework for Vue developers
+#bodyTitle
+The [Hybrid]{ .text-green-400 } Web Framework
+#bodyDescription
+[Focus on the big picture.]{ .font-semibold } Projects lets you group issues and get a birds-eye view of the progress your team is making.
+#bodyLink
+Go to the documentation
+::
+
+::home-section
+---
+bodyPlacement: 'right'
+withImage: true
+image: 'modules.png'
+to: '/modules'
+imageClass: 'absolute lg:max-w-xl right-0 top-44'
+---
+#sectionTitle
+Scale with
+#title
+Powerful Modules
+#bodyTitle
+Build your dreamed web app with [hundreds of modules]{ .text-green-400 }
+#bodyDescription
+[Focus on the big picture.]{ .font-semibold } Projects lets you group issues and get a birds-eye view of the progress your team is making.
+#bodyLink
+List of modules
+::
