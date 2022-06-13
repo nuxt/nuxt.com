4076cf2de309c24d8dd50786c362834c85273a50
Florent <florentdelerue@hotmail.com>
2022-06-13 17:35:33 +0200

    feat(home): sections (wip)

--- components/content/home/HomeHero.vue
+++ components/content/home/HomeHero.vue
@@ -1,31 +1,37 @@
 <template>
-  <div class="not-prose">
-    <div class="flex flex-col items-center justify-center pb-48 gap-y-8 pt-36 md:pt-44 lg:pb-56 lg:pt-36 ">
-      <div v-if="$slots.badge" class="flex gap-x-2">
-        <UBadge rounded label="Warning" />
-        <span class="u-text-gray-500">
-          <slot name="badge" />
-        </span>
+  <UContainer padded class="relative not-prose">
+    <div class="flex flex-row items-center justify-between pb-20 pt-36 md:pt-44 lg:pt-36">
+      <div class="flex flex-col justify-start gap-y-8">
+        <div v-if="$slots.badgeLabel" class="flex gap-x-2">
+          <UBadge rounded variant="green">
+            <Markdown :use="$slots.badgeLabel" unwrap="p" />
+          </UBadge>
+          <span class="u-text-gray-800">
+            <Markdown :use="$slots.news" unwrap="p" />
+          </span>
+        </div>
+        <h1 v-if="$slots.title" class="max-w-2xl font-bold text-left text-7xl u-text-gray-900">
+          <Markdown use="title" unwrap="p" />
+        </h1>
+        <p v-if="$slots.description" class="w-3/5 text-lg text-left u-text-gray-800">
+          <Markdown use="description" unwrap="p" />
+        </p>
+        <div class="flex gap-x-8">
+          <UButton
+            v-for="button of buttons"
+            :key="button.label"
+            :size="button.size || 'xl'"
+            :variant="button.variant || 'transparent'"
+            :icon="button.icon || undefined"
+            :label="button.label || ''"
+            :to="button.to || '#'"
+            :trailing="button.trailing"
+          />
+        </div>
       </div>
-      <h1 v-if="$slots.title" class="max-w-lg font-semibold text-center text-7xl u-text-gray-900">
-        <Markdown :use="$slots.title" unwrap="p" />
-      </h1>
-      <p v-if="$slots.description" class="w-1/2 text-lg text-center u-text-gray-500">
-        <Markdown :use="$slots.description" unwrap="p" />
-      </p>
-      <div class="flex gap-x-8">
-        <UButton
-          v-for="button of buttons"
-          :key="button.label"
-          :size="button.size || 'xl'"
-          :variant="button.variant || 'transparent'"
-          :icon="button.icon || undefined"
-          :label="button.label || ''"
-          :to="button.to || '#'"
-        />
-      </div>
+      <img src="/assets/home/hero-gradient.svg" class="absolute -rotate-[40deg] -top-[200px] -right-[300px] opacity-60 overflow-hidden">
     </div>
-  </div>
+  </UContainer>
 </template>
 
 <script setup lang="ts">
@@ -33,7 +39,7 @@ import { PropType } from 'vue'
 
 defineProps({
   buttons: {
-    type: Array as PropType<{ label?: string, variant?: string, to?: string, size?: string, icon?: string }[]>,
+    type: Array as PropType<{ label?: string, variant?: string, to?: string, size?: string, icon?: string, trailing?: Boolean }[]>,
     default: () => []
   }
 })
