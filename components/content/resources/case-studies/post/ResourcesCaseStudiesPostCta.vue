fc861b90675714b944b883679e9c7726d5b0b0bd
Benjamin Canac <canacb1@gmail.com>
2022-06-14 14:34:14 +0200

    fix(resources): improve case studies design

--- components/content/resources/case-studies/post/ResourcesCaseStudiesPostCta.vue
+++ components/content/resources/case-studies/post/ResourcesCaseStudiesPostCta.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="py-10">
-    <UCard class="relative u-bg-black" body-class="p-12">
+    <UCard padded class="relative u-bg-black" body-class="p-6 sm:p-12">
       <span class="z-0 _ellipse" />
       <div class="flex flex-wrap items-center justify-between">
         <h2 class="text-white z-[1] tracking-tight">
-          <span class="block text-3xl font-semibold">Contact us</span>
-          <span class="block mt-2 text-lg">Start building, or get in touch with our app experts.</span>
+          <span class="block text-2xl font-semibold sm:text-3xl">Contact us</span>
+          <span class="block mt-2 sm:text-lg">Start building, or get in touch with our app experts.</span>
         </h2>
         <div class="mt-8 flex lg:mt-0 lg:flex-shrink-0 gap-3 z-[1] relative dark">
           <UButton variant="secondary" label="Get in touch" size="lg" class="flex justify-center w-40" />
