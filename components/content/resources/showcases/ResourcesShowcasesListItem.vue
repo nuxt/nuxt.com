40e9d763f102a388362756449419dbeca8bd5c96
Benjamin Canac <canacb1@gmail.com>
2022-06-14 12:45:50 +0200

    fix(resources): improve showcases cards

--- components/content/resources/showcases/ResourcesShowcasesListItem.vue
+++ components/content/resources/showcases/ResourcesShowcasesListItem.vue
@@ -1,30 +1,29 @@
 <template>
-  <NuxtLink :to="showcase.url" target="_blank" class="block overflow-hidden border group rounded-xl u-border-gray-200">
-    <div class="aspect-w-4 aspect-h-3">
-      <img
-        :src="`https://res.cloudinary.com/nuxt/image/upload/f_auto,q_auto/${showcase.screenshotUrl}`"
-        :alt="showcase.hostname"
-        width="800"
-        height="600"
-        loading="lazy"
-        class="absolute inset-0 object-cover"
-      >
-      <div class="absolute inset-0">
-        <div class="absolute bottom-0 inset-x-0 h-[50%] hidden group-hover:block" :style="{ background: 'linear-gradient(#00000000, #00000055)' }" />
-        <div class="absolute inset-x-0 bottom-0 px-4 py-3 u-bg-gray-50 group-hover:bg-transparent">
-          <div class="flex items-center justify-between gap-1">
-            <h2 class="font-semibold truncate u-text-gray-900 group-hover:text-white">
-              {{ showcase.title }}
-            </h2>
-            <UIcon name="heroicons-outline:external-link" class="flex-shrink-0 hidden w-5 h-5 text-white group-hover:inline" />
-          </div>
-          <p class="text-sm truncate group-hover:hidden u-text-gray-500">
-            {{ showcase.hostname }}
-          </p>
-        </div>
+  <UCard padded body-class="aspect-w-4 aspect-h-3" shadow-class="" class="relative transition duration-200 group hover:ring-2 hover:u-ring-gray-900">
+    <img
+      :src="`https://res.cloudinary.com/nuxt/image/upload/f_auto,q_auto/${showcase.screenshotUrl}`"
+      :alt="showcase.hostname"
+      width="800"
+      height="600"
+      loading="lazy"
+      class="absolute inset-0 object-cover"
+    >
+
+    <div class="absolute inset-0">
+      <div class="absolute inset-x-0 bottom-0 px-4 py-3 u-bg-gray-50">
+        <h2 class="font-semibold truncate u-text-gray-900">
+          {{ showcase.title }}
+        </h2>
+        <p class="text-sm truncate u-text-gray-500">
+          {{ showcase.hostname }}
+        </p>
       </div>
     </div>
-  </NuxtLink>
+
+    <NuxtLink :to="showcase.url" target="_blank" class="focus:outline-none" tabindex="-1">
+      <span class="absolute inset-0" aria-hidden="true" />
+    </NuxtLink>
+  </UCard>
 </template>
 
 <script setup lang="ts">
