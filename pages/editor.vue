0c34e27de2238dbdbc9a9a56bc5e7852199351ce
Kevin Marrec <kevin@marrec.io>
2022-06-15 15:24:45 +0200

    chore: tmp editor page

---
+++ pages/editor.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <ProjectContentFileEditor :content="content" />
+  </div>
+</template>
+
+<script setup>
+const content = {
+  key: '',
+  markdown: '```vue\n<template>\nTest\n</template>\n```',
+  matter: {}
+}
+</script>
