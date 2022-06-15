0dbc502e152a38f7897df08926388ff8f392c893
Benjamin Canac <canacb1@gmail.com>
2022-06-15 14:32:22 +0200

    up

--- components/content/community/about/CommunityAboutJoin.vue
+++ components/content/community/about/CommunityAboutJoin.vue
@@ -1,6 +1,6 @@
 <template>
   <UContainer :constrained="false" class="flex flex-col items-center justify-center py-32 bg-black gap-y-12 dark:bg-gray-900">
-    <h2 class="text-4xl font-semibold text-center text-white leading-[140%]">
+    <h2 class="text-4xl font-semibold text-center text-white">
       <Markdown :use="$slots.title" unwrap="p" />
     </h2>
     <ul class="grid items-center justify-center grid-cols-2 sm:grid-cols-3 xl:grid-cols-5 place-items-center gap-x-8 sm:gap-x-16">
--- components/content/community/about/CommunityAboutTestimonials.vue
+++ components/content/community/about/CommunityAboutTestimonials.vue
@@ -1,9 +1,9 @@
 <template>
-  <UContainer padded class="flex flex-col items-center justify-center py-20 gap-y-8">
-    <h4 class="text-[24px] font-semibold u-text-gray-900 leading-[140%]">
+  <UContainer padded class="flex flex-col items-center justify-center py-20">
+    <h4 class="mb-2 text-2xl font-semibold u-text-gray-900">
       <Markdown :use="$slots.title" unwrap="p" />
     </h4>
-    <p class="text-center u-text-gray-600">
+    <p class="mb-8 text-center u-text-gray-600">
       <Markdown :use="$slots.description" unwrap="p" />
     </p>
     <Markdown :use="$slots.list" unwrap="p" />
