1bcbe26e49e6f09c597acb44e51beb415656242e
Florent <florentdelerue@hotmail.com>
2022-06-15 12:05:14 +0200

    refactor(communiyabout): update titles leading

--- components/content/community/about/CommunityAboutStats.vue
+++ components/content/community/about/CommunityAboutStats.vue
@@ -1,6 +1,6 @@
 <template>
   <UContainer padded class="flex flex-col items-center justify-center py-20 text-center gap-y-12 u-bg-white">
-    <h2 class="text-4xl font-semibold u-text-gray-900">
+    <h2 class="text-4xl font-semibold u-text-gray-900 leading-[140%]">
       <Markdown :use="$slots.title" unwrap="p" />
     </h2>
     <p class="w-1/2 text-center xl:w-[40%] u-text-gray-600">
