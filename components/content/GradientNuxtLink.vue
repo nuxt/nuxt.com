31c137603589ec940a25a077d260fcf3b51da1eb
Sylvain Marroufin <marroufin.sylvain@gmail.com>
2022-06-17 18:29:14 +0200

    feat(pages): make gradient link a component + anim improvement

---
+++ components/content/GradientNuxtLink.vue
@@ -0,0 +1,18 @@
+<template>
+  <NuxtLink class="relative inline-flex items-center group flex-nowrap max-w-max">
+    <slot />
+    <span
+      class="rounded absolute left-0 font-semibold -bottom-1 h-0.5 w-4 group-hover:w-full duration-300 transition-ease bg-gradient-to-r from-green-400 via-teal-400 to-indigoblue-600"
+    />
+    <UIcon v-if="icon" :name="icon" class="w-5 h-5 ml-1 mt-0.5" />
+  </NuxtLink>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  icon: {
+    type: String,
+    default: ''
+  }
+})
+</script>
--- components/content/resources/live-events/ResourcesLiveEventsHero.vue
+++ components/content/resources/live-events/ResourcesLiveEventsHero.vue
@@ -8,15 +8,9 @@
     </template>
     <template #extra>
       <!-- TODO: update link -->
-      <a class="relative inline-flex items-center flex-nowrap u-text-gray-900 sm:text-lg group max-w-max" href="#">
-        <span class="flex items-center font-semibold gap-x-2">
-          <Markdown :use="$slots.link" unwrap="p" />
-          <UIcon name="heroicons-outline:arrow-sm-right" class="w-5 h-5" />
-        </span>
-        <span
-          class="rounded absolute left-0 font-semibold -bottom-1 h-0.5 w-4 group-hover:w-full duration-300 transition-ease bg-gradient-to-r from-green-400 via-teal-400 to-indigoblue-600"
-        />
-      </a>
+      <GradientNuxtLink icon="heroicons-outline:arrow-sm-right" to="#" class="font-semibold u-text-gray-900 sm:text-lg">
+        <Markdown :use="$slots.link" unwrap="p" />
+      </GradientNuxtLink>
     </template>
     <template #background>
       <img src="/assets/resources/live-events/hero.svg" alt="World map" class="object-cover h-full opacity-30">
