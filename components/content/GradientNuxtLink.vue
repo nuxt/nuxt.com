624766e31bc0b8e804748b94b218be95e615e490
Sylvain Marroufin <marroufin.sylvain@gmail.com>
2022-06-17 18:42:53 +0200

    improve gradient anim

--- components/content/GradientNuxtLink.vue
+++ components/content/GradientNuxtLink.vue
@@ -1,8 +1,8 @@
 <template>
-  <NuxtLink class="relative inline-flex items-center group flex-nowrap max-w-max">
+  <NuxtLink class="relative inline-flex items-center pb-1 overflow-hidden group flex-nowrap max-w-max">
     <slot />
     <span
-      class="rounded absolute left-0 font-semibold -bottom-1 h-0.5 w-4 group-hover:w-full duration-300 transition-ease bg-gradient-to-r from-green-400 via-teal-400 to-indigoblue-600"
+      class="rounded absolute -left-[calc(100%-16px)] group-hover:left-0 font-semibold bottom-0 h-0.5 w-full duration-300 transition-ease bg-gradient-to-r from-green-400 via-teal-400 to-indigoblue-600"
     />
     <UIcon v-if="icon" :name="icon" class="w-5 h-5 ml-1 mt-0.5" />
   </NuxtLink>
