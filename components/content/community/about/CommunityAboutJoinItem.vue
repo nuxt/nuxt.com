6e431edd9dc3af81eb8d932d3fe9cf351e54dca7
Florent Delerue <florentdelerue@hotmail.com>
2022-06-20 18:05:15 +0200

    chore(community): about page improvements (#680)

--- components/content/community/about/CommunityAboutJoinItem.vue
+++ components/content/community/about/CommunityAboutJoinItem.vue
@@ -1,6 +1,12 @@
 <template>
-  <li>
-    <a :href="href" class="flex flex-col w-[120px] h-[120px] sm:w-[148px] sm:h-[146px] items-center justify-center py-6 transition duration-300 rounded-md gap-y-2 hover:bg-gray-800 px-1">
+  <li
+    class="flex w-full xl:justify-center"
+    :class="itemClass"
+  >
+    <a
+      :href="href"
+      class="flex flex-col w-[120px] h-[120px] sm:w-[160px] sm:h-[160px] items-center justify-center py-6 transition duration-300 rounded-md gap-y-2 hover:bg-gray-800 px-1"
+    >
       <UIcon :name="`fa6-brands:${icon}`" class="flex-shrink-0 w-10 h-10 text-white" :alt="`${icon} icon`" />
       <div class="font-semibold text-center text-white sm:text-xl">
         <Markdown :use="$slots.title" unwrap="p" />
@@ -16,6 +22,10 @@ defineProps({
     default: ''
   },
   icon: {
+    type: String,
+    default: ''
+  },
+  itemClass: {
     type: String,
     default: ''
   }
