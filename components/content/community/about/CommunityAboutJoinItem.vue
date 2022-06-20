ad9c793bd843a4b084d7704354ffab8ce35a02d9
Florent <florentdelerue@hotmail.com>
2022-06-20 17:17:31 +0200

    refactor(communityaboutjoin): update item height and width

--- components/content/community/about/CommunityAboutJoinItem.vue
+++ components/content/community/about/CommunityAboutJoinItem.vue
@@ -4,8 +4,8 @@
     :class="itemClass"
   >
     <a
-      :href="href"
-      class="flex flex-col w-[120px] h-[120px] sm:w-[148px] sm:h-[146px] items-center py-6 transition duration-300 rounded-md gap-y-2 hover:bg-gray-800 px-1"
+      :hr`e`f="href"
+      class="flex flex-col w-[120px] h-[120px] sm:w-[160px] sm:h-[160px] items-center justify-center py-6 transition duration-300 rounded-md gap-y-2 hover:bg-gray-800 px-1"
     >
       <UIcon :name="`fa6-brands:${icon}`" class="flex-shrink-0 w-10 h-10 text-white" :alt="`${icon} icon`" />
       <div class="font-semibold text-center text-white sm:text-xl">
