c9b4e882f4985ed266dcbb341e74488114a6c02e
Florent <florentdelerue@hotmail.com>
2022-06-20 15:23:54 +0200

    refactor(communityaboutstats): update spacing and font size

--- components/content/community/about/CommunityAboutStats.vue
+++ components/content/community/about/CommunityAboutStats.vue
@@ -3,10 +3,10 @@
     <h2 class="text-4xl font-semibold u-text-gray-900 leading-[140%]">
       <Markdown :use="$slots.title" unwrap="p" />
     </h2>
-    <p class="w-1/2 text-center xl:w-[40%] u-text-gray-600">
+    <p class="w-3/4 sm:w-1/2 text-xl text-center xl:w-[40%] u-text-gray-600">
       <Markdown :use="$slots.description" unwrap="p" />
     </p>
-    <ul class="grid items-start grid-cols-3 gap-x-2 sm:gap-x-12 md:gap-x-32">
+    <ul class="grid items-start grid-cols-1 gap-y-8 sm:grid-cols-3 gap-x-2 sm:gap-x-12 md:gap-x-32">
       <Markdown :use="$slots.items" unwrap="p" />
     </ul>
   </UContainer>
