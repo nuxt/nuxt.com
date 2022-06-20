7d4b5be24934370e8cfadbf92a866d74aa8a37b8
Benjamin Canac <canacb1@gmail.com>
2022-06-20 14:51:30 +0200

    chore(config): move tailwind to `tailwind.config.js`

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,31 @@
-module.exports = {}
+import colors from './presets/colors'
+import typography from './presets/typography'
+
+module.exports = {
+  theme: {
+    colors,
+    extend: {
+      typography,
+      fontFamily: {
+        sans: '"RoobertPRO", sans-serif'
+      },
+      linearBorderGradients: ({ theme }) => ({
+        colors: {
+          gray: [theme('colors.gray.900')],
+          gradient: [colors.green[400], colors.teal[400], colors.indigoblue[400]]
+        },
+        background: theme('colors')
+      })
+    }
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('tailwindcss-border-gradient-radius')
+  ],
+  content: [
+    'presets/*.ts',
+    'content/**/*.md',
+    'editor/**/*.vue'
+  ],
+  safelist: [12, 24, 36, 48, 60, 72, 84, 96, 108, 120].map(number => `pl-[${number}px]`)
+}
