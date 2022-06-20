284f7d0fbb2dcf4c3c9d5c8f06e6b1ba79b28f65
Sébastien Chopin <seb@nuxtjs.com>
2022-06-20 02:31:09 +0200

    feat: add caching middleware

--- server/middleware/swr.ts
+++ server/middleware/swr.ts
@@ -1,5 +1,8 @@
 export default cachedEventHandler((event) => {
-  if (process.env.NODE_ENV === 'production' && event.req.url.startsWith('/api/_content/')) {
-    event.res.setHeader('Cache-Control', 's-maxage=1, stale-while-revalidate=59')
+  if (process.env.NODE_ENV === 'production') {
+    // Cache like static assets, invalidate after new deployment
+    if (event.req.url.startsWith('/api/_content/')) {
+      event.res.setHeader('Cache-Control', 'public,max-age=31536000,immutable')
+    }
   }
 })
