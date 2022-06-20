c9c5f04cab50ad8fccd1cbc047552832cb6f5557
Florent <florentdelerue@hotmail.com>
2022-06-20 12:43:04 +0200

    refactor(communityaboutstats): center grid items on tablet/mobile

--- components/content/community/about/CommunityAboutJoin.vue
+++ components/content/community/about/CommunityAboutJoin.vue
@@ -3,7 +3,7 @@
     <h2 class="text-4xl font-semibold text-center text-white leading-[140%]">
       <Markdown :use="$slots.title" unwrap="p" />
     </h2>
-    <ul class="grid items-center justify-center grid-cols-2 sm:grid-cols-3 xl:grid-cols-5 place-items-center gap-x-8 sm:gap-x-16">
+    <ul class="grid items-center justify-center grid-cols-2 sm:grid-cols-6 xl:grid-cols-5 place-items-center gap-x-8 sm:gap-x-16">
       <Markdown :use="$slots.items" unwrap="p" />
     </ul>
   </UContainer>
--- components/content/community/about/CommunityAboutStats.vue
+++ components/content/community/about/CommunityAboutStats.vue
@@ -6,7 +6,7 @@
     <p class="w-1/2 text-center xl:w-[40%] u-text-gray-600">
       <Markdown :use="$slots.description" unwrap="p" />
     </p>
-    <ul class="grid items-start grid-cols-3 gap-x-2 sm:gap-x-12 md:gap-x-32">
+    <ul class="grid items-start grid-cols-[repeat(auto-fit, minmax(170px, 1fr))] gap-x-2 sm:gap-x-12 md:gap-x-32">
       <Markdown :use="$slots.items" unwrap="p" />
     </ul>
   </UContainer>
