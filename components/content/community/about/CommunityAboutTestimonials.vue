cb63ddeb190a1c8559261a1256dc80eb3e94c1b9
Florent <florentdelerue@hotmail.com>
2022-06-20 15:52:37 +0200

    refactor(communityabouttestimonials): add leading on title

--- components/content/community/about/CommunityAboutTestimonials.vue
+++ components/content/community/about/CommunityAboutTestimonials.vue
@@ -1,6 +1,6 @@
 <template>
   <UContainer padded class="flex flex-col items-center justify-center pt-20 pb-4 sm:py-20 gap-y-8">
-    <h4 class="text-[24px] font-semibold u-text-gray-900">
+    <h4 class="text-[24px] font-semibold u-text-gray-900 leading-[140%]">
       <Markdown :use="$slots.title" unwrap="p" />
     </h4>
     <p class="mb-8 text-center u-text-gray-600">
