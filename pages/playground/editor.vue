65f808bbdf1c402dbb069d783efd90092115a067
Kevin Marrec <kevin@marrec.io>
2022-06-21 14:50:18 +0200

    chore: add editor playground (dev only)

---
+++ pages/playground/editor.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <ProjectContentFileEditor
+      :components="components"
+      :content="{
+        key: 'playground',
+        markdown,
+        matter: {}
+      }"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { ComponentSchema } from '~/editor/types'
+
+const components: ComponentSchema[] = [
+  {
+    name: 'MyComponent',
+    props: [],
+    slots: [
+      {
+        name: 'default'
+      }
+    ]
+  }
+]
+
+const markdown = ref('# Hello World')
+</script>
