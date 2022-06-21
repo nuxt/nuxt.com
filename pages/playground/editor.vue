535ddeca41ccce33510d43488ce1c61733e258fe
Kevin Marrec <kevin@marrec.io>
2022-06-21 15:27:53 +0200

    chore: add url input to editor playground

--- pages/playground/editor.vue
+++ pages/playground/editor.vue
@@ -1,6 +1,24 @@
 <template>
   <div>
+    <div class="flex my-2 gap-3">
+      <UInput
+        v-model="markdownURL"
+        name="url"
+        autocomplete="off"
+        placeholder="https://raw.githubusercontent.com/nuxt/content/main/docs/content/1.index.md"
+        class="w-full"
+        :custom-class="markdownURLError ? 'border-red-500 dark:border-red-500': ''"
+        @input="markdownURLError = false"
+      />
+      <UButton
+        icon="heroicons-outline:download"
+        type="submit"
+        :loading="markdownLoading"
+        @click="loadMarkdown"
+      />
+    </div>
     <ProjectContentFileEditor
+      class="mt-4"
       :components="components"
       :content="{
         key: 'playground',
@@ -13,6 +31,8 @@
 
 <script setup lang="ts">
 import type { ComponentSchema } from '~/editor/types'
+
+const { parse: parseMarkdown } = useMarkdown()
 
 const components: ComponentSchema[] = [
   {
@@ -27,4 +47,18 @@ const components: ComponentSchema[] = [
 ]
 
 const markdown = ref('# Hello World')
+const markdownLoading = ref(false)
+const markdownURL = ref('')
+const markdownURLError = ref(false)
+
+async function loadMarkdown () {
+  markdownURLError.value = false
+  markdownLoading.value = true
+  try {
+    markdown.value = parseMarkdown(await $fetch(markdownURL.value)).content
+  } catch (e) {
+    markdownURLError.value = true
+  }
+  markdownLoading.value = false
+}
 </script>
