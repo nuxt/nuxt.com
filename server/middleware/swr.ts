2dfe21f58d5d9c5ed89bcdaff55a6aae54bf5b66
Sébastien Chopin <seb@nuxtjs.com>
2022-06-21 13:24:22 +0200

    fix: cache-control headers

--- server/middleware/swr.ts
+++ server/middleware/swr.ts
@@ -1,8 +1,10 @@
-export default cachedEventHandler((event) => {
+
+export default eventHandler((event) => {
   if (process.env.NODE_ENV === 'production') {
-    // Cache like static assets, invalidate after new deployment
+  // Cache like static assets, invalidate after new deployment
     if (event.req.url.startsWith('/api/_content/')) {
-      event.res.setHeader('Cache-Control', 'public,max-age=31536000,immutable')
+    // Cache for 7 days
+      event.res.setHeader('Cache-Control', 'public, max-age=604800, s-max-age=604800, stale-while-revalidate')
     }
   }
 })
