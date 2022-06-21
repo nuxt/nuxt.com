ea47927486d6f644ca833a9a7ea22b44ffca37a1
Sébastien Chopin <seb@nuxtjs.com>
2022-06-21 14:38:06 +0200

    chore: reduce cache

--- server/middleware/swr.ts
+++ server/middleware/swr.ts
@@ -3,8 +3,8 @@ export default eventHandler((event) => {
   if (process.env.NODE_ENV === 'production') {
   // Cache like static assets, invalidate after new deployment
     if (event.req.url.startsWith('/api/_content/')) {
-    // Cache for 7 days
-      event.res.setHeader('Cache-Control', 'public, max-age=604800, s-max-age=604800, stale-while-revalidate')
+    // Cache for 1 hour
+      event.res.setHeader('Cache-Control', 'public,max-age=3600,s-max-age=3600,stale-while-revalidate=1')
     }
   }
 })
