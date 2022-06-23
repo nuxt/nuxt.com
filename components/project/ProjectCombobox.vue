307717e5293c89ca14e6d9c4a09bee93fc50f893
Benjamin Canac <canacb1@gmail.com>
2022-06-23 14:06:48 +0200

    fix(project): remove autocomplete on comboboxes

--- components/project/ProjectCombobox.vue
+++ components/project/ProjectCombobox.vue
@@ -3,7 +3,14 @@
     <div class="flex flex-col flex-1 min-h-0 divide-y u-divide-gray-100">
       <div class="relative">
         <UIcon name="heroicons-outline:search" class="pointer-events-none absolute top-3.5 left-5 h-5 w-5 u-text-gray-400" aria-hidden="true" />
-        <ComboboxInput ref="comboboxInput" :value="query" class="w-full h-12 pr-4 placeholder-gray-400 dark:placeholder-gray-500 bg-transparent border-0 pl-[3.25rem] u-text-gray-900 focus:ring-0 sm:text-sm" placeholder="Search..." @change="query = $event.target.value" />
+        <ComboboxInput
+          ref="comboboxInput"
+          :value="query"
+          class="w-full h-12 pr-4 placeholder-gray-400 dark:placeholder-gray-500 bg-transparent border-0 pl-[3.25rem] u-text-gray-900 focus:ring-0 sm:text-sm"
+          autocomplete="off"
+          placeholder="Search..."
+          @change="query = $event.target.value"
+        />
       </div>
 
       <ComboboxOptions v-if="hasOptions" static hold class="relative flex-1 overflow-y-auto divide-y u-divide-gray-100 scroll-py-2">
