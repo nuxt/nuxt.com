9a0850d317737357a7f8653d48381f874a749acd
Benjamin Canac <canacb1@gmail.com>
2022-06-28 16:05:08 +0200

    chore: improve responsive heros

--- components/content/community/partners/CommunityPartnersHero.vue
+++ components/content/community/partners/CommunityPartnersHero.vue
@@ -19,7 +19,7 @@
     </template>
 
     <template #image>
-      <img src="/assets/community/partners/gems.svg" class="object-contain h-full mx-10 opacity-30 lg:opacity-100">
+      <img src="/assets/community/partners/gems.svg" class="object-contain h-full lg:mx-10 opacity-10 lg:opacity-100">
     </template>
   </PageHero>
 </template>
