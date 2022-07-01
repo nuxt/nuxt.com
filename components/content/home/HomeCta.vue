7bdd8491463d6fa6a7283092730ccd5ca7ac71f6
Florent Delerue <florentdelerue@hotmail.com>
2022-07-01 17:39:47 +0200

    refactor(home): improvements (#748)
    Co-authored-by: Vincent Rodriguez <64779770+R-mooon@users.noreply.github.com>

--- components/content/home/HomeCta.vue
+++ components/content/home/HomeCta.vue
@@ -1,6 +1,11 @@
 <template>
-  <div class="relative pb-16 sm:pb-32">
-    <img src="/assets/home/gradient-cta.svg" class="absolute w-full h-full bottom-24 -left-8">
+  <div
+    ref="ctaContainer"
+    class="relative pt-10 pb-40 transition duration-700 sm:pt-44 sm:px-0 md:pt-28 lg:pt-40"
+    :class="!slideIn ? 'opacity-0 translate-y-20' : 'opacity-100 translate-y-0'"
+  >
+    <div ref="mouseLight" class="absolute top-0 rounded-full blur-[50px] bg-gradient-to-t from-green-400 via-teal-400 to-indigoblue-400" />
+
     <UContainer padded class="relative flex flex-col items-center justify-center gap-y-6">
       <h3 class="text-4xl font-semibold u-text-gray-900">
         <Markdown :use="$slots.title" unwrap="p" />
@@ -8,15 +13,17 @@
       <p class="max-w-xl text-xl text-center u-text-gray-900">
         <Markdown :use="$slots.description" unwrap="p" />
       </p>
-      <form class="flex flex-wrap justify-start justify-center w-full gap-6 sm:flex-nowrap" @submit.prevent="onSubmit">
-        <UInput
-          v-model="form.email"
-          name="email"
-          placeholder="Enter your email"
-          class="w-full sm:w-64"
-          size="lg"
-          required
-        />
+      <form ref="root" class="flex flex-wrap justify-start justify-center w-full gap-6 sm:flex-nowrap" @submit.prevent="onSubmit">
+        <div ref="inputForm">
+          <UInput
+            v-model="form.email"
+            name="email"
+            placeholder="Enter your email"
+            class="w-full sm:w-64"
+            size="lg"
+            required
+          />
+        </div>
         <UButton
           type="submit"
           submit
@@ -31,12 +38,28 @@
 </template>
 
 <script setup lang="ts">
+import type { Ref } from 'vue'
+
 defineProps({
   buttonText: {
     type: String,
     default: ''
   }
 })
+
+const observer = ref() as Ref<IntersectionObserver>
+const root = ref(null) as Ref<Element>
+const slideIn = ref(false)
+const mouseLight = ref(null)
+const ctaContainer = ref(null)
+const inputForm = ref(null)
+
+const observerCallback = (entries: IntersectionObserverEntry[]) =>
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      slideIn.value = true
+    }
+  })
 
 const { $toast } = useNuxtApp()
 
@@ -59,4 +82,33 @@ async function onSubmit () {
 
   loading.value = false
 }
+
+onBeforeMount(() => (observer.value = new IntersectionObserver(observerCallback)))
+
+onMounted(() => {
+  observer.value.observe(root.value)
+
+  document.addEventListener('mousemove', (e) => {
+    const refNumber = 1000
+    const input = inputForm.value.getBoundingClientRect()
+    const x = e.clientX - (input.left + (inputForm.value.clientWidth / 2))
+    const y = e.clientY - (input.top + (inputForm.value.clientHeight / 2))
+    const coord = Math.abs(y) + Math.abs(x)
+    const size = refNumber - coord
+
+    mouseLight.value.style.top = `${e.clientY - ctaContainer.value.getBoundingClientRect().y - mouseLight.value.clientHeight / 2}px`
+    mouseLight.value.style.left = `${e.clientX - mouseLight.value.clientWidth / 2}px`
+
+    mouseLight.value.style.width = `${(size / 3)}px`
+    mouseLight.value.style.height = `${(size / 3)}px`
+
+    if (e.clientY < ctaContainer.value.getBoundingClientRect().y) {
+      mouseLight.value.style.visibility = 'hidden'
+    } else {
+      mouseLight.value.style.visibility = 'visible'
+    }
+  })
+})
+
+onBeforeUnmount(() => observer.value?.disconnect())
 </script>
--- components/content/home/HomeHero.vue
+++ components/content/home/HomeHero.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="pb-20 pt-36 md:pt-44 lg:pt-28">
-    <img src="/assets/home/hero-gradient.svg" class="absolute top-0 right-0 overflow-hidden select-none">
+    <img src="/assets/home/hero-gradient.svg" class="absolute top-0 right-0 hidden overflow-hidden select-none lg:block">
+    <img src="/assets/home/hero-gradient-tablet.svg" class="absolute top-0 right-0 hidden overflow-hidden select-none sm:block lg:hidden">
+    <img src="/assets/home/hero-gradient-mobile.svg" class="absolute inset-x-0 top-0 overflow-hidden select-none sm:hidden">
     <UContainer padded class="relative flex flex-col items-center sm:items-start gap-y-8">
       <div v-if="$slots.badgeLabel" class="flex gap-x-2">
         <UBadge rounded variant="green">
@@ -10,13 +12,16 @@
           <Markdown :use="$slots.news" unwrap="p" />
         </span>
       </div>
-      <h1 v-if="$slots.title" class="max-w-2xl text-5xl font-semibold text-center sm:text-left md:text-6xl lg:text-7xl u-text-gray-900">
+      <h1 v-if="$slots.title" class="relative max-w-2xl text-5xl font-semibold text-center sm:text-left md:text-6xl lg:text-7xl u-text-gray-900">
         <Markdown use="title" unwrap="p" />
+        <span ref="title" class="absolute transition duration-700" style="translate: transform(0); opacity: 0">
+          <Markdown :use="$slots.titleAnimationWord" unwrap="p" />
+        </span>
       </h1>
       <p v-if="$slots.description" class="max-w-lg text-lg text-center text-gray-500 sm:w-3/5 sm:text-left dark:text-gray-100">
         <Markdown use="description" unwrap="p" />
       </p>
-      <div class="flex gap-6">
+      <div class="flex gap-6 z-[1]">
         <UButton :label="primaryButtonText" size="lg" variant="primary-gradient" truncate @click="scrollToVideo()" />
         <UButton
           :label="secondaryButtonText"
@@ -39,6 +44,7 @@
 <script setup lang="ts">
 const route = useRoute()
 const router = useRouter()
+const title = ref(null)
 
 defineProps({
   primaryButtonText: {
@@ -50,6 +56,15 @@ defineProps({
     default: ''
   }
 })
+
+onMounted(() => animateTitle())
+
+const animateTitle = () => {
+  setTimeout(() => {
+    title.value.style.transform = 'translate(16px)'
+    title.value.style.opacity = 1
+  }, 500)
+}
 
 const scrollToVideo = () => {
   router.push({
