78e94d471864a3da673a29e28f4c96972aff122e
Florent Delerue <florentdelerue@hotmail.com>
2022-07-01 17:39:47 +0200

    refactor(home): improvements (#748)
    Co-authored-by: Vincent Rodriguez <64779770+R-mooon@users.noreply.github.com>

--- components/content/home/HomeSection.vue
+++ components/content/home/HomeSection.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="flex flex-col px-4 pt-24 pb-12 sm:px-0 sm:pb-20 md:pb-28 lg:pb-40" :class="containerClass">
+  <div
+    class="flex flex-col px-4 pt-24 transition duration-700"
+    :class="[containerClass, !slideIn ? 'opacity-0 translate-y-20' : 'opacity-100 translate-y-0']"
+  >
     <!-- Main title -->
     <div class="flex flex-col items-center justify-center" :class="mainTitleContainerClass">
       <div :class="sectionTitleColorClass" class="pb-2 font-semibold">
@@ -25,9 +28,9 @@
       </div>
     </div>
     <!-- body section -->
-    <div class="relative">
+    <div ref="root" class="relative">
       <UContainer
-        class="flex flex-col items-center justify-between w-full lg:flex-row gap-y-12"
+        class="flex flex-col items-center justify-between w-full lg:flex-row gap-y-10"
         :class="[
           bodyContainerClass,
           { 'gap-y-8': ['left', 'right'].includes(bodyPlacement) },
@@ -45,7 +48,7 @@
           <div v-if="$slots.bodyTitle" class="text-2xl font-semibold xl:text-3xl 2xl:text-4xl u-text-gray-900">
             <Markdown :use="$slots.bodyTitle" unwrap="p" />
           </div>
-          <div v-if="$slots.bodyDescription" class="items-center text-lg xl:text-xl 2xl:text-2xl u-text-gray-500">
+          <div v-if="$slots.bodyDescription" class="items-center text-lg text-gray-500 xl:text-xl 2xl:text-2xl dark:text-gray-400">
             <Markdown :use="$slots.bodyDescription" unwrap="p" />
           </div>
           <NuxtLink v-if="$slots.bodyLink" :to="to" class="relative flex-nowrap max-w-max group">
@@ -75,12 +78,12 @@
 </template>
 
 <script setup lang="ts">
-import type { PropType } from 'vue'
+import type { PropType, Ref } from 'vue'
 
 defineProps({
   containerClass: {
     type: String,
-    default: 'gap-y-12 md:gap-y-16 lg:gap-y-20'
+    default: 'gap-y-10 pb-10 sm:px-0 sm:pb-20 md:pb-28 lg:pb-40'
   },
   sectionTitleColorClass: {
     type: String,
@@ -130,4 +133,21 @@ defineProps({
     default: ''
   }
 })
+
+const observer = ref() as Ref<IntersectionObserver>
+const root = ref(null) as Ref<Element>
+const slideIn = ref(false)
+
+const observerCallback = (entries: IntersectionObserverEntry[]) =>
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      slideIn.value = true
+    }
+  })
+
+onBeforeMount(() => (observer.value = new IntersectionObserver(observerCallback)))
+
+onMounted(() => observer.value.observe(root.value))
+
+onBeforeUnmount(() => observer.value?.disconnect())
 </script>
