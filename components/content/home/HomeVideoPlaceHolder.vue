5a94d3b1c1fad4102b7af82f2dc85896ed8afdbe
Florent <florentdelerue@hotmail.com>
2022-07-04 15:24:52 +0200

    refactor(home): update due to design review

--- components/content/home/HomeVideoPlaceHolder.vue
+++ components/content/home/HomeVideoPlaceHolder.vue
@@ -3,7 +3,7 @@
     <HomeModal v-model="isOpen" :video-id="videoId" :title="title" />
     <img src="/assets/home/place-holder.png" class="w-full h-full rounded-xl lg:rounded-r-xl lg:rounded-l-none">
     <div class="absolute inset-0 flex items-center justify-center">
-      <UIcon name="heroicons-solid:play" alt="play icon" class="w-20 h-20 group-hover:text-gray-300" />
+      <UIcon name="ic:round-play-arrow" alt="play icon" class="w-20 h-20 text-gray-100 group-hover:text-gray-300" />
     </div>
   </div>
 </template>
