40db4517f8ab62ca8786ca7ca5ee1411af4b5644
Vincent Rodriguez <64779770+R-mooon@users.noreply.github.com>
2022-07-07 12:34:45 +0200

    refactor(home): Gem responsive improvements (#795)

--- components/content/home/HomeGemWrapper.vue
+++ components/content/home/HomeGemWrapper.vue
@@ -1,5 +1,15 @@
 <template>
   <ClientOnly>
-    <Markdown :use="$slots.default" unwrap="p" />
+    <span :class="gemWrapperClass">
+      <Markdown :use="$slots.default" unwrap="p" />
+    </span>
   </ClientOnly>
 </template>
+<script setup lang="ts">
+defineProps({
+  gemWrapperClass: {
+    type: String,
+    default: ''
+  }
+})
+</script>
