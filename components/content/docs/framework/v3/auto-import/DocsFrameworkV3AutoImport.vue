4277a3d2e55086edca2bb8fefb3a70291817741b
Florent <florentdelerue@hotmail.com>
2022-07-12 17:25:29 +0200

    refactor(docsv3): auto import section (wip)

--- components/content/docs/framework/v3/auto-import/DocsFrameworkV3AutoImport.vue
+++ components/content/docs/framework/v3/auto-import/DocsFrameworkV3AutoImport.vue
@@ -1,41 +1,48 @@
 <template>
-  <div ref="root" class="grid grid-cols-12 pt-12">
-    <ul class="flex flex-col col-span-3 gap-y-6">
+  <div ref="root" class="relative flex flex-col justify-center gap-y-20 xl:flex-row xl:justify-between">
+    <ul class="grid grid-cols-1 rounded-md xl:flex xl:flex-col xl:w-2/5 gap-y-2">
       <li
         v-for="(data, index) in autoImportData.autoImport"
         :key="data.title"
-        class="flex items-center opacity-100 cursor-pointer gap-x-4"
-        @mouseenter="hoveredSection(index)"
+        class="relative flex flex-col p-4 transition duration-200 rounded-md cursor-pointer md:flex-row gap-x-2 group hover:u-bg-gray-50"
+        @mouseenter="currentSection = index"
         @mouseleave="currentSection = null"
-        @click="hoveredSection(index)"
+        @click="currentSection = index"
       >
-        <div class="relative">
-          <Hexagon class="w-16 u-text-gray-50 h-14" />
-          <div class="absolute top-0 flex items-center justify-center w-16 h-14 z-1">
-            <img
-              :src="`/assets/docs/v3/auto-import/${data.icon}`"
-              class="absolute w-8 h-8 transition-opacity duration-0"
-              :alt="`${data.title} icon`"
-              :class="currentSection.value === index ? 'opacity-0' : 'opacity-100'"
-            >
-            <img
-              :src="`/assets/docs/v3/auto-import/${data.iconColor}`"
-              class="absolute w-8 h-8 transition-opacity duration-0"
-              :alt="`${data.title} icon`"
-              :class="currentSection.value === index ? 'opacity-100' : 'opacity-0'"
-            >
-          </div>
-        </div>
-        <h6
-          class="text-lg transition-colors duration-0"
-          :class="currentSection.value === index ? 'text-green-400' : 'u-text-gray-500'"
+        <div
+          class="absolute top-0 left-0 flex items-center justify-center w-12 h-12 p-3 mt-4 ml-4 transition duration-200 rounded-md group-hover:opacity-0 u-bg-gray-200"
+          :class="currentSection === index ? 'opacity-0' : 'opacity-100'"
         >
-          {{ data.title }}
-        </h6>
+          <img
+            :src="`/assets/docs/v3/auto-import/${data.icon}`"
+            class="transition duration-200 group-hover:opacity-0"
+            :class="currentSection === index ? 'opacity-0' : 'opacity-100'"
+          >
+        </div>
+        <div
+          class="absolute top-0 left-0 flex items-center justify-center w-12 h-12 p-3 mt-4 ml-4 transition duration-200 rounded-md opacity-0 group-hover:opacity-100 bg-gradient-to-b from-green-600 to-green-400"
+          :class="currentSection === index ? 'opacity-100' : 'opacity-0'"
+        >
+          <img
+            :src="`/assets/docs/v3/auto-import/${data.iconColor}`"
+            class="transition duration-200 opacity-0 group-hover:opacity-100"
+            :class="currentSection === index ? 'opacity-100' : 'opacity-0'"
+          >
+        </div>
+        <div class="flex flex-col pt-16 md:pt-0 md:pl-16 gap-y-2">
+          <h6 class="text-lg font-semibold u-text-gray-900">
+            {{ data.title }}
+          </h6>
+          <p class="u-text-gray-700">
+            {{ data.description }}
+          </p>
+        </div>
       </li>
     </ul>
-    <div class="relative flex items-center justify-center w-full h-full col-span-9">
-      <!-- DocsFrameworkV3AutoImportContainer :current-section="currentSection" :section1-steps="section1Steps" :section2-steps="section2Steps" :section3-steps="section3Steps" /-->
+    <div class="top-0 right-0 flex items-center justify-center flex-1 xl:absolute 2xl:-right-32 top-24">
+      <div class="xl:ml-20">
+        <DocsFrameworkV3AutoImportContainer :current-section="currentSection" />
+      </div>
     </div>
   </div>
 </template>
@@ -46,8 +53,7 @@ const { data: autoImportData } = await useAsyncData('autoImport', () => queryCon
 const currentSection = ref()
 
 const hoveredSection = (index) => {
-  console.log(index)
-  // currentSection.value = index
+  currentSection.value = index
 }
 
 </script>
--- content/docs/3.x/_collections/auto-import.md
+++ content/docs/3.x/_collections/auto-import.md
@@ -1,9 +1,15 @@
 ---
 autoImport:
   - title: Components
+    description: Nuxt server renders your pages to bring your website the full visibility it deserves so users will find you website.
     icon: components.svg
+    iconColor: components-green.svg
   - title: Composables
+    description: 'Once the website is loaded, users will discover a new experience: an application feeling right into their browser.'
     icon: composables.svg
+    iconColor: composables-green.svg
   - title: Plugins
+    description: Nuxt ships by default with best Lighthouse and Web vitals score so users won’t wait for accessing your website.
     icon: plugins.svg
+    iconColor: plugins-green.svg
 ---
