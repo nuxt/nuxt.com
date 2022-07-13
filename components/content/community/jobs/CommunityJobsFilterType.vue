6e307c43a2e5dd4f91304117a0940cb7c330545b
Sylvain Marroufin <marroufin.sylvain@gmail.com>
2022-07-13 14:19:49 +0200

    feat(community): add jobs page (#803)
    Co-authored-by: Benjamin Canac <canacb1@gmail.com>

---
+++ components/content/community/jobs/CommunityJobsFilterType.vue
@@ -0,0 +1,43 @@
+<template>
+  <USelectCustom
+    v-model="type"
+    name="type"
+    :options="typesWithPlaceholder"
+    size="sm"
+    placeholder="Type"
+    class="min-w-[128px]"
+  />
+</template>
+
+<script setup lang="ts">
+const route = useRoute()
+const router = useRouter()
+
+const { types, selectedType } = useCommunityJobs()
+
+const typesWithPlaceholder = computed(() => [
+  {
+    value: '',
+    text: 'All'
+  },
+  ...types.value
+])
+
+const type = computed({
+  get () {
+    return selectedType.value
+  },
+  set (type) {
+    router.push({
+      name: 'community-jobs',
+      query: {
+        ...route.query,
+        type: type?.value || undefined
+      },
+      params: {
+        smooth: '#smooth'
+      }
+    })
+  }
+})
+</script>
---
+++ components/content/community/jobs/CommunityJobsList.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="relative overflow-hidden">
+    <img src="/assets/community/jobs/gradient.svg" class="absolute right-0 hidden mt-32 lg:block">
+
+    <Page id="smooth" class="relative pt-16 -mt-16">
+      <PageList :title="`${filteredJobs.length} job${filteredJobs.length > 1 ? 's' : ''} found`">
+        <template #filters>
+          <CommunityJobsFilterLocation />
+          <CommunityJobsFilterType />
+        </template>
+
+        <ul v-if="filteredJobs.length" class="flex flex-col gap-8 mt-8">
+          <li v-for="job in filteredJobs" :key="job.id">
+            <CommunityJobsListItem :job="job" />
+          </li>
+        </ul>
+        <div v-else class="relative flex flex-col items-center gap-6 mt-16 lg:mt-24">
+          <span class="text-xl font-medium text-center u-text-gray-700">
+            No job found.
+          </span>
+        </div>
+      </PageList>
+    </Page>
+  </div>
+</template>
+
+<script setup lang="ts">
+const { jobs, selectedLocation, selectedType } = useCommunityJobs()
+
+const filteredJobs = computed(() => {
+  return [...jobs.value]
+    .filter((job) => {
+      if (selectedLocation.value && job.location !== selectedLocation.value.value) {
+        return false
+      }
+      if (selectedType.value && job.type !== selectedType.value.value) {
+        return false
+      }
+      return true
+    })
+})
+</script>
