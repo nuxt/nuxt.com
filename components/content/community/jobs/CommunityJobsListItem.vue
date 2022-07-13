b32ef2adcbbcc96d29ce15c8b45616c6bdb55b32
Sylvain Marroufin <marroufin.sylvain@gmail.com>
2022-07-13 11:14:28 +0200

    update responsiveness

--- components/content/community/jobs/CommunityJobsListItem.vue
+++ components/content/community/jobs/CommunityJobsListItem.vue
@@ -8,16 +8,16 @@
     class="hover:ring-2 hover:u-ring-gray-900"
   >
     <NuxtLink :to="`https://vuejobs.com/jobs/${job.id}`" target="_blank" class="flex flex-col gap-2 px-4 py-5 sm:p-6">
-      <div class="flex gap-4">
+      <div class="grid grid-cols-2 gap-4 sm:flex">
         <img :src="job.company.avatar" :alt="job.company.name" class="object-contain w-16 h-16 rounded-full sm:w-20 sm:h-20">
-        <div class="flex flex-col flex-1 gap-1">
+        <span class="order-1 text-right sm:order-3 u-text-gray-400">{{ job.published_at.for_humans }}</span>
+        <div class="flex flex-col flex-1 order-2 col-span-2 gap-1">
           <div class="flex flex-col gap-2 sm:flex-row">
             <span class="flex-shrink text-xl font-semibold u-text-gray-900">{{ job.title }}</span>
             <span class="self-start px-2 py-0.5 rounded-full whitespace-nowrap" :class="typeClass">{{ typeLabel }}</span>
           </div>
           <span class="u-text-gray-500">{{ job.location }}</span>
         </div>
-        <span class="u-text-gray-400">{{ job.published_at.for_humans }}</span>
       </div>
       <div>
         <span class="u-text-gray-500" v-html="description"></span>
