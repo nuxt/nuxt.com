1fafd67863eb164bb209b41cfeb17693cb353ad9
Florent <florentdelerue@hotmail.com>
2022-07-20 11:56:33 +0200

    refactor(docsv3): auto import section responsive

--- components/content/docs/framework/v3/auto-import/DocsFrameworkV3AutoImportContainer.vue
+++ components/content/docs/framework/v3/auto-import/DocsFrameworkV3AutoImportContainer.vue
@@ -2,55 +2,18 @@
   <div class="relative flex">
     <img src="/assets/docs/v3/auto-import/gem.svg" alt="gem illustration" class="absolute -right-16 -bottom-24">
     <div class="relative">
-      <svg
-        width="756"
-        height="367"
-        viewBox="0 0 756 367"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        class="text-gray-800 dark:text-gray-900"
-      >
-        <rect width="756" height="367" rx="14.2105" fill="currentColor" />
-        <rect y="35" width="756" height="1" fill="#3F3F46" />
-        <rect
-          x="230"
-          y="367"
-          width="332"
-          height="1.00003"
-          transform="rotate(-90 230 367)"
-          fill="#3F3F46"
-        />
-        <circle cx="17.4999" cy="17.5" r="5.5" fill="#3F3F46" />
-        <circle cx="34.4999" cy="17.5" r="5.5" fill="#3F3F46" />
-        <circle cx="52.4999" cy="17.5" r="5.5" fill="#3F3F46" />
-        <defs>
-          <linearGradient
-            id="paint0_linear_4107_49654"
-            x1="32"
-            y1="90"
-            x2="32"
-            y2="149.791"
-            gradientUnits="userSpaceOnUse"
-          >
-            <stop stop-color="#71717A" />
-            <stop offset="1" stop-color="#71717A" stop-opacity="0" />
-          </linearGradient>
-          <linearGradient
-            id="paint1_linear_4107_49654"
-            x1="32"
-            y1="209"
-            x2="32"
-            y2="333.649"
-            gradientUnits="userSpaceOnUse"
-          >
-            <stop stop-color="#71717A" />
-            <stop offset="1" stop-color="#71717A" stop-opacity="0" />
-          </linearGradient>
-        </defs>
-      </svg>
+      <div class="h-[500px] md:h-[367px] w-[310px] md:w-[756px] bg-gray-800 rounded-xl">
+        <div class="absolute top-3 left-3 flex gap-x-2">
+          <div v-for="(dot, index) in 3" :key="index" class="h-3 w-3 rounded-full bg-gray-600" />
+        </div>
+        <div class="absolute h-[1px] bg-gray-600 top-8 inset-x-0" />
+        <div class="absolute h-[1px] bg-gray-600 top-[290px] inset-x-0 md:hidden" />
+        <div class="absolute w-[1px] bg-gray-600 left-[220px] top-8 bottom-0 hidden md:block" />
+      </div>
+      <!-- img src="/assets/docs/v3/auto-import/terminal.svg" alt="terminal illustration" -->
       <!-- left container -->
-      <div class="absolute inset-0 flex pt-[35px] ">
-        <div class="w-[330px] relative">
+      <div class="absolute inset-0 flex flex-col md:flex-row pt-[35px] ">
+        <div class="w-[310px] md:w-[330px] relative">
           <div class="absolute flex flex-col text-gray-300 transition-opacity duration-300 top-4 left-6 gap-y-4" :class="currentSection === null ? 'opacity-100' : 'opacity-0'">
             <div v-for="(text, index) in ['pages', 'components', 'plugins']" :key="index" class="flex items-center gap-x-2">
               <UIcon name="uil:angle-right" class="w-5 h-5 mt-1" /> <span class="font-medium">{{ text }}</span>
@@ -58,9 +21,9 @@
           </div>
           <div class="absolute flex flex-col text-gray-300 transition-opacity duration-300 top-4 left-6 gap-y-4" :class="currentSection === 0 ? 'opacity-100' : 'opacity-0'">
             <div v-for="(text, index) in ['pages', 'components']" :key="index" class="relative">
-              <div class="relative flex items-center gap-x-2" :class="{ 'pt-[59px]': index === 1 }">
+              <div class="relative flex items-center gap-x-2" :class="{ 'pt-[20px] md:pt-[59px]': index === 1 }">
                 <UIcon name="uil:angle-down" class="w-5 h-5 mt-1" /> <span class="font-medium">{{ text }}</span>
-                <div class="absolute left-[9px] w-0.5 bg-gradient-to-b from-gray-400 to-transparent" :class="index === 1 ? 'top-[89px] h-[109px] ' : 'top-[26px] h-[59px] '" />
+                <div class="absolute left-[9px] w-0.5 bg-gradient-to-b from-gray-400 to-transparent" :class="index === 1 ? 'top-[46px] md:top-[89px] h-[109px] ' : 'top-[26px] h-[59px] '" />
               </div>
               <div class="flex items-center pt-4 pl-8 gap-x-4">
                 <UIcon name="uil:file" class="w-6 h-6" :class="index === 0 ? 'text-green-400' : 'text-gray-300'" />
@@ -74,9 +37,9 @@
           </div>
           <div class="absolute flex flex-col text-gray-300 transition-opacity duration-300 top-4 left-6 gap-y-4" :class="currentSection === 1 ? 'opacity-100' : 'opacity-0'">
             <div v-for="(text, index) in ['components', 'composables']" :key="index" class="relative">
-              <div class="relative flex items-center gap-x-2" :class="{ 'pt-[59px]': index === 1 }">
+              <div class="relative flex items-center gap-x-2" :class="{ 'pt-[20px] md:pt-[59px]': index === 1 }">
                 <UIcon name="uil:angle-down" class="w-5 h-5 mt-1" /> <span class="font-medium">{{ text }}</span>
-                <div class="absolute left-[9px] w-0.5 bg-gradient-to-b from-gray-400 to-transparent" :class="index === 1 ? 'top-[89px] h-[109px] ' : 'top-[26px] h-[59px] '" />
+                <div class="absolute left-[9px] w-0.5 bg-gradient-to-b from-gray-400 to-transparent" :class="index === 1 ? 'top-[46px] md:top-[89px] h-[109px] ' : 'top-[26px] h-[59px] '" />
               </div>
               <div class="flex items-center pt-4 pl-8 gap-x-4">
                 <UIcon name="uil:file" class="w-6 h-6" :class="index === 0 ? 'text-green-400' : 'text-gray-300'" />
@@ -86,9 +49,9 @@
           </div>
           <div class="absolute flex flex-col text-gray-300 transition-opacity duration-300 top-4 left-6 gap-y-4" :class="currentSection === 2 ? 'opacity-100' : 'opacity-0'">
             <div v-for="(text, index) in ['pages', 'plugins']" :key="index" class="relative">
-              <div class="relative flex items-center gap-x-2" :class="{ 'pt-[59px]': index === 1 }">
+              <div class="relative flex items-center gap-x-2" :class="{ 'pt-[20px] md:pt-[59px]': index === 1 }">
                 <UIcon name="uil:angle-down" class="w-5 h-5 mt-1" /> <span class="font-medium">{{ text }}</span>
-                <div class="absolute left-[9px] w-0.5 bg-gradient-to-b from-gray-400 to-transparent" :class="index === 1 ? 'top-[89px] h-[109px] ' : 'top-[26px] h-[59px] '" />
+                <div class="absolute left-[9px] w-0.5 bg-gradient-to-b from-gray-400 to-transparent" :class="index === 1 ? 'top-[46px] md:top-[89px] h-[109px] ' : 'top-[26px] h-[59px] '" />
               </div>
               <div class="flex items-center pt-4 pl-8 gap-x-4">
                 <UIcon name="uil:file" class="w-6 h-6" :class="index === 0 ? 'text-green-400' : 'text-gray-300'" />
@@ -98,18 +61,18 @@
           </div>
         </div>
         <!-- right container -->
-        <div class="relative w-full">
-          <div class="absolute font-mono text-sm italic text-gray-300 transition-opacity duration-300 top-4 left-4" :class="currentSection === null ? 'opacity-100' : 'opacity-0'">
+        <div class="relative w-full mt-40 md:mt-0">
+          <div class="absolute font-mono text-sm italic text-gray-300 transition-opacity duration-300 top-28 md:top-4 left-4" :class="currentSection === null ? 'opacity-100' : 'opacity-0'">
             Open components, composables or plugins
           </div>
-          <div class="absolute font-mono text-gray-300 transition-opacity duration-300 top-4 left-4" :class="currentSection === 0 ? 'opacity-100' : 'opacity-0'">
+          <div class="absolute font-mono text-gray-300 transition-opacity duration-300 top-28 md:top-4 left-4" :class="currentSection === 0 ? 'opacity-100' : 'opacity-0'">
             <span class="text-gray-300">&lt;template&gt;</span>
             <div v-for="(text, index) in ['Header', 'Footer']" :key="index" class="pl-4">
               <span>&lt;<span class="text-green-400">{{ text }}&nbsp;</span>/&gt;</span>
             </div>
             <span class="text-gray-300">&lt;/template&gt;</span>
           </div>
-          <div class="absolute font-mono text-gray-300 transition-opacity duration-300 top-4 left-4" :class="currentSection === 1 ? 'opacity-100' : 'opacity-0'">
+          <div class="absolute font-mono text-gray-300 transition-opacity duration-300 top-[98px] md:top-4 left-4" :class="currentSection === 1 ? 'opacity-100' : 'opacity-0'">
             <span class="text-gray-300">&lt;template&gt;</span>
             <br>
             <span class="pl-4 text-gray-300">&nbsp;&lt;p&gt;</span>
@@ -142,7 +105,7 @@
               <span class="text-gray-300">&lt;/script&gt;</span>
             </div>
           </div>
-          <div class="absolute font-mono text-gray-300 transition-opacity duration-300 top-4 left-4" :class="currentSection === 2 ? 'opacity-100' : 'opacity-0'">
+          <div class="absolute font-mono text-gray-300 transition-opacity duration-300 top-28 md:top-4 left-4" :class="currentSection === 2 ? 'opacity-100' : 'opacity-0'">
             <span class="text-gray-300">&lt;template&gt;</span>
             <br>
             <span class="text-gray-300">&nbsp;&nbsp;&lt;h1&gt;</span>
