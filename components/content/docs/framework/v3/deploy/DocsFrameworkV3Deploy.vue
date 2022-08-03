04fa3204c9c760afce1da86ca552e6a0bc3bae87
Florent <florentdelerue@hotmail.com>
2022-08-03 14:18:32 +0200

    refactor(docsframeworkv3routing): update

--- components/content/docs/framework/v3/deploy/DocsFrameworkV3Deploy.vue
+++ components/content/docs/framework/v3/deploy/DocsFrameworkV3Deploy.vue
@@ -5,34 +5,34 @@
         v-for="(data, index) in deployData.deploy"
         ref="sections"
         :key="data.title"
-        class="relative flex flex-row p-4 transition duration-200 rounded-md cursor-pointer gap-x-2 group hover:u-bg-gray-50"
+        class="relative flex flex-col p-4 transition duration-200 rounded-md cursor-pointer gap-x-2 group"
         @mouseenter="currentSection = index"
         @mouseleave="currentSection = null"
         @click="currentSection = index"
       >
         <div
           class="absolute top-0 left-0 flex items-center justify-center w-12 h-12 p-3 mt-4 ml-4 transition duration-200 rounded-md group-hover:opacity-0 u-bg-gray-100"
-          :class="{ 'opacity-0': index === currentSection }"
+          :class="currentSection === index ? 'opacity-0' : 'opacity-100'"
         >
           <img
             :src="`/assets/docs/v3/deploy/${data.icon}`"
             :alt="`${data.icon} icon`"
             class="transition duration-200 group-hover:opacity-0"
-            :class="{ 'opacity-0': index === currentSection }"
+            :class="currentSection === index ? 'opacity-0' : 'opacity-100'"
           >
         </div>
         <div
           class="absolute top-0 left-0 flex items-center justify-center w-12 h-12 p-3 mt-4 ml-4 transition duration-200 rounded-md opacity-0 group-hover:opacity-100 bg-gradient-to-b from-green-600 to-green-400"
-          :class="index === currentSection ? 'opacity-100' : 'opacity-0'"
+          :class="currentSection === index ? 'opacity-100' : 'opacity-0'"
         >
           <img
             :src="`/assets/docs/v3/deploy/${data.iconColor}`"
             :alt="`${data.iconColor} icon`"
             class="transition duration-200 opacity-0 group-hover:opacity-100"
-            :class="index === currentSection ? 'opacity-100' : 'opacity-0'"
+            :class="currentSection === index ? 'opacity-100' : 'opacity-0'"
           >
         </div>
-        <div class="flex flex-col pl-16 gap-y-2">
+        <div class="flex flex-col pt-16 md:pt-0 md:pl-16 gap-y-2">
           <h6 class="text-lg font-semibold u-text-gray-900">
             {{ data.title }}
           </h6>
