13cccbc09f285f8c2fdcb635bcf7385b5beb23ab
Clément Ollivier <clement.o2p@gmail.com>
2022-10-04 10:16:53 +0200

    feat: wip on removing v2 docs

--- pages/docs/index.vue
+++ pages/docs/index.vue
@@ -1,7 +1,34 @@
 <template>
-  <div />
+  <Page :sticky="!!$route.params.slug">
+    <template #header>
+      <SubNavbar title="Docs" :links="links">
+        <template #left>
+          <div class="flex items-center gap-3">
+            <NuxtLink to="/docs" class="font-semibold u-text-gray-900 focus:outline-none" tabindex="-1">
+              Docs
+            </NuxtLink>
+          </div>
+        </template>
+
+        <template #right>
+          <UButton icon="fa-brands:github" variant="transparent" to="https://github.com/nuxt/framework" class="!p-0" />
+        </template>
+      </SubNavbar>
+    </template>
+
+    <template v-if="$route.params.slug && tree && tree.length" #aside>
+      <DocsAsideTree :tree="tree" />
+    </template>
+
+    <NuxtPage />
+  </Page>
 </template>
 
 <script setup lang="ts">
-await navigateTo('/docs/3.x')
+const route = useRoute()
+const { navFromPath } = usePage()
+
+const links = computed(() => navFromPath('/docs')?.children.filter(link => !['/docs/migration', '/docs/bridge'].includes(link._path)))
+const path = computed(() => route.path.split('/').slice(0, 4).join('/'))
+const tree = computed(() => navFromPath(path.value)?.children)
 </script>
