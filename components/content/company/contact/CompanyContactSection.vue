e8fc6fa5c01cdbd8c8b969ec217a26f0ed280d04
Clément Ollivier <clement.o2p@gmail.com>
2022-10-07 14:52:09 +0200

    feat: remove unused content and components for mvp

--- components/content/company/contact/CompanyContactSection.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <UContainer padded class="pt-8 pb-16 sm:pb-32">
-    <img src="/assets/company/contact/gradient.svg" class="absolute right-0 hidden lg:block">
-
-    <div class="relative z-[1] flex flex-col gap-12 lg:grid lg:grid-cols-2 lg:gap-20">
-      <ContentSlot :use="$slots.default" />
-    </div>
-  </UContainer>
-</template>
