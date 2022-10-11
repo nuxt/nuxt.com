aee16dc0ff09aa740e9f14b89cce9d3dc0a6d2be
Florent Delerue <florentdelerue@hotmail.com>
2022-10-11 16:46:45 +0200

    feat(sponsors): immplement page

---
+++ components/content/sponsors/SponsorsHero.vue
@@ -0,0 +1,46 @@
+<template>
+  <PageHero centered>
+    <template #title>
+      <ContentSlot :use="$slots.title" unwrap="p" />
+    </template>
+    <template #description>
+      <span class="u-text-black">
+        <ContentSlot :use="$slots.description" unwrap="p" />
+      </span>
+    </template>
+
+    <template #extra>
+      <div class="flex gap-6">
+        <UButton
+          v-for="(button, index) in buttons"
+          :key="index"
+          v-bind="button"
+          size="lg"
+        />
+      </div>
+      <div class="absolute bottom-2 left-0 right-0 w-full h-full flex items-center justify-center -z-[1]">
+        <span class="_ellipse" />
+      </div>
+    </template>
+  </PageHero>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  buttons: {
+    type: Array,
+    default: () => []
+  }
+})
+</script>
+
+<style scoped>
+._ellipse {
+  width: 300.14px;
+  height: 100.22px;
+  inset: 0;
+  filter: blur(60px);
+  background: linear-gradient(97.62deg, rgba(0, 71, 225, 1) 2.27%, rgba(26, 214, 255, 1) 60.88%, #00DC82 80%);
+  transform: matrix(-0.97, 0.24, 0.24, 0.97, 0, 0);
+}
+</style>
---
+++ components/content/sponsors/SponsorsSectionList.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import type { PropType } from 'vue'
+import type { Sponsor } from '../../../types/sponsors'
+
+defineProps({
+  sponsors: {
+    type: Array as PropType<Array<Sponsor>>,
+    default: () => {}
+  },
+  sponsorType: {
+    type: String as PropType<'platinum' | 'silver' | 'gold' | 'sponsor' | 'backers'>,
+    required: true
+  }
+})
+</script>
+<template>
+  <ul :class="['platinum', 'silver', 'gold'].includes(sponsorType) ? 'grid grid-cols-3 gap-12' : 'flex gap-5 flex-wrap px-40'">
+    <li v-for="{ sponsorEntity } in sponsors" :key="sponsorEntity.login">
+      <a :href="`https://github.com/${sponsorEntity.login}`" :title="sponsorEntity.name" target="_blank" rel="noopener noreferrer">
+        <UCard v-if="['platinum', 'silver', 'gold'].includes(sponsorType)" class="w-72" background-class="hover:u-bg-gray-50 transition-colors duration-200">
+          <template #header>
+            <div class="flex justify-center items-center">
+              <UAvatar :rounded="sponsorType === 'silver'" :size="sponsorType === 'silver' ? 'md' : sponsorType === 'gold' ? 'lg' : 'xl'" :src="sponsorEntity.avatarUrl" />
+            </div>
+          </template>
+          <div class="text-center truncate font-semibold text-xl">
+            {{ sponsorEntity.name }}
+          </div>
+        </UCard>
+
+        <UAvatar v-else rounded :size="sponsorType === 'backers' ? 'sm' : 'md'" :src="sponsorEntity.avatarUrl" />
+      </a>
+    </li>
+  </ul>
+</template>
