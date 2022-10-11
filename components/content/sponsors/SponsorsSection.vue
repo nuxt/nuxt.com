9c2e6f80906a7b789deb6f660d8ed3a1ddbab7e6
Florent Delerue <florentdelerue@hotmail.com>
2022-10-11 17:14:35 +0200

    feat(sponsors): improvements

--- components/content/sponsors/SponsorsSection.vue
+++ components/content/sponsors/SponsorsSection.vue
@@ -3,68 +3,14 @@ const { data } = await useFetch('/api/sponsors')
 </script>
 
 <template>
-  <section class="flex flex-col gap-y-12">
-    <article v-for="(value, key) in data.sponsors" :key="key" class="flex flex-col items-center gap-y-12">
-      <h2 class="text-3xl font-semibold u-text-gray-900 capitalize">
-        {{ key }}
-      </h2>
-      <SponsorsSectionList :sponsors="data.sponsors[key]" :sponsor-type="key">
-      <!-- <ul class="flex flex-wrap">
-        <li v-for="{ sponsorEntity } in data.sponsors.platinum" :key="sponsorEntity.login">
-          <a :href="`https://github.com/${sponsorEntity.login}`" :title="sponsorEntity.name" target="_blank" rel="noopener noreferrer">
-            <img :src="sponsorEntity.avatarUrl" :alt="sponsorEntity.name" class="rounded-full w-12">
-          </a>
-        </li>
-      </ul> -->
-      </sponsorssectionlist>
-    </article>
-    <!-- <article class="mb-12">
-      <h2 class="items-center justify-between hidden text-3xl font-semibold u-text-gray-900 md:flex">
-        Gold
-      </h2>
-      <ul class="flex flex-wrap">
-        <li v-for="{ sponsorEntity } in data.sponsors.gold" :key="sponsorEntity.login">
-          <a :href="`https://github.com/${sponsorEntity.login}`" :title="sponsorEntity.name" target="_blank" rel="noopener noreferrer">
-            <img :src="sponsorEntity.avatarUrl" :alt="sponsorEntity.name" class="rounded-full w-12">
-          </a>
-        </li>
-      </ul>
-    </article>
-    <article class="mb-12">
-      <h2 class="items-center justify-between hidden text-3xl font-semibold u-text-gray-900 md:flex">
-        Silver
-      </h2>
-      <ul class="flex flex-wrap">
-        <li v-for="{ sponsorEntity } in data.sponsors.silver" :key="sponsorEntity.login">
-          <a :href="`https://github.com/${sponsorEntity.login}`" :title="sponsorEntity.name" target="_blank" rel="noopener noreferrer">
-            <img :src="sponsorEntity.avatarUrl" :alt="sponsorEntity.name" class="rounded-full w-12">
-          </a>
-        </li>
-      </ul>
-    </article>
-    <article class="mb-12">
-      <h2 class="items-center justify-between hidden text-3xl font-semibold u-text-gray-900 md:flex">
-        Bronze
-      </h2>
-      <ul class="flex flex-wrap">
-        <li v-for="{ sponsorEntity } in data.sponsors.bronze" :key="sponsorEntity.login">
-          <a :href="`https://github.com/${sponsorEntity.login}`" :title="sponsorEntity.name" target="_blank" rel="noopener noreferrer">
-            <img :src="sponsorEntity.avatarUrl" :alt="sponsorEntity.name" class="rounded-full w-12">
-          </a>
-        </li>
-      </ul>
-    </article>
-    <article class="mb-12">
-      <h2 class="items-center justify-between hidden text-3xl font-semibold u-text-gray-900 md:flex">
-        Backers
-      </h2>
-      <ul class="flex flex-wrap">
-        <li v-for="{ sponsorEntity } in data.sponsors.backer" :key="sponsorEntity.login">
-          <a :href="`https://github.com/${sponsorEntity.login}`" :title="sponsorEntity.name" target="_blank" rel="noopener noreferrer">
-            <img :src="sponsorEntity.avatarUrl" :alt="sponsorEntity.name" class="rounded-full w-12">
-          </a>
-        </li>
-      </ul>
-    </article> -->
+  <section>
+    <UContainer class="flex flex-col gap-y-16 pt-16 pb-28">
+      <article v-for="(value, key) in data.sponsors" :key="key" class="flex flex-col items-center gap-y-12">
+        <h2 class="text-4xl font-semibold u-text-gray-900 capitalize">
+          {{ key }}
+        </h2>
+        <SponsorsSectionList :sponsors="data.sponsors[key]" :sponsor-type="key" />
+      </article>
+    </UContainer>
   </section>
 </template>
