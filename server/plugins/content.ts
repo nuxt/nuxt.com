8e496d36b1b84513bb997609ea2ac8b686d14302
Clément Ollivier <clement.o2p@gmail.com>
2022-10-18 15:15:49 +0200

    fix(docs): exclude absolute links from rewrite

--- server/plugins/content.ts
+++ server/plugins/content.ts
@@ -4,7 +4,8 @@ export default defineNitroPlugin((nitroApp) => {
   nitroApp.hooks.hook('content:file:beforeParse', (file) => {
     if (file._id.endsWith('.md')) {
       firstLevels.forEach((level) => {
-        file.body = file.body.replaceAll(`/${level}/`, `/docs/${level}/`)
+        file.body = file.body.replaceAll(`(/${level}/`, `(/docs/${level}/`)
+        file.body = file.body.replaceAll(`"/${level}/`, `"/docs/${level}/`)
       })
     }
   })
