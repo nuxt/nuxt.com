cb376580c1fbb9e5eda2c6b94b78f3f5023a335b
Clément Ollivier <clement.o2p@gmail.com>
2022-10-19 13:16:14 +0200

    fix: allow page to be undefined

--- pages/docs/[...slug].vue
+++ pages/docs/[...slug].vue
@@ -1,5 +1,5 @@
 <template>
-  <DocsPage :toc="page.toc">
+  <DocsPage :toc="page?.toc">
     <div class="prose dark:prose-invert prose-green">
       <ContentRenderer v-if="page" :value="page" />
     </div>
