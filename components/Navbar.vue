71d8158702aca13d53e207b0657616c7d4cbd17e
CarolinaDelerue <99035993+CarolinaDelerue@users.noreply.github.com>
2022-10-20 17:50:49 +0200

    implement SocialLinks component and update nav and footer

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -36,39 +36,7 @@
             </NuxtLink>
           </li>
         </ul>
-
-        <ul class="flex items-center justify-end gap-3">
-          <li>
-            <UButton
-              icon="uil:twitter"
-              variant="transparent"
-              to="https://twitter.com/nuxt_js"
-              target="_blank"
-              class="!p-0 u-text-gray-900"
-              title="Go to Nuxt Twitter Account"
-            />
-          </li>
-          <li>
-            <UButton
-              icon="fa-brands:discord"
-              variant="transparent"
-              to="https://discord.com/invite/ps2h6QT"
-              target="_blank"
-              class="!p-0 u-text-gray-900 focus-visible:ring-2"
-              title="Go to Nuxt Discord Server"
-            />
-          </li>
-          <li>
-            <UButton
-              icon="uil:github"
-              variant="transparent"
-              to="https://github.com/nuxt/framework"
-              target="_blank"
-              class="!p-0 u-text-gray-900"
-              title="Go to Nuxt Github page"
-            />
-          </li>
-        </ul>
+        <SocialLinks />
       </nav>
     </UContainer>
   </header>
