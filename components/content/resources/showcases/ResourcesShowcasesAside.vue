661fa7a7ff0e2e7803cecadf551eea7e46f626b1
CarolinaDelerue <99035993+CarolinaDelerue@users.noreply.github.com>
2022-10-25 17:59:30 +0200

    fix showcase page

--- components/content/resources/showcases/ResourcesShowcasesAside.vue
+++ components/content/resources/showcases/ResourcesShowcasesAside.vue
@@ -1,18 +1,26 @@
 <template>
   <nav>
-    <p class="py-1.5 font-semibold u-text-gray-900">
+    <p class="py-1.5 font-semibold u-text-gray-900 text-lg">
       Categories
     </p>
 
-    <ul v-if="categories.length" class="flex flex-col py-4 gap-y-2">
-      <li v-for="category in categories" :key="category.id" class="inline-flex items-center">
-        <Icon v-if="category.icon" :name="category.icon" class="w-5 h-5 mr-1" />
+    <ul v-if="categories.length" class="flex flex-col py-4 gap-y-3">
+      <li v-for="category in categories" :key="category.id" class="inline-flex items-center group">
+        <Icon
+          v-if="category.icon"
+          :name="category.icon"
+          class="w-5 h-5 mr-1 "
+          :class="{
+            'u-text-gray-900': selectedCategory?.name === category.name,
+            'text-gray-400 dark:text-gray-500 group-hover:u-text-gray-900': selectedCategory?.name !== category.name
+          }"
+        />
         <NuxtLink
           :to="category.to"
           class="relative flex"
           :class="{
             'u-text-gray-900 font-medium': selectedCategory?.name === category.name,
-            'u-text-gray-500 hover:u-text-gray-900 focus:u-text-gray-900': selectedCategory?.name !== category.name
+            'text-gray-500 dark:text-gray-400 group-hover:u-text-gray-900 focus:u-text-gray-900': selectedCategory?.name !== category.name
           }"
         >
           <span class="relative">
