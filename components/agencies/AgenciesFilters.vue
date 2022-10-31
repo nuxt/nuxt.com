eba5b051dd7d75c01fa731749ac04bcfe83012dd
Clément Ollivier <clement.o2p@gmail.com>
2022-10-31 14:47:27 +0100

    chore: move agencies components outside content dir

---
+++ components/agencies/AgenciesFilters.vue
@@ -0,0 +1,23 @@
+<template>
+  <div v-if="filters.length" class="flex items-center gap-1.5">
+    <UButton
+      v-for="filter of filters"
+      :key="filter.key"
+      :label="filter.title"
+      :to="filter.to"
+      icon="uil:multiply"
+      variant="gray"
+      trailing
+      rounded
+      size="sm"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+const { selectedService, selectedRegion } = useAgencyPartners()
+
+const filters = computed(() => {
+  return [selectedService.value, selectedRegion.value].filter(Boolean)
+})
+</script>
