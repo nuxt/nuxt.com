48796581de7ba68928ffd91e1ce7abc36db7b928
Florent Delerue <florentdelerue@hotmail.com>
2022-11-07 12:34:17 +0100

    refactor: heroes (#908)

--- components/content/ShowcasesHero.vue
+++ components/content/ShowcasesHero.vue
@@ -8,21 +8,27 @@
     </template>
 
     <template #extra>
-      <div class="flex gap-3">
+      <div class="flex gap-3 flex-col sm:flex-row items-center">
         <span class="u-text-gray-500">Download extension:</span>
-        <div class="flex gap-6">
-          <GradientNuxtLink to="https://chrome.google.com/webstore/detail/vue-telescope/neaebjphlfplgdhedjdhcnpjkndddbpd" target="_blank" class="font-semibold u-text-gray-900">
+        <div class="flex flex-col sm:flex-row gap-6 justify-center items-center">
+          <UButton variant="secondary" icon="logos:chrome" to="https://chrome.google.com/webstore/detail/vue-telescope/neaebjphlfplgdhedjdhcnpjkndddbpd" target="_blank">
             Chrome
-          </GradientNuxtLink>
-          <GradientNuxtLink to="https://addons.mozilla.org/en/firefox/addon/vue-telescope/" target="_blank" class="font-semibold u-text-gray-900">
+          </UButton>
+          <UButton variant="secondary" icon="logos:firefox" to="https://addons.mozilla.org/en/firefox/addon/vue-telescope/" target="_blank">
             Firefox
-          </GradientNuxtLink>
+          </UButton>
+          <UButton variant="secondary" icon="logos:microsoft-edge" to="https://microsoftedge.microsoft.com/addons/detail/vue-telescope/icgcillpgelpleniodgkmohgdmeogodl" target="_blank">
+            Edge
+          </UButton>
         </div>
       </div>
     </template>
 
     <template #image>
-      <img src="/assets/resources/showcases/gems.svg" class="object-contain h-full lg:mx-10 opacity-10 lg:opacity-100" role="presentation">
+      <div class="flex h-full items-center justify-center">
+        <img src="/assets/resources/showcases/hero-light.svg" class="dark:hidden object-contain h-3/4 lg:mx-10 opacity-0 md:opacity-100" role="presentation">
+        <img src="/assets/resources/showcases/hero-dark.svg" class="hidden dark:block object-contain h-3/4 lg:mx-10 opacity-0 md:opacity-100" role="presentation">
+      </div>
     </template>
   </PageHero>
 </template>
