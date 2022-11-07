a6cce2270abfc37211b3229d93a99c49a9e38f73
Florent Delerue <florentdelerue@hotmail.com>
2022-11-07 12:34:17 +0100

    refactor: heroes (#908)

--- components/modules/ModulesHero.vue
+++ components/modules/ModulesHero.vue
@@ -7,52 +7,10 @@
       Discover our list of modules to supercharge your Nuxt project. Created by the Nuxt team and community.
     </template>
     <template #image>
-      <img src="/assets/modules/gems.svg" class="object-cover h-full lg:mx-10 opacity-10 lg:opacity-100" role="presentation">
-
-      <UCard padded body-class="p-4" class="hidden lg:block absolute left-11 top-3 w-44 backdrop-blur-lg !bg-opacity-10">
-        <Icon name="uil:arrow-to-bottom" class="w-5 h-5 text-teal-400" />
-
-        <div>
-          <p class="mb-2 text-4xl font-semibold u-text-gray-900">
-            {{ formatNumber(stats.downloads, 1) }}
-          </p>
-          <p class="text-xs u-text-gray-500">
-            Downloads Last 30 Days
-          </p>
-        </div>
-      </UCard>
-
-      <UCard padded body-class="p-4" class="hidden lg:block absolute -left-10 bottom-0 w-32 backdrop-blur-lg !bg-opacity-10">
-        <Icon name="uil:puzzle-piece" class="w-5 h-5 text-green-400" />
-
-        <div>
-          <p class="mb-2 text-4xl font-semibold u-text-gray-900">
-            {{ formatNumber(stats.modules) }}
-          </p>
-          <p class="text-xs u-text-gray-500">
-            Modules
-          </p>
-        </div>
-      </UCard>
-
-      <UCard padded body-class="p-4" class="hidden lg:block absolute right-0 -bottom-10 w-32 backdrop-blur-lg !bg-opacity-10">
-        <Icon name="uil:users-alt" class="w-5 h-5 text-indigoblue-600" />
-
-        <div>
-          <p class="mb-2 text-4xl font-semibold u-text-gray-900">
-            {{ formatNumber(stats.contributors) }}
-          </p>
-          <p class="text-xs u-text-gray-500">
-            Contributors
-          </p>
-        </div>
-      </UCard>
+      <div class="h-full flex items-center justify-center">
+        <img src="/assets/modules/hero-light.svg" class="dark:hidden object-cover h-3/4 lg:mx-10 opacity-0 md:opacity-100" role="presentation">
+        <img src="/assets/modules/hero-dark.svg" class="hidden dark:block object-cover h-3/4 lg:mx-10 opacity-0 md:opacity-100" role="presentation">
+      </div>
     </template>
   </PageHero>
 </template>
-
-<script setup lang="ts">
-import { formatNumber } from '~/utils'
-
-const { stats } = useModules()
-</script>
