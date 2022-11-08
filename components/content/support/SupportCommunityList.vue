1d1f3e5cd8dd64dafdbd63996e81c62f4e9d8882
Florent Delerue <florentdelerue@hotmail.com>
2022-11-08 15:21:58 +0100

    refactor(support): page (#918)
    * refactor(support): page
    
    * refactor(support): update experts network list

---
+++ components/content/support/SupportCommunityList.vue
@@ -0,0 +1,25 @@
+<template>
+  <li class="flex flex-col sm:flex-row gap-4">
+    <img :src="`/assets/support/solutions/${icon}-light.svg`" class="w-[44px] h-[44px] dark:hidden">
+    <img :src="`/assets/support/solutions/${icon}.svg`" class="w-[44px] h-[44px] hidden dark:block">
+
+    <div class="flex flex-col gap-y-2">
+      <h2 class="u-text-gray-900 text-xl font-semibold">
+        <ContentSlot :use="$slots.title" unwrap="p" />
+      </h2>
+
+      <p class="u-text-gray-800">
+        <ContentSlot :use="$slots.description" unwrap="p" />
+      </p>
+    </div>
+  </li>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  icon: {
+    type: String,
+    default: ''
+  }
+})
+</script>
