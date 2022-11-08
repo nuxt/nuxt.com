383979c8f8ea0d159f6987f411cc4aa1ce714cb8
Florent Delerue <florentdelerue@hotmail.com>
2022-11-08 17:17:14 +0100

    refactor(home): improve companies carousel

--- components/content/home/HomeCompaniesCarousel.vue
+++ components/content/home/HomeCompaniesCarousel.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="relative flex flex-col justify-center">
-    <div class="hidden sm:block absolute left-0 w-40 inset-y-0 bg-gradient-to-r from-white dark:from-black to-transparent z-10" />
-    <div class="hidden sm:block absolute right-0 w-40 inset-y-0 bg-gradient-to-l from-white dark:from-black to-transparent z-10" />
+    <div class="hidden sm:block absolute left-0 w-20 inset-y-0 bg-gradient-to-r from-white via-white dark:via-black dark:from-black to-transparent z-10" />
+    <div class="hidden sm:block absolute right-0 w-20 inset-y-0 bg-gradient-to-l from-white via-white dark:via-black dark:from-black to-transparent z-10" />
 
     <ClientOnly>
       <Swiper
@@ -16,9 +16,30 @@
         :loop-additional-slides="selectedShowcases.length"
         :autoplay="autoplay"
         :speed="1000"
+        :slide-to-clicked-slide="false"
+        @slideChangeTransitionEnd="slideChangeTransitionEnd"
+        @slideNextTransitionStart="slideNextTransitionStart"
+        @click="click"
       >
         <SwiperSlide v-for="(showcase, i) in selectedShowcases" :key="i" :style="{ height: '160px' }">
-          <HomeCompaniesCarouselItem :showcase="showcase" />
+          <Component :is="transition.end === true && realIndex === i ? 'a' : 'div'" :href="showcase.url" target="_blank">
+            <div class="absolute inset-0 z-10 opacity-70 backdrop-blur-[2px] u-bg-white transition-opacity duration-300" :class="transition.end === true && realIndex === i ? '-z-[1] blur-[8px] u-bg-gray-900 opacity-100' : 'opacity-70 backdrop-blur-[2px] u-bg-white'" />
+
+            <img
+              :src="`https://res.cloudinary.com/nuxt/image/upload/f_auto,q_auto/${showcase.screenshotUrl}`"
+              :alt="showcase.hostname"
+              loading="lazy"
+              class="object-cover object-top w-full h-full px-4 sm:px-0"
+            >
+            <div class="px-4 py-3 text-center content transition-opacity duration-300" :class="transition.end === true ? 'opacity-100' : 'opacity-0'">
+              <h2 class="font-semibold truncate u-text-gray-900 text-xl">
+                {{ showcase.title || showcase.hostname }}
+              </h2>
+              <p class="truncate text-green-400">
+                {{ showcase.hostname }}
+              </p>
+            </div>
+          </Component>
         </Swiperslide>
       </Swiper>
     </ClientOnly>
@@ -39,9 +60,11 @@ import 'swiper/css/effect-coverflow'
 import 'swiper/css/pagination'
 
 const swiper = ref(null)
+const realIndex = ref(0)
+const activeIndex = ref(0)
 
 // swiper autoplay options
-const autoplay = { delay: 3000, pauseOnMouseEnter: true, disableOnInteraction: false }
+const autoplay = { delay: 3000, pauseOnMouseEnter: false, disableOnInteraction: false }
 
 const { smaller } = useBreakpoints(breakpointsTailwind)
 
@@ -57,6 +80,26 @@ const slidesPerView = computed(() => {
     return 3
   }
 })
+
+const transition = reactive({ end: false })
+
+const slideChangeTransitionEnd = (swiper) => {
+  transition.end = true
+  realIndex.value = swiper.realIndex
+  activeIndex.value = swiper.activeIndex
+}
+
+const slideNextTransitionStart = () => {
+  transition.end = false
+}
+
+const click = (swiper) => {
+  if (swiper.clickedIndex < activeIndex.value) {
+    swiper.slidePrev()
+  } else if (swiper.clickedIndex > activeIndex.value) {
+    swiper.slideNext()
+  }
+}
 
 const { list, selectedCategory } = useResourcesShowcases()
 
@@ -107,5 +150,17 @@ const selectedShowcases = computed(() => {
 .swiper-slide img {
   width: 100%;
   height: 160px;
+}
+
+.slide {
+  @apply absolute inset-0 z-10 opacity-70 backdrop-blur-[2px] u-bg-white
+}
+
+.swiper-slide-active > a > .content {
+  @apply block
+}
+
+.content {
+  @apply hidden
 }
 </style>
