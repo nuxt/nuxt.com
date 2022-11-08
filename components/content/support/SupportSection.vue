6792a5bfb7532ee5f2e7d43279003941528b8f1a
Florent Delerue <florentdelerue@hotmail.com>
2022-11-08 15:21:58 +0100

    refactor(support): page (#918)
    * refactor(support): page
    
    * refactor(support): update experts network list

--- components/content/support/SupportSection.vue
+++ components/content/support/SupportSection.vue
@@ -1,9 +1,42 @@
 <template>
-  <UContainer padded class="pt-8 pb-16">
-    <img src="/assets/support/solutions/gradient.svg" class="absolute right-0 hidden lg:block">
+  <UContainer padded class="pt-8 pb-[120px] flex flex-col gap-y-8">
+    <img :src="`/assets/support/solutions/${image}-light.svg`" class="dark:hidden w-[74px] h-[81px]">
+    <img :src="`/assets/support/solutions/${image}.svg`" class="hidden dark:block w-[74px] h-[81px]">
 
-    <div class="relative flex flex-col gap-12 lg:grid lg:grid-cols-2 lg:gap-20">
-      <ContentSlot :use="$slots.default" />
+    <h1 class="u-text-gray-900 text-4xl font-semibold">
+      <ContentSlot :use="$slots.title" unwrap="p" />
+    </h1>
+
+    <p class="u-text-gray-800 text-lg">
+      <ContentSlot :use="$slots.description" unwrap="p" />
+    </p>
+
+    <div v-if="$slots.extra">
+      <ContentSlot :use="$slots.extra" unwrap="p" />
     </div>
+
+    <ul v-if="$slots.list" class="gap-y-6 flex flex-col">
+      <ContentSlot :use="$slots.list" unwrap="p" />
+    </ul>
+
+    <NuxtLink v-if="to" :to="to" class="u-text-gray-900 flex gap-x-4 items-center relative group w-fit font-semibold">
+      <ContentSlot :use="$slots.link" unwrap="p" />
+      <Icon name="heroicons-solid:arrow-right" class="w-4 h-4 mt-1" />
+      <span class="absolute w-0 -bottom-1 h-[1px] u-bg-gray-900 group-hover:w-full transition-all duration-200" />
+    </NuxtLink>
   </UContainer>
 </template>
+
+<script setup lang="ts">
+
+defineProps({
+  image: {
+    type: String,
+    default: ''
+  },
+  to: {
+    type: String,
+    default: ''
+  }
+})
+</script>
