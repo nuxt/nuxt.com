1310a29d2c161e8e9225cf2b116f053f75966d0d
Clément Ollivier <clement.o2p@gmail.com>
2022-11-14 12:28:28 +0100

    feat(modules): add title to links

--- components/content/modules/ModulesListItem.vue
+++ components/content/modules/ModulesListItem.vue
@@ -37,7 +37,7 @@
       </div>
     </div>
 
-    <NuxtLink :to="`/modules/${module.name}`">
+    <NuxtLink :to="`/modules/${module.name}`" :title="module.name">
       <span class="absolute inset-0" aria-hidden="true" />
     </NuxtLink>
   </UCard>
