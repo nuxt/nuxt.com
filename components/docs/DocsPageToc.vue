249fc0f628a0eada3dd721b06f4af915b5691315
CarolinaDelerue <99035993+CarolinaDelerue@users.noreply.github.com>
2022-11-14 15:51:49 +0100

    add mastering nuxt

--- components/docs/DocsPageToc.vue
+++ components/docs/DocsPageToc.vue
@@ -28,20 +28,27 @@ const sponsor = computed(() => pickOne([...sponsors.value?.platinum, ...sponsors
         Sponsored by:
       </span>
       <div>
-        <NuxtLink :to="sponsor.sponsorUrl" class="block flex items-center bg-white dark:bg-gray-900 rounded-xl h-[60px] p-4 lg:p-2 align-middle border border-gray-200 dark:border-gray-800">
+        <NuxtLink :to="sponsor.sponsorUrl" target="_blank" class="block flex items-center bg-white dark:bg-gray-900 rounded-xl h-[60px] p-4 lg:p-2 align-middle border border-gray-200 dark:border-gray-800">
           <img :src="sponsor.sponsorLogo" role="presentation" class="mr-2 rounded-md h-8">
           <p class="font-semibold truncate">
             {{ sponsor.sponsorName }}
           </p>
         </NuxtLink>
       </div>
-      <div class="relative h-[68px] p-4 lg:p-2 flex items-center justify-between bg-white dark:bg-gray-900 rounded-xl h-[60px] border border-gray-200 dark:border-gray-800">
+      <NuxtLink to="https://masteringnuxt.com/nuxt3?ref=nuxt" target="_blank" class="h-[72px] p-4 lg:p-2 flex flex-col items-start justify-between bg-white dark:bg-gray-900 rounded-xl h-[60px] border border-gray-200 dark:border-gray-800">
+        <img src="/assets/toc/mastering-nuxt.svg" class="hidden dark:block h-6 lg:h-full">
+        <img src="/assets/toc/mastering-nuxt-light.svg" class="dark:hidden h-6 lg:h-full">
+        <p class="text-sm">
+          Learn Nuxt 3 with experts.
+        </p>
+      </NuxtLink>
+      <NuxtLink to="/jobs" target="_blank" class="relative h-[68px] p-4 lg:p-2 flex items-center justify-between bg-white dark:bg-gray-900 rounded-xl h-[60px] border border-gray-200 dark:border-gray-800">
         <p class="font-semibold">
           Looking for a Nuxt Jobs?
         </p>
         <img src="/assets/toc/jobs.svg" class="hidden dark:block absolute right-0 bottom-0 w-12 lg:w-10">
         <img src="/assets/toc/jobs-light.svg" class="dark:hidden absolute right-0 bottom-0 w-12 lg:w-10">
-      </div>
+      </NuxtLink>
     </LazyClientOnly>
   </nav>
 </template>
