1fb4c6093e629b9a86926c07d78d037f0b4f47d6
Sébastien Chopin <seb@nuxtjs.com>
2022-11-15 23:52:12 +0100

    chore: force generate

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -107,37 +107,37 @@ export default defineNuxtConfig({
     prerender: {
       routes: ['/docs', '/'],
       crawlLinks: true
-    },
-    hooks: {
-      'prerender:generate': (route) => {
-        const prerenderedRoutes = [
-          '/',
-          '/design-kit',
-          '/support/solutions',
-          '/support/agencies',
-          /^\/docs/,
-          /^\/api\/_content/
-        ]
-
-        route.skip = true
-
-        prerenderedRoutes.forEach((condition) => {
-          if (typeof condition === 'string') {
-            if (condition === route.route) { route.skip = false }
-          } else if (condition.test(route.route)) { route.skip = false }
-        })
-      }
     }
-  },
-  routeRules: {
-    // prerender is not yet implemented, using nitro.prerender.routes and hooks for it in the meantime
-    // '/': { prerender: true },
-    // '/docs/**': { prerender: true },
-    '/**': { cache: { swr: true, maxAge: 120, staleMaxAge: 60, headersOnly: true } },
-    '/docs': { redirect: '/docs/getting-started/installation' }
-    // '/modules/**': { swr: 60 },
-    // '/partners/**': { swr: 60 },
-    // '/showcase': { swr: 60 },
-    // '/api/**': { swr: 60 }
+    // hooks: {
+    //   'prerender:generate': (route) => {
+    //     const prerenderedRoutes = [
+    //       '/',
+    //       '/design-kit',
+    //       '/support/solutions',
+    //       '/support/agencies',
+    //       /^\/docs/,
+    //       /^\/api\/_content/
+    //     ]
+
+    //     route.skip = true
+
+    //     prerenderedRoutes.forEach((condition) => {
+    //       if (typeof condition === 'string') {
+    //         if (condition === route.route) { route.skip = false }
+    //       } else if (condition.test(route.route)) { route.skip = false }
+    //     })
+    //   }
+    // }
   }
+  // routeRules: {
+  //   // prerender is not yet implemented, using nitro.prerender.routes and hooks for it in the meantime
+  //   // '/': { prerender: true },
+  //   // '/docs/**': { prerender: true },
+  //   '/**': { cache: { swr: true, maxAge: 120, staleMaxAge: 60, headersOnly: true } },
+  //   '/docs': { redirect: '/docs/getting-started/installation' }
+  //   // '/modules/**': { swr: 60 },
+  //   // '/partners/**': { swr: 60 },
+  //   // '/showcase': { swr: 60 },
+  //   // '/api/**': { swr: 60 }
+  // }
 })
