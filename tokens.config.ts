a9f13f82e2a6e06355756a137625e324e7b75d0d
Sébastien Chopin <seb@nuxtjs.com>
2022-11-17 16:21:40 +0100

    chore: update deps and link typo tokens

--- tokens.config.ts
+++ tokens.config.ts
@@ -1,5 +1,6 @@
 import { defineTheme, palette } from 'pinceau'
 
+// See Typography config on https://github.com/nuxt-themes/typography/blob/main/tokens.config.ts
 export default defineTheme({
   typography: {
     colors: {
