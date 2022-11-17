652173cdc7e23488e1289e9aeba6e484d7aa5f11
Florent Delerue <florentdelerue@hotmail.com>
2022-11-17 12:40:47 +0100

    refactor(sponsorssectionList): rounded border on mobile for cards

--- components/content/support/sponsors/SponsorsSectionList.vue
+++ components/content/support/sponsors/SponsorsSectionList.vue
@@ -17,7 +17,7 @@ defineProps({
   <ul class="w-full" :class="['platinum', 'silver', 'gold'].includes(sponsorType) ? 'grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-12' : 'flex gap-5 flex-wrap'">
     <li v-for="{ sponsorId, sponsorUrl, sponsorLogo, sponsorName } in sponsors" :key="sponsorId">
       <a :href="sponsorUrl" :title="sponsorName" target="_blank" rel="noopener noreferrer">
-        <UCard v-if="['platinum', 'silver', 'gold'].includes(sponsorType)" class="w-full" background-class="hover:u-bg-gray-50 transition-colors duration-200">
+        <UCard v-if="['platinum', 'silver', 'gold'].includes(sponsorType)" class="w-full rounded-xl " background-class="hover:u-bg-gray-50 transition-colors duration-200">
           <template #header>
             <div class="flex justify-center items-center">
               <UAvatar :rounded="sponsorType === 'silver'" :size="sponsorType === 'silver' ? 'md' : sponsorType === 'gold' ? 'lg' : 'xl'" :src="sponsorLogo" />
