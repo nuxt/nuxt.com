5d447156f47311b301758b2eca03b8077ac41aab
CarolinaDelerue <99035993+CarolinaDelerue@users.noreply.github.com>
2022-11-18 16:37:54 +0100

    refactor(homecarditem): external links on stats section

--- components/content/home/HomeCardItem.vue
+++ components/content/home/HomeCardItem.vue
@@ -1,52 +1,51 @@
 <template>
-  <li ref="root" class="rounded-md relative" :class="{ 'cursor-pointer': to }" @click="onClick">
-    <div class="hidden gradient-border gradient-border-dark dark:block" />
-    <div class="dark:hidden gradient-border gradient-border-light" />
-    <UCard
-      class="h-full w-ful flex flex-col justify-between rounded-xl"
-      :body-class="bodyClass"
-      shadow-class="shadow-none"
-      background-class="dark:bg-gray-900/50 bg-white hover:dark:bg-gray-900"
-      :header-class="headerClass"
-      ring-class="ring-1 ring-gray-200 dark:ring-0 hover:ring-0"
-    >
-      <template v-if="image" #header>
-        <img
-          :src="`/assets/home/${image.dark}`"
-          :alt="`${image} image`"
-          class="h-full rounded-md hidden dark:block"
-          :width="imageWidth"
-          :height="imageHeight"
-          loading="lazy"
-        >
-        <img
-          :src="`/assets/home/${image.light}`"
-          :alt="`${image} image`"
-          class="h-full rounded-md dark:hidden"
-          :width="imageWidth"
-          :height="imageHeight"
-          loading="lazy"
-        >
-      </template>
-      <div class="flex flex-col" :class="contentClass">
-        <Icon v-if="icon" :name="icon" class="w-6 h-6" />
-        <header class="font-semibold u-text-gray-900" :class="!icon ? 'text-xl' : 'text-5xl'">
-          <NuxtLink v-if="to" :to="to">
+  <li ref="root" class="rounded-md relative group">
+    <UIcon name="uil:external-link-alt" class="absolute right-4 top-4 text-gray-500 transition-opacity duration-200 opacity-0" :class="{ 'group-hover:opacity-100': to && target === '_blank' }" />
+    <component :is="component" :href="to" target="_blank">
+      <div class="hidden gradient-border gradient-border-dark dark:block" />
+      <div class="dark:hidden gradient-border gradient-border-light" />
+      <UCard
+        class="h-full w-ful flex flex-col justify-between rounded-xl"
+        :body-class="bodyClass"
+        shadow-class="shadow-none"
+        background-class="dark:bg-gray-900/50 bg-white hover:dark:bg-gray-900"
+        :header-class="headerClass"
+        ring-class="ring-1 ring-gray-200 dark:ring-0 hover:ring-0"
+      >
+        <template v-if="image" #header>
+          <img
+            :src="`/assets/home/${image.dark}`"
+            :alt="`${image} image`"
+            class="h-full rounded-md hidden dark:block"
+            :width="imageWidth"
+            :height="imageHeight"
+            loading="lazy"
+          >
+          <img
+            :src="`/assets/home/${image.light}`"
+            :alt="`${image} image`"
+            class="h-full rounded-md dark:hidden"
+            :width="imageWidth"
+            :height="imageHeight"
+            loading="lazy"
+          >
+        </template>
+        <div class="flex flex-col" :class="contentClass">
+          <Icon v-if="icon" :name="icon" class="w-6 h-6" />
+          <header class="font-semibold u-text-gray-900" :class="!icon ? 'text-xl' : 'text-5xl'">
             <ContentSlot :use="$slots.title" unwrap="p" />
-          </NuxtLink>
-          <ContentSlot v-else :use="$slots.title" unwrap="p" />
-        </header>
-        <p class="u-text-gray-500" :class="{ 'text-lg font-medium': icon }">
-          <ContentSlot :use="$slots.description" unwrap="p" />
-        </p>
-      </div>
-    </UCard>
+          </header>
+          <p class="u-text-gray-500" :class="{ 'text-lg font-medium': icon }">
+            <ContentSlot :use="$slots.description" unwrap="p" />
+          </p>
+        </div>
+      </UCard>
+    </component>
   </li>
 </template>
 
 <script setup lang="ts">
 import type { PropType } from 'vue'
-import { hasProtocol } from 'ufo'
 
 const props = defineProps({
   image: {
@@ -74,6 +73,10 @@ const props = defineProps({
     type: String,
     default: ''
   },
+  target: {
+    type: String,
+    default: '_blank'
+  },
   headerClass: {
     type: String,
     default: 'px-4 pt-5 sm:px-6 justify-center'
@@ -88,11 +91,6 @@ const props = defineProps({
   }
 })
 
-const onClick = () => {
-  if (props.to && !window.getSelection().toString()) {
-    navigateTo(props.to, { external: hasProtocol(props.to) })
-  }
-}
 const headerClass = computed(() => {
   return [
     'flex items-center border-none',
@@ -100,19 +98,9 @@ const headerClass = computed(() => {
   ].join(' ')
 })
 
-// Solution to get great block links
-// From https://css-tricks.com/block-links-the-search-for-a-perfect-solution/#aa-method-4-sprinkle-javascript-on-the-second-method
-const root = ref(null)
-const stopPropagation = (e: Event) => e.stopPropagation()
-onMounted(() => {
-  Array.from(root.value.querySelectorAll('a')).forEach((el: Element) => {
-    el.addEventListener('click', stopPropagation)
-  })
-})
-onBeforeUnmount(() => {
-  Array.from(root.value.querySelectorAll('a')).forEach((el: Element) => {
-    el.removeEventListener('click', stopPropagation)
-  })
+const component = computed(() => {
+  if (props.to) { return resolveComponent('NuxtLink') }
+  return 'div'
 })
 </script>
 
