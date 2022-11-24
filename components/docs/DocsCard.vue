2c4e2e741721ba8894bbaf2fa5d4a3315f0d769d
CarolinaDelerue <99035993+CarolinaDelerue@users.noreply.github.com>
2022-11-24 17:37:53 +0100

    wip

--- components/docs/DocsCard.vue
+++ components/docs/DocsCard.vue
@@ -1,19 +1,33 @@
 <template>
-  <li class="flex justify-between border border-gray-200 dark:border-gray-800 rounded-lg p-2">
+  <li class="relative flex justify-between border border-gray-200 dark:border-gray-800 rounded-lg p-4">
     <div>
-      <h1 v-if="$slots.title" class="text-4xl font-semibold sm:text-5xl u-text-gray-900">
+      <h2 v-if="$slots.title" class="text-3xl font-semibold sm:text-5xl u-text-gray-900 mb-2 mt-2">
         <ContentSlot :use="$slots.title" unwrap="p" />
-      </h1>
-      <h2 v-if="$slots.extraTitle" class="font-semibold sm:text-lg u-text-gray-900">
+      </h2>
+      <h2 v-if="$slots.extraTitle" class="font-semibold u-text-gray-900" :class="fontSizeClass">
         <ContentSlot :use="$slots.extraTitle" unwrap="p" />
       </h2>
-      <p class="font-medium sm:text-lg u-text-gray-500">
-        <ContentSlot :use="$slots.description" unwrap="p" class="lg:w-2/3" />
+      <p class="u-text-gray-500">
+        <ContentSlot :use="$slots.description" unwrap="p" />
       </p>
-      <div v-if="$slots.image" class="flex h-full w-full gap-x-4 items-end justify-center">
-        <img src="`/assets/docs/getting-started/views/${image}`" width="143" height="136" class="dark:hidden object-cover h-[136px] w-[143px] opacity-0 md:opacity-100" role="presentation">
-        <img src="/assets/docs/getting-started/views/hero-dark.png" width="143" height="136" class="hidden dark:block object-cover h-[136px] w-[143px] opacity-0 md:opacity-100" role="presentation">
-      </div>
+    </div>
+    <div v-if="image" class="flex flex-col h-full w-full gap-x-4 items-end justify-center">
+      <img :src="`/assets/docs/getting-started/views/${image}-light.png`" width="72" height="92" class="dark:hidden absolute right-0 bottom-0 object-cover h-[52px] w-[42px] md:h-[92px] md:w-[72px] opacity-100" role="presentation">
+      <img :src="`/assets/docs/getting-started/views/${image}-dark.png`" width="72" height="92" class="hidden dark:block absolute right-0 bottom-0 object-cover h-[52px] w-[42px] md:h-[92px] md:w-[72px] opacity-100" role="presentation">
     </div>
   </li>
 </template>
+
+<script setup lang="ts">
+
+defineProps({
+  image: {
+    type: String,
+    default: ''
+  },
+  fontSizeClass: {
+    type: String,
+    default: ''
+  }
+})
+</script>
