2dff42c003c2ae05efe63fc1b6bb0292f9741f60
CarolinaDelerue <99035993+CarolinaDelerue@users.noreply.github.com>
2022-11-29 17:36:31 +0100

    wip

--- components/docs/community/CommunityHero.vue
+++ components/docs/community/CommunityHero.vue
@@ -1,38 +1,38 @@
 <template>
-  <div class="flex justify-between gap-y-4">
-    <div class="flex flex-col gap-6">
-      <h1 class="text-4xl font-semibold sm:text-5xl u-text-gray-900">
-        <ContentSlot :use="$slots.title" unwrap="p" />
-      </h1>
-      <p class="font-medium sm:text-lg u-text-gray-500">
-        <ContentSlot :use="$slots.description" unwrap="p" class="lg:w-2/3" />
-      </p>
-    </div>
-    <div v-if="imagePath" class="flex h-full w-full gap-x-4 items-end justify-center hidden md:block">
-      <img
-        :src="`${imagePath}-light.png`"
-        alt=""
-        width="143"
-        height="136"
-        class="dark:hidden object-cover h-[136px] w-[143px] opacity-0 md:opacity-100"
-        role="presentation"
-      >
-      <img
-        :src="`${imagePath}-dark.png`"
-        alt=""
-        width="143"
-        height="136"
-        class="hidden dark:block object-cover h-[136px] w-[143px] opacity-0 md:opacity-100"
-        role="presentation"
-      >
-    </div>
-  </div>
+  <DocsHero :image="image">
+    <template #title>
+      <ContentSlot :use="$slots.title" unwrap="p" />
+    </template>
+    <template #description>
+      <ContentSlot :use="$slots.description" unwrap="p" />
+    </template>
+
+    <template #extra>
+      <div class="flex flex-col sm:flex-row gap-3 items-start justify-center md:justify-start sm:items-center">
+        <UInput
+          name="email"
+          placeholder="Enter your email"
+          class="w-full sm:w-64"
+          size="lg"
+          required
+        />
+        <UButton variant="primary-gradient" to="https://chrome.google.com/webstore/detail/vue-telescope/neaebjphlfplgdhedjdhcnpjkndddbpd" target="_blank">
+          {{ buttonText }}
+        </UButton>
+      </div>
+    </template>
+  </DocsHero>
 </template>
+
 <script setup lang="ts">
 defineProps({
-  imagePath: {
+  image: {
+    type: Object,
+    default: () => {}
+  },
+  buttonText: {
     type: String,
-    default: ''
+    default: 'Subscribe'
   }
 })
 </script>
