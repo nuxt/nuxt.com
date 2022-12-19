7235de7d45a3c0f47f30c6e4f6a600fb8b2dd7d7
Carolina Delerue <99035993+CarolinaDelerue@users.noreply.github.com>
2022-12-19 14:49:51 +0100

    refactor(docs nav): accessibility (#1107)

--- components/docs/DocsTocLinks.vue
+++ components/docs/DocsTocLinks.vue
@@ -42,12 +42,12 @@ const scrollToHeading = (id: string) => {
 </script>
 
 <template>
-  <ul>
+  <ul class="px-1">
     <li v-for="link in links" :key="link.text" class="min-w-0 group" :class="[{ 'pl-3': link.depth === 3, 'pl-6': link.depth === 4 }]">
       <a
         :href="`#${link.id}`"
         class="block py-1 text-sm truncate lg:pr-3"
-        :class="[activeHeadings.includes(link.id) ? 'text-green-500 dark:text-green-400' : 'u-text-gray-500 group-hover:u-text-gray-900']"
+        :class="[activeHeadings.includes(link.id) ? 'text-green-500 dark:text-green-400 font-semibold' : 'u-text-gray-500 group-hover:u-text-gray-900']"
         @click.prevent="scrollToHeading(link.id)"
       >
         {{ link.text }}
