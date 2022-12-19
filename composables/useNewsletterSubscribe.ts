ec7e2bb77afbdf4bf09a6d82b7022d869710192c
Clément Ollivier <clement.o2p@gmail.com>
2022-12-19 15:03:14 +0100

    chore(newsletter): remove module (#1112)
    * chore(newsletter): remove module

--- composables/useNewsletterSubscribe.ts
+++
@@ -1,7 +0,0 @@
-/* Temporary fix before the module's composable is fixed */
-export async function useNewsletterSubscribe (email: string) {
-  return await $fetch('/api/newsletter/subscribe', {
-    body: { email },
-    method: 'POST'
-  }).catch(e => e.data)
-}
