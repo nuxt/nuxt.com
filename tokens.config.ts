58a8c282376dcc4a27216f5920dd64e018148f40
Sébastien Chopin <seb@nuxtjs.com>
2022-12-19 16:34:16 +0100

    fix: use color in singular

--- tokens.config.ts
+++ tokens.config.ts
@@ -3,7 +3,7 @@ import { defineTheme, palette } from 'pinceau'
 // See Typography config on https://github.com/nuxt-themes/typography/blob/main/tokens.config.ts
 export default defineTheme({
   typography: {
-    colors: {
+    color: {
       primary: palette('#00dc82')
     }
   }
