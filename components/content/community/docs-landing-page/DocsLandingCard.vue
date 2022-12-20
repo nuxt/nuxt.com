9ead704040b499783380e9f566aaf7c81e521f37
Clément Ollivier <clement.o2p@gmail.com>
2022-12-20 15:11:53 +0100

    refactor: add useblocklinks composable

--- components/content/community/docs-landing-page/DocsLandingCard.vue
+++ components/content/community/docs-landing-page/DocsLandingCard.vue
@@ -109,20 +109,9 @@ const headerClass = computed(() => {
   ].join(' ')
 })
 
-// Solution to get great block links
-// From https://css-tricks.com/block-links-the-search-for-a-perfect-solution/#aa-method-4-sprinkle-javascript-on-the-second-method
-const root = ref(null)
-const stopPropagation = (e: Event) => e.stopPropagation()
-onMounted(() => {
-  Array.from(root.value.querySelectorAll('a')).forEach((el: Element) => {
-    el.addEventListener('click', stopPropagation)
-  })
-})
-onBeforeUnmount(() => {
-  Array.from(root.value.querySelectorAll('a')).forEach((el: Element) => {
-    el.removeEventListener('click', stopPropagation)
-  })
-})
+const root = ref<HTMLElement | null>(null)
+
+useBlockLinks(root)
 </script>
 
 <style scoped lang="postcss">
