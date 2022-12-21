0c85db0a333cca4a71fcd3403987f34887b7cb7b
Clément Ollivier <clement.o2p@gmail.com>
2022-12-21 18:27:55 +0100

    fix: handle non provided slots

--- components/content/community/docs-landing-page/DocsLandingSection.vue
+++ components/content/community/docs-landing-page/DocsLandingSection.vue
@@ -4,10 +4,10 @@
       <h1 class="text-4xl font-semibold sm:text-5xl u-text-gray-900">
         <ContentSlot :use="$slots.title" unwrap="p" />
       </h1>
-      <p class="font-medium sm:text-lg u-text-gray-500">
+      <p v-if="$slots.description" class="font-medium sm:text-lg u-text-gray-500">
         <ContentSlot :use="$slots.description" unwrap="p" />
       </p>
-      <div>
+      <div v-if="$slots.extra">
         <ContentSlot :use="$slots.extra" unwrap="p" />
       </div>
     </div>
