151397dd91a20e18313e54cdd5bfb8bf6b33b581
Clément Ollivier <clement.o2p@gmail.com>
2023-01-02 17:30:02 +0100

    refactor(homepage): explain fetching at root component

--- pages/index.vue
+++ pages/index.vue
@@ -9,6 +9,7 @@ const { page } = useContent()
 const { fetchList: fetchShowcases } = useResourcesShowcases()
 const { fetchList: fetchModules } = useModules()
 
+/* resources are needed in the page, start fetching as soon as possible */
 await Promise.all([fetchShowcases(), fetchModules()])
 
 useContentHead(page)
