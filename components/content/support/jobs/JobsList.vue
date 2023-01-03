1df5e077d587b5ba0fd9d4f212309f0e52c01ba6
Clément Ollivier <clement.o2p@gmail.com>
2023-01-03 17:06:57 +0100

    refactor(jobs): use props and emits for filters

--- components/content/support/jobs/JobsList.vue
+++ components/content/support/jobs/JobsList.vue
@@ -3,12 +3,12 @@
     <Page v-if="!error" id="smooth" class="relative pt-16 -mt-16">
       <PageList :title="`${filteredJobs.length} job${filteredJobs.length > 1 ? 's' : ''} found`" :modules-filter="false">
         <template #filters>
-          <JobsFilterLocation />
-          <JobsFilterType />
+          <JobsFilterLocation :locations="locations" :selected-location="selectedLocation" @update:location="updateLocation" />
+          <JobsFilterType :types="types" :selected-type="selectedType" @update:type="updateType" />
         </template>
 
         <ul v-if="filteredJobs.length" class="flex flex-col gap-8 mt-8">
-          <li v-for="job in filteredJobs" :key="job.id">
+          <li v-for="job in filteredJobs" :key="job.link">
             <JobsListItem :job="job" />
           </li>
         </ul>
@@ -28,7 +28,36 @@
 </template>
 
 <script setup lang="ts">
-const { fetchList, filteredJobs, selectedLocation, selectedType } = useNuxtJobs()
+const { fetchList, filteredJobs, locations, selectedLocation, types, selectedType } = useNuxtJobs()
+
+const route = useRoute()
+const router = useRouter()
 
 const error = await fetchList()
+
+const updateLocation = (location: { value: string, text: string }) => {
+  router.replace({
+    name: 'support-jobs',
+    query: {
+      ...route.query,
+      location: location?.value || undefined
+    },
+    state: {
+      smooth: '#smooth'
+    }
+  })
+}
+
+const updateType = (type: { value: string, text: string }) => {
+  router.replace({
+    name: 'support-jobs',
+    query: {
+      ...route.query,
+      type: type?.value || undefined
+    },
+    state: {
+      smooth: '#smooth'
+    }
+  })
+}
 </script>
