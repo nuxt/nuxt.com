d926c5475f9ecd912675b9402e320f032ef59917
Florent Delerue <florentdelerue@hotmail.com>
2023-01-03 11:10:56 +0100

    refactor: home sections (#1130)
    * refactor(home): sections
    
    * fix(homesection): inline buttons
    
    * fix(homesections): buttons
    
    * refactor(homesection): remove unused props

--- components/content/home/HomeJourneySection.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="relative mb-20">
-    <ContentSlot :use="$slots.content" unwrap="p" />
-    <div class="absolute left-0 -top-8 blur-[80px] z-[-1]">
-      <img src="/assets/home/home-journey-gradient.svg" class="h-[300px]" height="300" width="541" role="presentation">
-    </div>
-  </div>
-</template>
