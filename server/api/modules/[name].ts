65787cf36496616bf0ed2883b4b874b0a057c436
Clément Ollivier <clement.o2p@gmail.com>
2023-01-10 10:55:13 +0100

    refactor(module): add type

--- server/api/modules/[name].ts
+++ server/api/modules/[name].ts
@@ -1,6 +1,7 @@
+import type { H3Event } from 'h3'
 import type { Module } from '../../../types'
 import { defineEventHandler } from '#imports'
 
-export default defineEventHandler(async (event) => {
+export default defineEventHandler(async (event: H3Event) => {
   return await $fetch<Module>(`https://modules.nuxtjs.org/api/modules/${event.context.params.name}`)
 })
