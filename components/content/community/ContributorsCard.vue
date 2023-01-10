8817d90657a92bc873ebe9de7b9b97c4ba405be1
Sébastien Chopin <seb@nuxtjs.com>
2023-01-10 16:04:53 +0100

    feat: add support for newsletter (#1118)
    * feat: add support for newsletter
    
    * fix: use correct contact list
    
    * chore: update env
    
    * chore: add list id and very contact in list
    
    * chore: revert main branch

--- components/content/community/ContributorsCard.vue
+++ components/content/community/ContributorsCard.vue
@@ -1,10 +1,17 @@
 <template>
-  <div class="flex flex-col gap-6">
-    <h2 v-if="$slots.title" class="font-semibold text-3xl align-middle pb-6">
+  <div class="flex flex-col">
+    <h2 v-if="$slots.title" class="font-semibold text-3xl align-middle">
       <ContentSlot :use="$slots.title" unwrap="p" />
     </h2>
-    <ol>
-      <ContentSlot :use="$slots.default" unwrap="p" />
-    </ol>
+    <ContentSlot v-if="$slots.default" :use="$slots.default" unwrap="p" />
   </div>
 </template>
+
+<style scoped lang="postcss">
+:deep(ol) {
+  @apply ml-4 my-0;
+  ::marker {
+    @apply text-3xl font-semibold;
+  }
+}
+</style>
---
+++ server/api/newsletter/confirm.post.ts
@@ -0,0 +1,39 @@
+import { defineEventHandler, readBody } from 'h3'
+import Joi from 'joi'
+import { validate } from '../../utils/validation'
+import { useSendgrid } from '../../lib/sendgrid'
+import { generateConfirmation } from '../../utils/newsletter'
+
+export default defineEventHandler(async (event) => {
+  // Get the email from body
+  const { email, confirmation } = await readBody(event)
+  await validate({ email, confirmation }, Joi.object({
+    email: Joi.string().email().trim().required(),
+    confirmation: Joi.string().required()
+  }))
+
+  // Validate confirmation code
+  if (generateConfirmation(email) !== confirmation) {
+    throw createError({
+      statusCode: 400,
+      message: 'Confirmation code is invalid.'
+    })
+  }
+
+  // Add to contacts list
+  await useSendgrid().client.request({
+    method: 'PUT',
+    url: '/v3/marketing/contacts',
+    body: {
+      list_ids: [useSendgrid().listId],
+      contacts: [{ email }]
+    }
+  }).catch((err: any) => {
+    throw createError({
+      message: err?.response?.body?.errors?.[0]?.message || 'Invalid email',
+      statusCode: 400
+    })
+  })
+
+  return { ok: true }
+})
