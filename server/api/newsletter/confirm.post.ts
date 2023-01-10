7c2afd17fab5da6623373b646c0a2d6a00b1f76e
Sébastien Chopin <seb@nuxtjs.com>
2023-01-10 16:17:18 +0100

    fix: explicit use createError

--- server/api/newsletter/confirm.post.ts
+++ server/api/newsletter/confirm.post.ts
@@ -1,4 +1,4 @@
-import { defineEventHandler, readBody } from 'h3'
+import { defineEventHandler, readBody, createError } from 'h3'
 import Joi from 'joi'
 import { validate } from '../../utils/validation'
 import { useSendgrid } from '../../lib/sendgrid'
