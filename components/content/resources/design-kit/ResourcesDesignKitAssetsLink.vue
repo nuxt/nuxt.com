9bc00411529a47ddc3c49bb9fd428bdcd8c7d9c9
CarolinaDelerue <99035993+CarolinaDelerue@users.noreply.github.com>
2023-01-17 11:54:56 +0100

    button design change and add button in hero of designkit page

--- components/content/resources/design-kit/ResourcesDesignKitAssetsLink.vue
+++ components/content/resources/design-kit/ResourcesDesignKitAssetsLink.vue
@@ -1,24 +1,27 @@
 <template>
-  <a class="relative inline-flex items-center group flex-nowrap max-w-max" href="/assets/design-kit/nuxt-assets.zip" download>
-    {{ linkText }}
-    <span
-      class="gradient-border rounded absolute bg-[length:200%] left-0 font-semibold -bottom-1 h-0.5 w-4 group-hover:bg-auto group-hover:w-full duration-300 transition-ease bg-gradient-to-r from-green-400 to-teal-400"
+  <div v-if="buttons.length" class="gap-6 flex flex-col sm:flex-row">
+    <UButton
+      v-for="button of buttons"
+      :key="button.label"
+      :variant="button.variant || 'transparent'"
+      :icon="button.icon || undefined"
+      :label="button.label || ''"
+      :to="button.to || null"
+      :trailing="button.trailing"
+      :size="'sm' || button.size"
+      truncate
+      class="focus-visible:ring-2"
     />
-    <Icon name="uil:arrow-right" class="w-5 h-5 ml-1 mt-0.5" />
-  </a>
+  </div>
 </template>
 
 <script setup lang="ts">
+import type { PropType } from 'vue'
+
 defineProps({
-  linkText: {
-    type: String,
-    default: 'Download design kit'
+  buttons: {
+    type: Array as PropType<{ label?: string, variant?: string, to?: string, icon?: string, trailing?: boolean, size?: string }[]>,
+    default: () => []
   }
 })
 </script>
-
-<style scoped>
-.transition-ease {
-  transition-timing-function: cubic-bezier(0.83, 0, 0.17, 1);
-}
-</style>
