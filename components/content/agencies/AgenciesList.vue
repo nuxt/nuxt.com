b4d458baf2a110f7c2826d895a8c637ac98a7a55
Florent Delerue <florentdelerue@hotmail.com>
2023-01-26 16:04:24 +0100

    refactor: notfound component

--- components/content/agencies/AgenciesList.vue
+++ components/content/agencies/AgenciesList.vue
@@ -32,6 +32,18 @@
           </CardListItem>
         </li>
       </ul>
+      <NotFound
+        v-else
+        icon="ph:handshake-thin"
+        :reset-filter="true"
+        :buttons="[
+          { variant: 'primary', size: 'lg', label: 'Reset filters', icon: 'grommet-icons:power-reset', trailing: true },
+          {
+            to: 'https://docs.google.com/forms/d/e/1FAIpQLSf85qskit5QqmGJcruGkGF0U7240Bh9MeN0pHB18UiOMWC8dA/viewform',
+            target: '_blank', variant: 'primary-gradient', size: 'lg', label: 'Submit my agency' }]"
+      >
+        <span>No agency matches your criteria for now.</span>
+      </NotFound>
     </PageList>
   </Page>
   <Page v-else>
@@ -47,8 +59,13 @@ const { filteredPartners, fetchList, services, selectedService, regions, selecte
 const { createReplaceRoute } = useFilters()
 
 const replaceRoute = createReplaceRoute('support-agencies')
+const router = useRouter()
 
 const error = await fetchList()
+
+const resetQuery = () => {
+  router.replace({ query: {} })
+}
 </script>
 
 <style scoped>
