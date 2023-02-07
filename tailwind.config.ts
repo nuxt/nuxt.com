8353050d55af351d06d30762086f35d2af14512f
Florent Delerue <florentdelerue@hotmail.com>
2023-02-07 16:01:05 +0100

    refactor: remove nuxthq/ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,15 +20,23 @@ module.exports = {
     }
   },
   plugins: [
-    require('tailwindcss-border-gradient-radius')
+    require('tailwindcss-border-gradient-radius'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio')
   ],
   content: [
     'ui/*.ts',
     '.demo/content/**/*.md',
-    'content/**/*.md'
+    'content/**/*.md',
   ],
   safelist: [
     ...[12, 24, 36, 48, 60, 72, 84, 96, 108, 120].map(number => `pl-[${number}px]`),
+    {
+      pattern: /rounded-(sm|md|lg|xl|2xl|3xl)/,
+      variants: ['sm']
+    },
     {
       pattern: new RegExp(`bg-(${safeColorsAsRegex})-(400|500)`),
       variants: ['hover', 'disabled', 'dark']
