40ab03939c22db1402128a346f459287c2fc5144
Florent Delerue <florentdelerue@hotmail.com>
2023-02-07 16:01:05 +0100

    refactor: remove nuxthq/ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -35,7 +35,7 @@ module.exports = {
   content: [
     'ui/*.ts',
     '.demo/content/**/*.md',
-    'content/**/*.md'
+    'content/**/*.md',
   ],
   safelist: [
     ...[12, 24, 36, 48, 60, 72, 84, 96, 108, 120].map(number => `pl-[${number}px]`),
