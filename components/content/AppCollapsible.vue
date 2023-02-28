2a43e52352994be7b99f992de62e523a455c51aa
Carolina Delerue <99035993+CarolinaDelerue@users.noreply.github.com>
2023-02-28 09:13:47 +0100

    feat: add workshop page (#1247)
    * wip
    
    * add workshop page
    
    * update workshop page after review
    
    * upadate after review
    
    * Update content/4.support/5.workshop.md
    
    ---------
    
    Co-authored-by: Clément Ollivier <clement.o2p@gmail.com>

---
+++ components/content/AppCollapsible.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="flex flex-col gap-4 border-b u-border-gray-200 py-6">
+    <AppButton base-class="px-0" class="flex justify-between" variant="transparent" @click="isCollapse = !isCollapse">
+      <div class="flex flex-col gap-y-4">
+        <h4 class="u-text-gray-700 text-2xl font-semibold">
+          <ContentSlot :use="$slots.title" unwrap="p" />
+        </h4>
+      </div>
+      <Icon :name="!isCollapse ? 'material-symbols:keyboard-arrow-up' : 'material-symbols:keyboard-arrow-down'" size="24px" />
+    </AppButton>
+    <Transition>
+      <p v-show="!isCollapse" class="u-text-gray-500">
+        <ContentSlot :use="$slots.description" unwrap="p" />
+      </p>
+    </Transition>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const isCollapse = ref(true)
+
+</script>
+
+<style lang="postcss">
+.v-enter-active,
+.v-leave-active {
+    max-height: 100vh;
+    opacity: 1;
+    transition: all 0.3s linear;
+}
+
+.v-enter-from,
+.v-leave-to {
+  max-height: 0;
+  opacity: 0;
+  transition: all 0.15s linear;
+}
+</style>
---
+++ components/content/support/workshop/WorkshopCard.vue
@@ -0,0 +1,51 @@
+<template>
+  <div
+    class="container py-6 flex flex-col gap-y-2 w-full"
+    :class="{ 'border u-border-gray-200 rounded-lg u-bg-gray-50': border }"
+  >
+    <div v-if="backgroundImage" :class="backgroundImageClass">
+      <img
+        :src="`${backgroundImage.path}-dark.${backgroundImage.format}`"
+        alt=""
+        class="h-full hidden dark:block"
+        :width="backgroundImage.width"
+        :height="backgroundImage.height"
+        loading="lazy"
+      >
+      <img
+        :src="`${backgroundImage.path}-light.${backgroundImage.format}`"
+        alt=""
+        class="h-full dark:hidden"
+        :width="backgroundImage.width"
+        :height="backgroundImage.height"
+        loading="lazy"
+      >
+    </div>
+    <h2 class="u-text-gray-900 text-2xl font-semibold">
+      <ContentSlot :use="$slots.title" unwrap="p" />
+    </h2>
+    <div v-if="$slots.list">
+      <ContentSlot :use="$slots.list" unwrap="p" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { PropType } from 'vue'
+import type { Image } from 'types'
+
+defineProps({
+  border: {
+    type: Boolean,
+    default: true
+  },
+  backgroundImageClass: {
+    type: String,
+    default: 'absolute right-0 bottom-0'
+  },
+  backgroundImage: {
+    type: Object as PropType<Image>,
+    default: () => ({})
+  }
+})
+</script>
