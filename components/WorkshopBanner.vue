1fc7d643087c70273b951aa8f7188ce3c23d13bd
CarolinaDelerue <99035993+CarolinaDelerue@users.noreply.github.com>
2023-03-02 15:43:20 +0100

    wip design banner

--- components/WorkshopBanner.vue
+++ components/WorkshopBanner.vue
@@ -23,16 +23,19 @@ if (process.server) {
 </script>
 
 <template>
-  <div ref="workshopBanner" class="relative w-full bg-white dark:bg-black z-50 workshop-banner">
-    <div class="flex flex-wrap sm:flex-row justify-center items-center gap-x-3.5 p-2">
+  <div ref="workshopBanner" class="relative w-full bg-white dark:bg-black z-50 workshop-banner border-b border-b-gray-300 dark:border-b-gray-700">
+    <div class="flex flex-wrap sm:flex-row justify-start sm:justify-center items-center gap-x-1.5 p-2">
       <p class="text-xs sm:text-sm text-left sm:text-center items-center text-black dark:text-white justify-center items-center">
-        📣 JUST RELEASED ! Try out ouf first workshop and level up to Nuxt 3 !
+        <span class="font-semibold ">
+          📣 Just Released!
+        </span>
+        Try out our first workshop and level up to Nuxt 3!
       </p>
-      <AppButton class="font-semibold" variant="transparent" size="xs" trailing-icon="material-symbols:arrow-right-alt" to="https://docs.google.com/forms/d/e/1FAIpQLScv6M2XJOLOK2t6tQnw6oXTritmH-K3fPkgX7ftVMxodNEJig/viewform">
+      <AppButton class="font-semibold" variant="link" size="xs" trailing-icon="material-symbols:arrow-right-alt" to="/support/workshop">
         Register now
       </AppButton>
       <div class="flex flex-row justify-end absolute inset-y-0 right-4">
-        <AppButton class="font-semibold" variant="transparent" size="xs" icon="carbon:close" @click="preferNoBanner" />
+        <AppButton class="font-semibold" variant="link" size="xs" icon="carbon:close" @click="preferNoBanner" />
       </div>
     </div>
   </div>
