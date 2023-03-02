a17d52ff9006920cdaf98719cace540a69e179c3
Clément Ollivier <clement.o2p@gmail.com>
2023-03-02 17:44:43 +0100

    feat: workshop banner (#1259)
    * feat: ensure no cls if banner is hidden
    
    * banner design wip
    
    * wip design banner
    
    * add banner styling
    
    * feat: use same pattern for hiding the banner the first time
    
    * Update components/WorkshopBanner.vue
    
    ---------
    
    Co-authored-by: CarolinaDelerue <99035993+CarolinaDelerue@users.noreply.github.com>
    Co-authored-by: Sébastien Chopin <seb@nuxt.com>

---
+++ components/WorkshopBanner.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+const preferNoBanner = () => {
+  localStorage.setItem('preferNoBanner', 'true')
+  document.querySelector('html')?.classList.add('hide-banner')
+}
+
+if (process.server) {
+  useHead({
+    script: [
+      {
+        key: 'prehydrate-workshop-banner',
+        innerHTML: `
+            if (localStorage.getItem('preferNoBanner') === 'true') {
+              document.querySelector('html').classList.add('hide-banner')
+            }`.replace(/\s+/g, ' '),
+        type: 'text/javascript'
+      }
+    ]
+  })
+}
+</script>
+
+<template>
+  <div class="relative w-full bg-white dark:bg-black z-50 border-b border-b-gray-300 dark:border-b-gray-700 workshop-banner">
+    <div class="flex flex-wrap sm:flex-row justify-start sm:justify-center items-center gap-x-1.5 p-2">
+      <p class="text-xs sm:text-sm text-left sm:text-center items-center text-black dark:text-white justify-center items-center pl-[10px] sm:pl-0 pr-10 sm:pr-0">
+        <span class="font-semibold">
+          📣 Just Released!
+        </span>
+        Our official workshop to level up from Nuxt 2 to Nuxt 3.
+      </p>
+      <AppButton class="font-semibold" variant="link" size="xs" trailing-icon="material-symbols:arrow-right-alt" to="/support/workshop">
+        Register now
+      </AppButton>
+      <div class="flex flex-row justify-end absolute inset-y-0 right-4">
+        <AppButton class="font-semibold" variant="link" size="xs" icon="carbon:close" @click="preferNoBanner" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+.hide-banner .workshop-banner{
+  display: none;
+}
+</style>
