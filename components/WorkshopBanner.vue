eb975eb44c9c0103bed67250148afd6b53005005
CarolinaDelerue <99035993+CarolinaDelerue@users.noreply.github.com>
2023-03-02 15:03:26 +0100

    banner design wip

--- components/WorkshopBanner.vue
+++ components/WorkshopBanner.vue
@@ -23,13 +23,18 @@ if (process.server) {
 </script>
 
 <template>
-  <div ref="workshopBanner" class="w-full h-[36px] bg-white z-50 workshop-banner">
-    <p class="text-center text-black">
-      Workshop Banner
-      <AppButton icon="carbon:close" @click="preferNoBanner">
-        Close
+  <div ref="workshopBanner" class="relative w-full bg-white dark:bg-black z-50 workshop-banner">
+    <div class="flex flex-wrap sm:flex-row justify-center items-center gap-x-3.5 p-2">
+      <p class="text-xs sm:text-sm text-left sm:text-center items-center text-black dark:text-white justify-center items-center">
+        📣 JUST RELEASED ! Try out ouf first workshop and level up to Nuxt 3 !
+      </p>
+      <AppButton class="font-semibold" variant="transparent" size="xs" trailing-icon="material-symbols:arrow-right-alt" to="https://docs.google.com/forms/d/e/1FAIpQLScv6M2XJOLOK2t6tQnw6oXTritmH-K3fPkgX7ftVMxodNEJig/viewform">
+        Register now
       </AppButton>
-    </p>
+      <div class="flex flex-row justify-end absolute inset-y-0 right-4">
+        <AppButton class="font-semibold" variant="transparent" size="xs" icon="carbon:close" @click="preferNoBanner" />
+      </div>
+    </div>
   </div>
 </template>
 
