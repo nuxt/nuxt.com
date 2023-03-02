243b24c2144c6d4aa8ee4db30a95e169b017418c
Sébastien Chopin <seb@nuxt.com>
2023-03-02 18:50:53 +0100

    chore: update core deps

--- tokens.config.ts
+++ tokens.config.ts
@@ -1,10 +1,21 @@
-import { defineTheme, palette } from 'pinceau'
+import { defineTheme } from 'pinceau'
 
 // See Typography config on https://github.com/nuxt-themes/typography/blob/main/tokens.config.ts
 export default defineTheme({
   typography: {
     color: {
-      primary: palette('#00dc82')
+      primary: {
+        50: '#d6ffee',
+        100: '#acffdd',
+        200: '#83ffcc',
+        300: '#30ffaa',
+        400: '#00dc82',
+        500: '#00bd6f',
+        600: '#009d5d',
+        700: '#007e4a',
+        800: '#005e38',
+        900: '#003f25'
+      }
     }
   }
 })
