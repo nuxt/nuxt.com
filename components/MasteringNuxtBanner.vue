d86448589142e239d93b0512b45fd4cbe10e6033
Clément Ollivier <clement.o2p@gmail.com>
2023-03-22 16:59:32 +0100

    feat: update banner end date (#1292)
    * feat: update banner end date
    
    * refactor
    
    * Update components/MasteringNuxtBanner.vue
    
    * fix: fix dates + add extended mention
    
    * chore: update
    
    ---------
    
    Co-authored-by: Sébastien Chopin <seb@nuxt.com>

--- components/MasteringNuxtBanner.vue
+++ components/MasteringNuxtBanner.vue
@@ -5,7 +5,16 @@ const preferNoMNBanner = () => {
   localStorage.setItem('preferNoMNBanner', 'true')
   document.querySelector('html')?.classList.add('hide-banner')
 }
-const endDate = +(new Date('2023-03-23T00:00:00Z')) - Date.now()
+
+const firstEndDate = +(new Date('2023-03-23T00:00:00Z')) - Date.now()
+const secondEndDate = +(new Date('2023-03-25T00:00:00Z')) - Date.now()
+
+let endDate = firstEndDate
+
+/* Renew end date */
+if (firstEndDate < 0) {
+  endDate = secondEndDate
+}
 
 if (process.server) {
   useHead({
@@ -27,13 +36,13 @@ if (process.server) {
   <div class="relative w-full bg-white dark:bg-black z-50 border-b border-b-gray-300 dark:border-b-gray-700 mn-banner">
     <div class="flex flex-wrap sm:flex-row justify-start sm:justify-center items-center gap-x-1.5 p-2">
       <p class="text-xs sm:text-sm text-left sm:text-center items-center text-black dark:text-white justify-center items-center pl-[10px] sm:pl-0 pr-10 sm:pr-0">
-        👨‍🏫 The <a href="https://masteringnuxt.com/?utm_source=nuxt&utm_medium=website&utm_campaign=affiliate&utm_content=top_banner&friend=nuxt" target="_blank" class="font-bold underline">Mastering Nuxt 3</a> course is now completed!
+        <span v-if="endDate > 0 && firstEndDate < 0" class="uppercase font-semibold mr-2">[ Extended ]</span>👨‍🏫 The <a href="https://masteringnuxt.com/?utm_source=nuxt&utm_medium=website&utm_campaign=affiliate&utm_content=top_banner&friend=nuxt" target="_blank" class="font-bold underline">Mastering Nuxt 3</a> course is now completed!
       </p>
       <a class="text-sm font-semibold bg-green-400 text-black px-2 py-1 rounded" href="https://masteringnuxt.com/?utm_source=nuxt&utm_medium=website&utm_campaign=affiliate&utm_content=top_banner&friend=nuxt" target="_blank">
         {{ endDate > 0 ? 'Claim 30% OFF' : 'Discover the course' }}
       </a>
       <ClientOnly>
-        <vue-countdown v-if="endDate > 0" :time="endDate" v-slot="{ days, hours, minutes }">
+        <vue-countdown v-if="endDate > 0" v-slot="{ days, hours, minutes }" :time="endDate">
           <span class="text-sm pl-[10px] sm:pl-0"><span v-if="days > 0">{{ days }} {{ days > 1 ? 'days' : 'day' }}, </span><span v-if="days > 0 || hours > 0">{{ hours }} {{ hours > 1 ? 'hours' : 'hour' }} and</span> {{ minutes }} {{ minutes > 1 ? 'minutes' : 'minute' }} left</span>
         </vue-countdown>
       </ClientOnly>
