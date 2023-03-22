e26e93a36406f92ce9f89b4a297e24d296a180be
Clément Ollivier <clement.o2p@gmail.com>
2023-03-22 16:32:55 +0100

    fix: fix dates + add extended mention

--- components/MasteringNuxtBanner.vue
+++ components/MasteringNuxtBanner.vue
@@ -6,8 +6,8 @@ const preferNoMNBanner = () => {
   document.querySelector('html')?.classList.add('hide-banner')
 }
 
-const firstEndDate = +(new Date('2021-03-23T00:00:00Z')) - Date.now()
-const secondEndDate = +(new Date('2021-03-25T00:00:00Z')) - Date.now()
+const firstEndDate = +(new Date('2023-03-23T00:00:00Z')) - Date.now()
+const secondEndDate = +(new Date('2023-03-25T00:00:00Z')) - Date.now()
 
 let endDate = firstEndDate
 
@@ -36,7 +36,7 @@ if (process.server) {
   <div class="relative w-full bg-white dark:bg-black z-50 border-b border-b-gray-300 dark:border-b-gray-700 mn-banner">
     <div class="flex flex-wrap sm:flex-row justify-start sm:justify-center items-center gap-x-1.5 p-2">
       <p class="text-xs sm:text-sm text-left sm:text-center items-center text-black dark:text-white justify-center items-center pl-[10px] sm:pl-0 pr-10 sm:pr-0">
-        👨‍🏫 The <a href="https://masteringnuxt.com/?utm_source=nuxt&utm_medium=website&utm_campaign=affiliate&utm_content=top_banner&friend=nuxt" target="_blank" class="font-bold underline">Mastering Nuxt 3</a> course is now completed!
+        <span class="uppercase font-semibold">Extended  </span>👨‍🏫 The <a href="https://masteringnuxt.com/?utm_source=nuxt&utm_medium=website&utm_campaign=affiliate&utm_content=top_banner&friend=nuxt" target="_blank" class="font-bold underline">Mastering Nuxt 3</a> course is now completed!
       </p>
       <a class="text-sm font-semibold bg-green-400 text-black px-2 py-1 rounded" href="https://masteringnuxt.com/?utm_source=nuxt&utm_medium=website&utm_campaign=affiliate&utm_content=top_banner&friend=nuxt" target="_blank">
         {{ endDate > 0 ? 'Claim 30% OFF' : 'Discover the course' }}
