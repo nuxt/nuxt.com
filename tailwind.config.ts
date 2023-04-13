b3d13addaf2d34e800dfadb5b4fa474b2a5809a4
Sébastien Chopin <seb@nuxt.com>
2023-04-13 11:45:29 +0200

    feat: use nuxt 3.4

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -29,7 +29,6 @@ module.exports = {
     require('tailwindcss-border-gradient-radius'),
     require('@tailwindcss/typography'),
     require('@tailwindcss/forms'),
-    require('@tailwindcss/line-clamp'),
     require('@tailwindcss/aspect-ratio')
   ],
   content: [
