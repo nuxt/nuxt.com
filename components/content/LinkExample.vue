5cde46e8185315fd81d3977ec59880fb23912469
Clément Ollivier <clement.o2p@gmail.com>
2023-05-30 13:56:34 +0200

    fix: remove duplicated icon (#1321)

--- components/content/LinkExample.vue
+++ components/content/LinkExample.vue
@@ -1,5 +1,5 @@
 <template>
-  <Alert type="info" icon="🔎">
+  <Alert type="info">
     <Icon class="inline-block w-5 h-5 mr-1" name="file-icons:sandbox" />
     Read and edit a live example in
     <NuxtLink :to="link">
