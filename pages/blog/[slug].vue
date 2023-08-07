689a8b27986580d5082dfcc4bfded936fa19668b
Benjamin Canac <canacb1@gmail.com>
2023-08-07 21:29:12 +0200

    up

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -2,6 +2,7 @@
 import type { BlogArticle } from '~/types'
 
 const route = useRoute()
+const { copy } = useCopyToClipboard()
 
 const { data: article } = await useAsyncData(route.path, () => queryContent<BlogArticle>(route.path).findOne())
 if (!article.value) {
@@ -14,12 +15,28 @@ const { data: surround } = await useAsyncData(`${route.path}-surround`, () => qu
   .sort({ date: -1 })
   .findSurround(route.path.endsWith('/') ? route.path.slice(0, -1) : route.path)
 )
+
+const socialLinks = computed(() => [{
+  icon: 'i-simple-icons-linkedin',
+  to: `https://www.linkedin.com/sharing/share-offsite/?url=https://nuxt.com${article.value._path}`
+}, {
+  icon: 'i-simple-icons-twitter',
+  to: `https://twitter.com/intent/tweet?text=I%20found%20this%20article%20interesting%20%20https://nuxt.com${article.value._path}&hashtags=nuxt`
+}])
+
+function copyLink () {
+  copy(`https://nuxt.com${article.value._path}`, { title: 'Copied to clipboard' })
+}
 </script>
 
 <template>
   <UContainer :ui="{ constrained: 'max-w-4xl' }">
     <UPage>
-      <UPageHeader :title="article.title" :description="article.description" :headline="article.category">
+      <UPageHeader :title="article.title" :description="article.description">
+        <template #headline>
+          {{ article.category }} <span class="text-muted">&middot;</span> <time class="text-muted"> {{ formatDateByLocale('en', article.date) }}</time>
+        </template>
+
         <div class="mt-4 flex flex-wrap items-center gap-6">
           <UButton
             v-for="(author, index) in article.authors"
@@ -55,6 +72,11 @@ const { data: surround } = await useAsyncData(`${route.path}-surround`, () => qu
 
       <UPageBody prose>
         <ContentRenderer v-if="article && article.body" :value="article" />
+
+        <div class="flex justify-end items-center gap-1.5 mt-12 not-prose">
+          <USocialButton icon="i-ph-link-simple" @click="copyLink" />
+          <USocialButton v-for="(link, index) in socialLinks" :key="index" v-bind="link" target="_blank" />
+        </div>
 
         <hr v-if="surround?.length" class="border-border my-8">
 
