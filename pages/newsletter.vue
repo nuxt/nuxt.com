e9b1701fffc1e403b1e7431f3e282421287ff7e1
Benjamin Canac <canacb1@gmail.com>
2023-09-12 16:52:14 +0200

    add newsletter page

---
+++ pages/newsletter.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const route = useRoute()
+
+const { data: page } = await useAsyncData(route.path, () => queryContent(route.path).findOne())
+
+useContentHead(page)
+</script>
+
+<template>
+  <UContainer>
+    <UPageHero v-bind="page">
+      <template #links>
+        <NewsletterForm class="flex-1 max-w-xs" :label="null" />
+      </template>
+    </UPageHero>
+
+    <UPage>
+      <UPageBody>
+        <ULandingCTA v-bind="page.cta" card />
+      </UPageBody>
+    </UPage>
+  </UContainer>
+</template>
