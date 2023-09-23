c5d9454941a37198457e42e0800649e2438657ef
Sébastien Chopin <seb@nuxt.com>
2023-09-23 15:24:46 +0200

    chore: remove caching for fetchOrgsContributors

--- server/utils/volta.ts
+++ server/utils/volta.ts
@@ -10,7 +10,7 @@ export interface VoltaContributor {
   reactions: number
 }
 
-export const fetchOrgsContributors = cachedFunction(async (_event: H3Event, orgs: string[]) => {
+export const fetchOrgsContributors = async (_event: H3Event, orgs: string[]) => {
   if (!process.env.NUXT_VOLTA_TOKEN) {
     throw createError({
       statusCode: 500,
@@ -21,9 +21,5 @@ export const fetchOrgsContributors = cachedFunction(async (_event: H3Event, orgs
   const orgsQquery = orgs.map(org => `owner=${org}`).join('&')
   return await $fetch<VoltaContributor[]>(`http://api.volta.net/users/stats?${orgsQquery}&token=${process.env.NUXT_VOLTA_TOKEN}`)
     .then(contributors => contributors.filter((contributor: any) => !isBot(contributor.username)))
-}, {
-  name: 'volta-orgs-contributors',
-  maxAge: 60 * 60, // 1 hour
-  getKey: (_event: H3Event, orgs: string[]) => orgs.join('-')
 })
 
