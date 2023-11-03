f6515220802f8ed491817157d6de2cd2aa47ceae
Sébastien Chopin <seb@nuxt.com>
2023-11-03 16:20:42 +0100

    docs: add UI Pro as ad

--- components/ads/Ads.vue
+++ components/ads/Ads.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
-const { $ads } = useNuxtApp()
+// const { $ads } = useNuxtApp()
 </script>
 
 <template>
   <div class="space-y-3">
-    <AdsFallback v-if="$ads.adBlocked.value" />
-    <AdsCarbon v-else :key="$route.path" />
+    <AdsUIPro />
+    <!-- <AdsFallback v-if="$ads.adBlocked.value" /> -->
+    <!-- <AdsCarbon v-else :key="$route.path" /> -->
   </div>
 </template>
