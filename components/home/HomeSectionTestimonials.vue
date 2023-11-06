d9ebf0df4e8a982ed19f46ee287cf46a2671ab6e
Joe <heyimjoeqj@gmail.com>
2023-11-06 16:44:05 +0000

    fix(home): broken testimonials cards on Safari (#1410)

--- components/home/HomeSectionTestimonials.vue
+++ components/home/HomeSectionTestimonials.vue
@@ -15,41 +15,45 @@ defineProps<{
 
 <template>
   <UPageColumns>
-    <UPageCard
+    <div
       v-for="(testimonial, index) in testimonials"
       :key="index"
-      :ui="{
-        background: 'dark:bg-gradient-to-b from-gray-700/50 to-gray-900/50',
-        body: {
-          base: 'flex-1',
-          background: 'dark:bg-gray-900/50'
-        }
-      }"
-      :to="testimonial.author.url"
-      target="_blank"
+      class="break-inside-avoid py-0.5"
     >
-      <q class="italic text-lg text-gray-500 dark:text-gray-400">
-        {{ testimonial.quote }}
-      </q>
+      <UPageCard
+        :ui="{
+          background: 'dark:bg-gradient-to-b from-gray-700/50 to-gray-900/50',
+          body: {
+            base: 'flex-1',
+            background: 'dark:bg-gray-900/50',
+          }
+        }"
+        :to="testimonial.author.url"
+        target="_blank"
+      >
+        <q class="italic text-lg text-gray-500 dark:text-gray-400">
+          {{ testimonial.quote }}
+        </q>
 
-      <div class="flex gap-x-4 items-center mt-6">
-        <UAvatar
-          :src="testimonial.author.imgSrc"
-          :srcset="testimonial.author.imgSrcSet"
-          loading="lazy"
-          :alt="testimonial.author.name"
-          size="md"
-          :ui="{ rounded: 'rounded-lg' }"
-        />
-        <div class="min-w-0">
-          <p class="font-semibold text-primary">
-            {{ testimonial.author.name }}
-          </p>
-          <p class="text-sm truncate">
-            {{ testimonial.author.job }}
-          </p>
+        <div class="flex gap-x-4 items-center mt-6">
+          <UAvatar
+            :src="testimonial.author.imgSrc"
+            :srcset="testimonial.author.imgSrcSet"
+            loading="lazy"
+            :alt="testimonial.author.name"
+            size="md"
+            :ui="{ rounded: 'rounded-lg' }"
+          />
+          <div class="min-w-0">
+            <p class="font-semibold text-primary">
+              {{ testimonial.author.name }}
+            </p>
+            <p class="text-sm truncate">
+              {{ testimonial.author.job }}
+            </p>
+          </div>
         </div>
-      </div>
-    </UPageCard>
+      </UPageCard>
+    </div>
   </UPageColumns>
 </template>
