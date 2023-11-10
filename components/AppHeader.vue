d3e224d3583ba0efbd9613a1090255047d9a97da
Benjamin Canac <canacb1@gmail.com>
2023-11-10 18:11:05 +0100

    fix: move banner into header `top` slot
    Fixes #1419

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -25,6 +25,18 @@ defineProps<{
 
 <template>
   <UHeader :links="links">
+    <template #top>
+      <AppBanner :id="1" to="https://masteringnuxt.com/?ref=nuxt">
+        <p class="text-gray-600 dark:text-gray-300 flex items-center gap-1">
+          <UIcon name="i-ph-lightbulb-duotone" class="w-4 h-4" />
+
+          Enjoy <span class="font-bold text-gray-900 dark:text-white">Mastering Nuxt</span> black friday!
+        </p>
+
+        <UButton label="Learn more" color="gray" trailing-icon="i-ph-arrow-right" size="xs" class="rounded-full" />
+      </AppBanner>
+    </template>
+
     <template #logo>
       <Logo class="block w-auto h-6" @click.right.prevent="$router.push('/design-kit')" />
     </template>
