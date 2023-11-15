18af1ce75af20f031ee561ac6fa61ae44f932863
Florent Delerue <florentdelerue@hotmail.com>
2023-11-15 11:22:56 +0100

    feat(integration): toc

--- pages/integrations/[slug].vue
+++ pages/integrations/[slug].vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 import type { Integration } from '~/types'
-import { withoutTrailingSlash, joinURL } from 'ufo'
+import { withoutTrailingSlash } from 'ufo'
 
 const route = useRoute()
 
@@ -37,49 +37,58 @@ defineOgImage({
   description,
   headline: 'Integrations'
 })
+
+const links = [
+  {
+    icon: 'i-ph-lightning-duotone',
+    label: 'View on Nitro',
+    to: `https://nitro.unjs.io/deploy/providers/${integration.value._path.split('/')[2]}`,
+    target: '_blank'
+  }
+]
 </script>
 
 <template>
-  <UContainer :ui="{ base: 'my-20', constrained: 'max-w-4xl' }">
-    <UPageHeader v-bind="integration" :ui="{ wrapper: 'mb-8', icon: { base: 'text-black dark:text-white' } }">
-      <template #title>
-        <div class="flex items-center space-x-4">
-          <UIcon v-if="integration.logoIcon" :name="integration.logoIcon" class="w-10 h-10 text-black dark:text-white" />
-          <NuxtImg v-else :src="integration.logoSrc" class="w-10 h-10" />
-          <div>{{ integration.title }}</div>
+  <UContainer :ui="{ base: 'my-4 md:my-8 lg:mt-20' }">
+    <UPage>
+      <UPageHeader v-bind="integration" :ui="{ wrapper: 'mb-8 lg:ml-40', icon: { base: 'text-black dark:text-white' } }">
+        <template #title>
+          <div class="flex items-center space-x-4">
+            <UIcon v-if="integration.logoIcon" :name="integration.logoIcon" class="w-10 h-10 text-black dark:text-white" />
+            <NuxtImg v-else :src="integration.logoSrc" class="w-10 h-10" />
+            <div>{{ integration.title }}</div>
+          </div>
+        </template>
+
+        <div class="absolute top-[34px] -left-[72px] hidden lg:flex">
+          <UTooltip text="Back to integrations">
+            <UButton
+              to="/integrations"
+              icon="i-ph-caret-left"
+              color="gray"
+              :ui="{ rounded: 'rounded-full' }"
+              size="lg"
+            />
+          </UTooltip>
         </div>
-      </template>
-      <template #links>
-        <UButton
-          label="View on Nitro"
-          icon="i-ph-lightning-duotone"
-          :to="`https://nitro.unjs.io/deploy/providers/${integration._path.split('/')[2]}`"
-          variant="solid"
-          color="gray"
-          target="_blank"
-        />
-        <UButton v-for="(link, index) in integration.links" :key="index" v-bind="link" @click="link.click" />
-      </template>
+      </UPageHeader>
+      <UPage :ui="{ center: { base: 'lg:ml-40' } }">
+        <UPageBody prose>
+          <ContentRenderer v-if="integration && integration.body" :value="integration" />
+          <UDivider class="py-8" />
 
-      <div class="absolute top-[34px] -left-[72px] hidden lg:flex">
-        <UTooltip text="Back to integrations">
-          <UButton
-            to="/integrations"
-            icon="i-ph-caret-left"
-            color="gray"
-            :ui="{ rounded: 'rounded-full' }"
-            size="lg"
-          />
-        </UTooltip>
-      </div>
-    </UPageHeader>
+          <UDocsSurround :surround="surround" />
+        </UPageBody>
 
-    <UPageBody prose>
-      <ContentRenderer v-if="integration && integration.body" :value="integration" />
-    </UPageBody>
-
-    <UDivider class="pb-12" />
-
-    <UDocsSurround :surround="surround" />
+        <template #right>
+          <UDocsToc :links="integration.body.toc.links">
+            <template #bottom>
+              <UDivider v-if="links" type="dashed" />
+              <UPageLinks title="Links" :links="links" />
+            </template>
+          </UDocsToc>
+        </template>
+      </UPage>
+    </UPage>
   </UContainer>
 </template>
