98739fdfac75e4ada4d0ea447ac36c8200557431
Anthony Fu <anthonyfu117@hotmail.com>
2023-11-20 13:55:47 +0100

    blog: new post about DevTools 1.0 (#1427)
    * chore: wip
    
    * chore: wip
    
    * Update content/5.blog/18.nuxt-devtools-v1-0.md
    
    Co-authored-by: Arash <38922203+arashsheyda@users.noreply.github.com>
    
    * Update content/5.blog/18.nuxt-devtools-v1-0.md
    
    Co-authored-by: Arash <38922203+arashsheyda@users.noreply.github.com>
    
    * feat: move demos to cloudinary
    
    * feat: update cover
    
    * chore: update links
    
    * Discard changes to nuxt.config.ts
    
    * typos
    
    * up
    
    * chore: remove autoplay
    
    * add in index page
    
    ---------
    
    Co-authored-by: Arash <38922203+arashsheyda@users.noreply.github.com>
    Co-authored-by: Sébastien Chopin <seb@nuxtlabs.com>
    Co-authored-by: Sébastien Chopin <seb@nuxt.com>

---
+++ components/content/ArticleVideo.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+defineProps<{
+  cloudinary: string
+}>()
+</script>
+
+<template>
+  <video
+    type="video/mp4"
+    :poster="`https://res.cloudinary.com/nuxt/video/upload/${cloudinary}.jpg`"
+    controls
+    muted
+    preload="auto"
+    class="w-[190] m-auto shadow-2xl"
+    :style="{
+      'border-radius': '1% / 1.5%',
+    }"
+  >
+    <source :src="`https://res.cloudinary.com/nuxt/video/upload/${cloudinary}.webm`" type="video/webm">
+    <source :src="`https://res.cloudinary.com/nuxt/video/upload/${cloudinary}.mp4`" type="video/mp4">
+    <source :src="`https://res.cloudinary.com/nuxt/video/upload/${cloudinary}.ogg`" type="video/ogg">
+  </video>
+</template>
