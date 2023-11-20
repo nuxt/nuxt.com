f03fde419e2572e973d5174ce9d6e4eadf1551ca
Sébastien Chopin <seb@nuxt.com>
2023-11-20 13:32:39 +0100

    chore: remove autoplay

--- components/content/ArticleVideo.vue
+++ components/content/ArticleVideo.vue
@@ -1,27 +1,20 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-
 defineProps<{
   cloudinary: string
 }>()
-
-const controls = ref(false)
 </script>
 
 <template>
   <video
     type="video/mp4"
     :poster="`https://res.cloudinary.com/nuxt/video/upload/${cloudinary}.jpg`"
-    loop
+    controls
     muted
-    autoplay
     preload="auto"
     class="w-[190] m-auto shadow-2xl"
-    :controls="controls"
     :style="{
       'border-radius': '1% / 1.5%',
     }"
-    @click="controls = !controls"
   >
     <source :src="`https://res.cloudinary.com/nuxt/video/upload/${cloudinary}.webm`" type="video/webm">
     <source :src="`https://res.cloudinary.com/nuxt/video/upload/${cloudinary}.mp4`" type="video/mp4">
