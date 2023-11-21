bbac19c9d02afd3501af29b7f17a272d2c5c6793
Stephen Zhou <hi@hyoban.cc>
2023-11-21 19:38:51 +0800

    refactor: move to feed

--- server/routes/blog/rss.xml.ts
+++ server/routes/blog/rss.xml.ts
@@ -1,37 +1,59 @@
 import { serverQueryContent } from '#content/server'
+import * as cheerio from 'cheerio'
+import { Feed } from 'feed'
 
-import RSS from 'rss'
-
-const feed = new RSS({
-  title: 'The Nuxt Blog',
-  description:
-    'Read the latest news about all Nuxt solutions, from framework announcements to integration tutorials.',
-  site_url: 'https://nuxt.com/blog',
-  feed_url: 'https://nuxt.com/blog/rss.xml'
-})
+const BaseUrl = 'https://nuxt.com'
+const SiteUrl = `${BaseUrl}/blog`
+const SiteName = 'The Nuxt Blog'
+const SiteDescription =
+  'Read the latest news about all Nuxt solutions, from framework announcements to integration tutorials.'
+const SiteLanguage = 'en'
+const Copyright = `Copyright © 2016-${ new Date().getFullYear() } Nuxt - MIT License`
 
 export default defineEventHandler(async (event) => {
+  const feed = new Feed({
+    title: SiteName,
+    description: SiteDescription,
+    id: SiteUrl,
+    link: SiteUrl,
+    language: SiteLanguage,
+    image: `${BaseUrl}/icon.png`,
+    favicon: `${BaseUrl}/icon.png`,
+    copyright: Copyright,
+    feedLinks: {
+      rss: `${SiteUrl}/rss.xml`
+    }
+  })
+
   const docs = await serverQueryContent(event)
     .sort({ date: -1 })
     .where({ _partial: false, _draft: false })
     .find()
-  const blogPosts = docs.filter(
+  const blogs = docs.filter(
     (doc) => doc?._path?.includes('/blog/') && doc.title && doc.date
   )
 
-  blogPosts.forEach((doc) => {
-    feed.item({
-      url: `https://nuxt.com${doc._path}`,
-      title: doc.title,
-      date: doc.date,
-      description: doc.description,
-      author: doc.authors[0]?.name,
-      categories: [doc.category]
-    })
-  })
+  for (const blog of blogs) {
+    const content = await $fetch<string>(blog._path)
+    const $ = cheerio.load(content)
+    const prose = $('.prose')
+      .html()
+      .replaceAll('<!--[-->', '')
+      .replaceAll('<!--]-->', '')
+      .replaceAll('<!---->', '')
+      .replaceAll(/class="[^"]*"/g, '')
 
-  const feedString = feed.xml({ indent: true })
+    feed.addItem({
+      link: `https://nuxt.com${blog._path}`,
+      title: blog.title,
+      date: new Date(blog.date),
+      description: blog.description,
+      author: blog.authors,
+      category: blog.category,
+      content: prose
+    })
+  }
 
   appendHeader(event, 'Content-Type', 'text/xml')
-  return feedString
+  return feed.rss2()
 })
