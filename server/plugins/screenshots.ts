a642042bd5af8db6ebb3f3eca2087178c3b36e48
Sébastien Chopin <seb@nuxt.com>
2023-12-12 19:54:06 +0100

    feat: add video courses

--- server/plugins/screenshots.ts
+++ server/plugins/screenshots.ts
@@ -7,23 +7,43 @@ export default defineNitroPlugin(async (nitroApp) => {
   if (!process.dev) return
   // @ts-ignore
   nitroApp.hooks.hook('content:file:afterParse', async (file) => {
-    if (file._path !== '/templates') return
-
-    for (const template of file.templates) {
-      const url = template.screenshotUrl || template.demo
-      if (!url) {
-        console.error(`Template ${template.slug} has no "demo" or "screenshotUrl" to take a screenshot from`)
-        continue
+    if (file._path === '/templates') {
+      for (const template of file.templates) {
+        const url = template.screenshotUrl || template.demo
+        if (!url) {
+          console.error(`Template ${template.slug} has no "demo" or "screenshotUrl" to take a screenshot from`)
+          continue
+        }
+        const filename = join(process.cwd(), 'public/assets/templates', `${template.slug}.png`)
+        if (existsSync(filename)) {
+          continue
+        }
+        console.log(`Generating screenshot for Template ${template.slug} hitting ${url}...`)
+        await captureWebsite.file(url, filename, {
+          ...(template.screenshotOptions || {}),
+          launchOptions: { headless: 'new' }
+        })
       }
-      const filename = join(process.cwd(), 'public/assets/templates', `${template.slug}.png`)
-      if (existsSync(filename)) {
-        continue
+    }
+    if (file._path === '/video-courses') {
+      for (const course of file.courses) {
+        const url = course.screenshotUrl || course.url
+        if (!url) {
+          console.error(`Video Course ${course.slug} has no "url" or "screenshotUrl" to take a screenshot from`)
+          continue
+        }
+        const filename = join(process.cwd(), 'public/assets/video-courses', `${course.slug}.png`)
+        if (existsSync(filename)) {
+          continue
+        }
+        console.log(`Generating screenshot for Video Course ${course.slug} hitting ${url}...`)
+        await captureWebsite.file(url, filename, {
+          ...(course.screenshotOptions || {}),
+          launchOptions: { headless: 'new' },
+          width: 1920,
+          height: 960
+        })
       }
-      console.log(`Generating screenshot for ${template.slug} hitting ${url}...`)
-      await captureWebsite.file(url, filename, {
-        ...(template.screenshotOptions || {}),
-        launchOptions: { headless: 'new' }
-      })
     }
   })
 })
---
+++ types/video-courses.ts
@@ -0,0 +1,7 @@
+export interface VideoCourse {
+  name: string
+  slug: string
+  description: string
+  url: string
+  badge?: string
+}
