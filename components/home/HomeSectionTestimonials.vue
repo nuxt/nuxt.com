9513985b66a94ab10975ee1d21bca411e3d182d8
Sébastien Chopin <seb@nuxt.com>
2023-12-15 10:07:49 +0100

    chore: use Nuxt UI Pro ULandingTestimonial

--- components/home/HomeSectionTestimonials.vue
+++ components/home/HomeSectionTestimonials.vue
@@ -4,10 +4,13 @@ defineProps<{
     quote: string
     author: {
       name: string
-      job: string
-      imgSrc: string
-      imgSrcSet: string
-      url: string
+      description: string
+      to: string
+      target?: string
+      avatar: {
+        src: string
+        srcset?: string
+      }
     }
   }[]
 }>()
@@ -15,45 +18,8 @@ defineProps<{
 
 <template>
   <UPageColumns>
-    <div
-      v-for="(testimonial, index) in testimonials"
-      :key="index"
-      class="break-inside-avoid py-0.5"
-    >
-      <UPageCard
-        :ui="{
-          background: 'dark:bg-gradient-to-b from-gray-700/50 to-gray-900/50',
-          body: {
-            base: 'flex-1',
-            background: 'dark:bg-gray-900/50',
-          }
-        }"
-        :to="testimonial.author.url"
-        target="_blank"
-      >
-        <q class="italic text-lg text-gray-500 dark:text-gray-400">
-          {{ testimonial.quote }}
-        </q>
-
-        <div class="flex gap-x-4 items-center mt-6">
-          <UAvatar
-            :src="testimonial.author.imgSrc"
-            :srcset="testimonial.author.imgSrcSet"
-            loading="lazy"
-            :alt="testimonial.author.name"
-            size="md"
-            :ui="{ rounded: 'rounded-lg' }"
-          />
-          <div class="min-w-0">
-            <p class="font-semibold text-primary">
-              {{ testimonial.author.name }}
-            </p>
-            <p class="text-sm truncate">
-              {{ testimonial.author.job }}
-            </p>
-          </div>
-        </div>
-      </UPageCard>
+    <div v-for="(testimonial, index) in testimonials" :key="index" class="break-inside-avoid">
+      <ULandingTestimonial v-bind="testimonial" />
     </div>
   </UPageColumns>
 </template>
