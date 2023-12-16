80a988dd86286bfb6077d9fcb7b51b060f6eea96
Daniel Roe <daniel@roe.dev>
2023-12-16 20:51:59 +0000

    fix: add back `Starter` type (used by `CardStarter`)

---
+++ types/starters.ts
@@ -0,0 +1,11 @@
+export interface Starter {
+  name: string
+  slug: string
+  description: string
+  image?: string
+  repo: string
+  branch: string
+  dir?: string
+  demo?: string
+  docs?: string
+}
