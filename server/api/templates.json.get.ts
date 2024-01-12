110cd3121f53f0a07fbec29f0bb0eb041d446809
Arash <38922203+arashsheyda@users.noreply.github.com>
2024-01-12 10:21:27 -0700

    chore: remove extra space

--- server/api/templates.json.get.ts
+++ server/api/templates.json.get.ts
@@ -1,6 +1,5 @@
 import { serverQueryContent } from '#content/server'
 
-
 export default defineCachedEventHandler(async (event) => {
   const list = await serverQueryContent(event, '/templates').only('templates').findOne()
   return list.templates
