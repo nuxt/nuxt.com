2b1539aaa2203b7ae1fee3dac40d122e212cb663
Anthony Fu <anthonyfu117@hotmail.com>
2024-01-25 22:54:32 +0100

    chore: update

--- layers/twoslash/plugins/twoslash.ts
+++ layers/twoslash/plugins/twoslash.ts
@@ -1,6 +1,7 @@
 import { defineNuxtPlugin } from '#imports'
 import TwoslashFloatingVue from 'vitepress-plugin-twoslash/client'
-import 'vitepress-plugin-twoslash/style.css'
+import '../vendor.css'
+import '../twoslash.css'
 
 export default defineNuxtPlugin(({ vueApp }) => {
   vueApp.use(TwoslashFloatingVue as any)
