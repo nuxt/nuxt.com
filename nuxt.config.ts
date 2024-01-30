858e60f9016a849d154d946d4920bd47d06dadac
Daniel Roe <daniel@roe.dev>
2024-01-30 15:41:40 +0000

    chore: remove ignore

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -100,7 +100,7 @@ export default defineNuxtConfig({
       // failOnError: false
       // TODO: investigate
       // Ignore weird url from crawler on some modules readme
-      ignore: ['/modules/%3C/span', '/modules/%253C/span', '/docs/getting-started/</span', '/docs/getting-started/%3C/span', '/blog/v3-10']
+      ignore: ['/modules/%3C/span', '/modules/%253C/span', '/docs/getting-started/</span', '/docs/getting-started/%3C/span']
     },
     hooks: {
       'prerender:generate' (route) {
