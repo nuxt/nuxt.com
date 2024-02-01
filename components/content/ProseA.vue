a349f9f5dc3deda152e2f0de6c3a631d2f9605f0
barbapapazes <e.soubiran25@gmail.com>
2024-02-01 09:48:03 +0100

    fix: add rel to props from prose a

---
+++ components/content/ProseA.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+defineProps<{
+  href: string
+  target?: string
+  rel?: string
+}>()
+</script>
+
+<template>
+  <NuxtLink
+    :href="href"
+    :target="target"
+  >
+    <slot />
+  </NuxtLink>
+</template>
