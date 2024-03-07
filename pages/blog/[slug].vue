d4308888ccabbdf911a2423ad613c311639c8e88
Sébastien Chopin <seb@nuxt.com>
2024-03-07 01:13:17 +0100

    feat: improve blog

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -47,17 +47,42 @@ if (article.value.image) {
   })
 }
 
+const authorTwitter = article.value.authors?.[0]?.twitter
 const socialLinks = computed(() => [{
   icon: 'i-simple-icons-linkedin',
   to: `https://www.linkedin.com/sharing/share-offsite/?url=https://nuxt.com${article.value._path}`
 }, {
   icon: 'i-simple-icons-twitter',
-  to: `https://twitter.com/intent/tweet?text=I%20found%20this%20article%20interesting%20%20https://nuxt.com${article.value._path}&hashtags=nuxt`
+  to: `https://twitter.com/intent/tweet?text=${encodeURIComponent(`${article.value.title}${authorTwitter ? ` by @${article.value.authors[0].twitter}` : ''}\n\n`)}https://nuxt.com${article.value._path}`
 }])
 
 function copyLink () {
   copy(`https://nuxt.com${article.value._path}`, { title: 'Copied to clipboard' })
 }
+const links = [
+  {
+    icon: 'i-ph-pen-duotone',
+    label: 'Edit this article',
+    to: `https://github.com/nuxt/nuxt.com/edit/main/content/7.blog/${article.value._file}`,
+    target: '_blank'
+  }, {
+    icon: 'i-ph-shooting-star-duotone',
+    label: 'Star on GitHub',
+    to: 'https://github.com/nuxt/nuxt',
+    target: '_blank'
+  }, {
+    icon: 'i-ph-chat-centered-text-duotone',
+    label: 'Chat on Discord',
+    to: 'https://discord.com/invite/nuxt',
+    target: '_blank'
+  }, {
+    icon: 'i-ph-hand-heart-duotone',
+    label: 'Become a Sponsor',
+    to: 'https://github.com/sponsors/nuxt',
+    target: '_blank'
+  }
+]
+
 </script>
 
 <template>
@@ -102,9 +127,23 @@ function copyLink () {
         <UPageBody prose class="dark:text-gray-300">
           <ContentRenderer v-if="article && article.body" :value="article" />
 
-          <div class="flex justify-end items-center gap-1.5 mt-12 not-prose">
-            <UButton icon="i-ph-link-simple" v-bind="($ui.button.secondary as any)" @click="copyLink" />
-            <UButton v-for="(link, index) in socialLinks" :key="index" v-bind="{ ...($ui.button.secondary as any), ...link }" target="_blank" />
+          <div class="flex items-center justify-between mt-12 not-prose">
+            <NuxtLink href="/blog" class="text-primary">
+              ← Back to blog
+            </NuxtLink>
+            <div class="flex justify-end items-center gap-1.5">
+              <UButton icon="i-ph-link-simple" v-bind="($ui.button.secondary as any)" @click="copyLink">
+                Copy URL
+              </UButton>
+              <UButton
+                v-for="(link, index) in socialLinks"
+                :key="index"
+                v-bind="link"
+                variant="ghost"
+                color="gray"
+                target="_blank"
+              />
+            </div>
           </div>
 
           <hr v-if="surround?.length">
@@ -113,7 +152,17 @@ function copyLink () {
         </UPageBody>
 
         <template #right>
-          <UContentToc v-if="article.body && article.body.toc" :links="article.body.toc.links" />
+          <UContentToc v-if="article.body && article.body.toc" :links="article.body.toc.links">
+            <template #bottom>
+              <div class="hidden lg:block space-y-6">
+                <UPageLinks title="Links" :links="links" />
+
+                <UDivider type="dashed" />
+
+                <Ads />
+              </div>
+            </template>
+          </UContentToc>
         </template>
       </UPage>
     </UPage>
