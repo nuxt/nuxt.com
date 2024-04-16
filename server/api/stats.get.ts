d29dd1cc71284a33f2febc40eaf5f0db06826ce1
Sébastien Chopin <seb@nuxt.com>
2024-04-16 12:25:31 +0200

    feat: add version to /stats

--- server/api/stats.get.ts
+++ server/api/stats.get.ts
@@ -1,9 +1,11 @@
 export default cachedEventHandler(async (event) => {
   const repo = await github.fetchRepo('nuxt', 'nuxt')
-  const { downloads} = await npm.fetchPackageStats('nuxt')
+  const { version } = await $fetch<any>('https://registry.npmjs.org/nuxt/latest').catch(() => {})
+  const { downloads } = await npm.fetchPackageStats('nuxt')
 
   return {
     ...repo,
+    version,
     monthlyDownloads: downloads
   }
 }, {
