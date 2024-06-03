5c05787d8e8db4c4f12c2d2c7ce2f9d3cf0dce9a
Sébastien Chopin <seb@nuxt.com>
2024-06-03 22:00:14 +0200

    chore: add NuxtHub banner

--- app.vue
+++ app.vue
@@ -52,17 +52,17 @@ provide('navigation', navigation)
     <NuxtLoadingIndicator />
 
     <AppBanner
-      id="frontend-nation-2024"
-      to="https://frontendnation.com/?utm_source=nuxt-website"
+      id="nuxthub-beta"
+      to="https://hub.nuxt.com/blog/beta?utm_source=nuxt-website&utm_medium=banner&utm_campaign=nuxthub-beta"
     >
       <div class="flex items-center gap-2">
         <UIcon
-          name="i-ph-microphone-stage-duotone"
-          class="w-5 h-5 flex-shrink-0 pointer-events-none"
+          name="i-simple-icons-nuxtdotjs"
+          class="w-5 h-5 flex-shrink-0 pointer-events-none hidden lg:inline-block"
         />
-        <span>Join the Nuxt Team at Frontend Nation 4-7 June.</span>
+        <span>NuxtHub is now in public beta<span class="hidden sm:inline">: build & deploy full-stack Nuxt applications</span>.</span>
         <UButton
-          label="Sign up for free"
+          label="Learn more"
           color="white"
           trailing-icon="i-ph-arrow-right"
           size="2xs"
