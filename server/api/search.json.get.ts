9b56108a12424fbd51532ffcabf506d8cd496cb1
Arash <38922203+arashsheyda@users.noreply.github.com>
2024-06-21 16:11:42 -0600

    chore: lint

--- server/api/search.json.get.ts
+++ server/api/search.json.get.ts
@@ -1,21 +1,20 @@
 import { serverQueryContent } from '#content/server'
 
 export default eventHandler(async (event) => {
-
   const { select } = getQuery<{ select: string | undefined }>(event)
   const fieldsToSelect = select?.split(',') ?? []
 
   return await serverQueryContent(event, '/docs')
-  .only(fieldsToSelect)
-  .where({
-    _type: 'markdown',
-    _path: {
-      $and: [{
-        $ne: new RegExp('^/docs/bridge')
-      }, {
-        $ne: new RegExp('^/docs/migration')
-      }]
-    },
-    navigation: { $ne: false }
-  }).find()
+    .only(fieldsToSelect)
+    .where({
+      _type: 'markdown',
+      _path: {
+        $and: [{
+          $ne: new RegExp('^/docs/bridge')
+        }, {
+          $ne: new RegExp('^/docs/migration')
+        }]
+      },
+      navigation: { $ne: false }
+    }).find()
 })
