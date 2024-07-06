bd4392a423c0f8eff2be8671f204c953cb6a68c5
Arash <38922203+arashsheyda@users.noreply.github.com>
2024-07-06 10:48:11 -0600

    fix: filter afterwards

--- server/api/search.json.get.ts
+++ server/api/search.json.get.ts
@@ -2,19 +2,31 @@ import { serverQueryContent } from '#content/server'
 
 export default eventHandler(async (event) => {
   const { select } = getQuery<{ select: string | undefined }>(event)
-  const fieldsToSelect = select?.split(',') ?? []
 
-  return await serverQueryContent(event, '/docs')
-    .only(fieldsToSelect)
-    .where({
-      _type: 'markdown',
-      _path: {
-        $and: [{
-          $ne: new RegExp('^/docs/bridge')
-        }, {
-          $ne: new RegExp('^/docs/migration')
-        }]
-      },
-      navigation: { $ne: false }
-    }).find()
+  const docs = await serverQueryContent(event, '/docs').where({
+    _type: 'markdown',
+    _path: {
+      $and: [{
+        $ne: new RegExp('^/docs/bridge')
+      }, {
+        $ne: new RegExp('^/docs/migration')
+      }]
+    },
+    navigation: { $ne: false }
+  }).find()
+
+  if (select.length) {
+    const fieldsToSelect = select.split(',') ?? []
+    return docs.map((doc) => {
+      return fieldsToSelect.reduce((acc, field) => {
+        if (doc[field] !== undefined) {
+          acc[field] = doc[field]
+        }
+        return acc
+      }, {})
+    })
+  }
+  else {
+    return docs
+  }
 })
