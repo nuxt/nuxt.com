6b6b6f4fd94cd5474790bb69ee418c5a795476a2
Florent Delerue <florentdelerue@hotmail.com>
2024-07-22 15:14:52 +0200

    fix(support): line color

--- pages/enterprise/support.vue
+++ pages/enterprise/support.vue
@@ -107,10 +107,10 @@ onBeforeUnmount(() => {
                 viewBox="0 0 2 150"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
-                class="absolute left-4 -top-12 h-full z-[-1]"
+                class="absolute left-4 -top-12 h-full z-[-1] text-gray-300 dark:text-gray-700"
               >
-                <path d="M1 0L1 153" stroke="#334155" stroke-dasharray="4 4" />
-                <path d="M1 142L1 295" stroke="#334155" stroke-dasharray="4 4" />
+                <path d="M1 0L1 153" stroke="currentColor" stroke-dasharray="4 4" />
+                <path d="M1 142L1 295" stroke="currentColor" stroke-dasharray="4 4" />
               </svg>
             </div>
             <ul class="flex flex-col gap-y-4 pt-1">
