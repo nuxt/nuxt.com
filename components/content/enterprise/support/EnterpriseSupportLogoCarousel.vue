d8b1f9771ebcee49c879b375c5420dac2cc78983
Florent Delerue <florentdelerue@hotmail.com>
2024-07-26 19:57:32 +0200

    feat: enterprise support page rework (#1623)
    * feat(support): add carousel logo
    
    * feat(support): update logos carousel
    
    * fix(support): logo carousel
    
    * feat(support): carousel testimonials (wip)
    
    * feat(appbanner): improve seo
    
    * feat(support): refactor logo carousel
    
    * feat(support): expertise logo improvements
    
    * feat(support): end of page, responsive and seo
    
    * chore: revert lockfile changes
    
    * Update 1.support.yml
    
    * Update 1.support.yml
    
    * fix(enterprisesupportlogocarousel): update overlays on tablet/mobile
    
    * fix(enterprisesupportclientcard): spacing and badge color
    
    * fix(support): light mode on proceed section
    
    * feat(support): pause interval if mouse hover carousel
    
    * feat(support): clear carousel interval on before unmount
    
    * fix(support): spacing
    
    * fix(support): line color
    
    * feat(enterprisesupportlogocarousel): stop animation on hover
    
    * Update 1.support.yml
    
    * feat(support): icons and improvements
    
    * up
    
    * up
    
    * fix(support): texts and logos
    
    * up
    
    * feat(support): social card
    
    ---------
    
    Co-authored-by: Daniel Roe <daniel@roe.dev>
    Co-authored-by: Ferdinand Coumau <ferdinand@nuxtlabs.com>

---
+++ components/content/enterprise/support/EnterpriseSupportLogoCarousel.vue
@@ -0,0 +1,60 @@
+<script lang="ts" setup>
+const { logos } = defineProps({
+  logos: {
+    type: Array<{ src: string, width: number, height: number, alt: string, light: string, dark: string }>,
+    require: true
+  }
+})
+
+const carousel = ref<HTMLElement>(null)
+
+const stopAnimation = () => {
+  if (carousel.value) {
+    carousel.value.style.animationPlayState = 'paused'
+  }
+}
+
+const startAnimation = () => {
+  if (carousel.value) {
+    carousel.value.style.animationPlayState = 'running'
+  }
+}
+</script>
+
+<template>
+  <div class="overflow-hidden whitespace-nowrap relative h-12">
+    <div class="h-12 w-[50px] sm:w-[100px] md:w-[200px] lg:w-[400px] bg-gradient-to-r from-white dark:from-slate-950 via-transparent to-transparent absolute left-0 z-10" />
+    <div class="h-12 w-[50px]sm:w-[100px] md:w-[200px] lg:w-[400px] bg-gradient-to-r to-white dark:to-slate-950 via-transparent from-transparent absolute right-0 z-10" />
+    <div
+      ref="carousel"
+      class="flex carousel"
+      @mouseover="stopAnimation"
+      @mouseleave="startAnimation"
+    >
+      <div v-for="({ light, dark, width, height, alt }, index) in logos.concat(logos)" :key="index" class="carousel-item items-center">
+        <UColorModeImage :light="light" :dark="dark" :width="width" :height="height" :alt="alt" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="postcss">
+.carousel {
+  animation: scroll 30s linear infinite;
+  animation-play-state: running;
+}
+
+.carousel-item {
+  flex: 0 0 auto;
+  margin-right: 68px;
+}
+
+@keyframes scroll {
+  0% {
+    transform: translateX(0);
+  }
+  100% {
+    transform: translateX(calc(-166%));
+  }
+}
+</style>
