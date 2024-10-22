379f7daae200a625c27f7bf214f7803fb64a1a35
Sébastien Chopin <seb@nuxt.com>
2024-10-22 12:15:33 +0200

    chore: add nuxtnation banner

--- app/app.vue
+++ app/app.vue
@@ -66,17 +66,17 @@ onMounted(() => {
     <NuxtLoadingIndicator />
 
     <AppBanner
-      id="nuxt-ui-pro"
-      to="https://ui.nuxt.com/pro?utm_source=nuxt&utm_medium=website&utm_campaign=banner"
+      id="nuxt-nation-2024"
+      to="https://nuxtnation.com/?utm_source=nuxt&utm_medium=website&utm_campaign=banner"
     >
       <div class="flex items-center gap-1">
         <UIcon
-          name="i-ph-layout-duotone"
+          name="i-ph-microphone-stage-duotone"
           class="w-5 h-5 flex-shrink-0 pointer-events-none hidden sm:inline-block"
         />
-        <span>Create beautiful & responsive Nuxt apps with Nuxt UI Pro</span>
+        <span><span class="font-semibold">Nuxt Nation</span> conference is coming. Join us on November 12-13.</span>
         <UButton
-          label="Learn more"
+          label="Sign up"
           color="white"
           trailing-icon="i-ph-arrow-right"
           size="2xs"
