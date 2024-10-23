93cdcda7e8136f94b090e8e234d83307c29ceb09
Sébastien Chopin <seb@nuxt.com>
2024-10-23 15:10:33 +0200

    docs: add mastering nuxt course

--- app/pages/docs/[...slug].vue
+++ app/pages/docs/[...slug].vue
@@ -60,6 +60,11 @@ const communityLinks = computed(() => [{
   label: 'Become a Sponsor',
   to: 'https://go.nuxt.com/sponsor',
   target: '_blank'
+}, {
+  icon: 'i-ph-chef-hat',
+  label: 'Master Nuxt',
+  to: 'https://masteringnuxt.com/nuxt3',
+  target: '_blank'
 }])
 
 const title = page.value.head?.title || page.value.title
