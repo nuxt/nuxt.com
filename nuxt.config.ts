1df3a2c0a0921a854a38b4f085a20d418f4b1cb3
Daniel Roe <daniel@roe.dev>
2025-02-04 21:16:40 +0000

    chore: ignore additional agency query

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -176,7 +176,7 @@ export default defineNuxtConfig({
       // failOnError: false
       // TODO: investigate
       // Ignore weird url from crawler on some modules readme
-      ignore: ['/modules/%3C/span', '/modules/%253C/span', '/docs/getting-started/</span', '/docs/getting-started/%3C/span', '/modules/Mojo CSS', '/modules/Mojo%20CSS', '/enterprise/agencies?service=content-marketing']
+      ignore: ['/modules/%3C/span', '/modules/%253C/span', '/docs/getting-started/</span', '/docs/getting-started/%3C/span', '/modules/Mojo CSS', '/modules/Mojo%20CSS', '/enterprise/agencies?service=content-marketing', '/enterprise/agencies?service=mobile-development']
     },
     hooks: {
       'prerender:generate'(route) {
