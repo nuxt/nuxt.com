3e92e437b6a16b1a343b350a461fb8ebece46988
Sébastien Chopin <atinux@gmail.com>
2025-03-12 18:15:51 +0100

    chore: update ui pro

--- app/components/ads/AdsUIPro.vue
+++ app/components/ads/AdsUIPro.vue
@@ -8,7 +8,7 @@
       body: { padding: 'p-2 sm:p-2', base: 'text-center' } }"
   >
     <template #header>
-      <UColorModeImage dark="https://ui.nuxt.com/_ipx/s_1152x720/templates/dashboard3.png" light="https://ui.nuxt.com/_ipx/s_1152x720/templates/dashboard2.png" alt="Nuxt UI Pro" />
+      <img src="https://ui.nuxt.com/pro/ad.png" alt="Nuxt UI Pro">
       <!-- <NuxtImg src="/assets/ads/nuxt-ui-pro-black-friday.png" width="168" height="105" alt="Nuxt UI Pro Black Friday" class="rounded" /> -->
     </template>
     <div class="inline-flex gap-1 my-2">
