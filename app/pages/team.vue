87109dd5258a0cda3f8467f04a9d6ab70fba7b9d
Daniel Roe <daniel@roe.dev>
2025-03-12 13:03:00 +0100

    feat: add ecosystem team

--- app/pages/team.vue
+++ app/pages/team.vue
@@ -39,7 +39,16 @@ const icons = {
   github: 'i-simple-icons-github'
 }
 
-const { data: team } = await useFetch<TeamMember[]>('https://api.nuxt.com/teams/core')
+const { data: coreTeam } = await useFetch<TeamMember[]>('https://api.nuxt.com/teams/core')
+const { data: ecosystemTeam } = await useFetch<TeamMember[]>('https://api.nuxt.com/teams/ecosystem', {
+  transform(team) {
+    return team.filter(t => !coreTeam.value?.some(c => c.login === t.login))
+  }
+})
+const teams = {
+  'Core team': coreTeam.value,
+  'Ecosystem team': ecosystemTeam.value
+}
 </script>
 
 <template>
@@ -53,64 +62,70 @@ const { data: team } = await useFetch<TeamMember[]>('https://api.nuxt.com/teams/
 
     <UPage>
       <UPageBody>
-        <UPageGrid class="xl:grid-cols-4">
-          <UPageCard
-            v-for="(user, index) in team"
-            :key="index"
-            :title="user.name"
-            :description="user.location"
-            :ui="{
-              title: 'justify-center',
-              description: 'text-center'
-            }"
-          >
-            <template #icon>
-              <UAvatar :src="`https://ipx.nuxt.com/f_auto,s_80x80/gh_avatar/${user.login}`" :srcset="`https://ipx.nuxt.com/f_auto,s_160x160/gh_avatar/${user.login} 2x`" size="3xl" class="mx-auto" />
-            </template>
+        <template v-for="(team, name, index) in teams" :key="`${name}-heading`">
+          <h2 class="font-bold text-xl px-2 mb-4 md:mb-12" :class="{ 'mt-12 md:mt-24': !!index }">
+            {{ name }}
+          </h2>
+          <UPageGrid :key="`${name}-grid`" class="xl:grid-cols-4">
+            <UPageCard
+              v-for="(user, teamIndex) in team"
+              :key="teamIndex"
+              :title="user.name"
+              :description="[user.pronouns, user.location].filter(Boolean).join(' ・ ')"
+              :ui="{
+                title: 'justify-center',
+                description: 'text-center'
+              }"
+            >
+              <template #icon>
+                <UAvatar :src="`https://ipx.nuxt.com/f_auto,s_80x80/gh_avatar/${user.login}`" :srcset="`https://ipx.nuxt.com/f_auto,s_160x160/gh_avatar/${user.login} 2x`" size="3xl" class="mx-auto" />
+              </template>
 
-            <div class="flex items-center justify-center gap-1.5 mt-4">
-              <UButton
-                v-for="(link, key) in user.socialAccounts"
-                :key="key"
-                color="gray"
-                variant="link"
-                :to="link.url"
-                :icon="icons[key] || icons.website"
-                :alt="`Link to ${user.name}'s ${key} profile`"
-                target="_blank"
-              />
-              <UButton
-                :to="`https://github.com/${user.login}`"
-                color="gray"
-                variant="link"
-                :alt="`Link to ${user.name}'s GitHub profile`"
-                :icon="icons.github"
-                target="_blank"
-              />
-              <UButton
-                v-if="user.websiteUrl"
-                :to="user.websiteUrl"
-                color="gray"
-                variant="link"
-                :alt="`Link to ${user.name}'s personal website`"
-                :icon="icons.website"
-                target="_blank"
-              />
-            </div>
-            <div v-if="user.sponsorsListing" class="flex items-center justify-center mt-4">
-              <UButton
-                :to="user.sponsorsListing"
-                target="_blank"
-                color="gray"
-                icon="i-ph-heart"
-                icon-color="red"
-                :ui="{ icon: { base: 'text-pink-500' } }"
-              >
-                Sponsor
-              </UButton>
-            </div>
-          </UPageCard>
-        </UPageGrid>
+              <div class="flex items-center justify-center gap-1.5 mt-4">
+                <UButton
+                  v-for="(link, key) in user.socialAccounts"
+                  :key="key"
+                  color="gray"
+                  variant="link"
+                  :to="link.url"
+                  :icon="icons[key] || icons.website"
+                  :alt="`Link to ${user.name}'s ${key} profile`"
+                  target="_blank"
+                />
+                <UButton
+                  :to="`https://github.com/${user.login}`"
+                  color="gray"
+                  variant="link"
+                  :alt="`Link to ${user.name}'s GitHub profile`"
+                  :icon="icons.github"
+                  target="_blank"
+                />
+                <UButton
+                  v-if="user.websiteUrl"
+                  :to="user.websiteUrl"
+                  external
+                  color="gray"
+                  variant="link"
+                  :alt="`Link to ${user.name}'s personal website`"
+                  :icon="icons.website"
+                  target="_blank"
+                />
+              </div>
+              <div v-if="user.sponsorsListing" class="flex items-center justify-center mt-4">
+                <UButton
+                  :to="user.sponsorsListing"
+                  target="_blank"
+                  color="gray"
+                  icon="i-ph-heart"
+                  icon-color="red"
+                  :ui="{ icon: { base: 'text-pink-500' } }"
+                >
+                  Sponsor
+                </UButton>
+              </div>
+            </UPageCard>
+          </UPageGrid>
+        </template>
       </UPageBody>
     </UPage>
   </UContainer>
