e6b11ca403fec44aab43d3cffcaf0983f5058bea
Benjamin Canac <canacb1@gmail.com>
2025-03-23 12:59:55 +0100

    up

--- app/app.vue
+++ app/app.vue
@@ -1,20 +1,4 @@
 <script setup lang="ts">
-  }
-}
-// import { debounce } from 'perfect-debounce'
-
-// interface SearchValue {
-//   commandPaletteRef?: {
-//     query: string
-//     results: {
-//       id: string
-//       label: string
-//       to: string
-//     }[]
-//   }
-// }
-
-// const search = ref<SearchValue | null>(null)
 const colorMode = useColorMode()
 const { searchGroups, searchLinks, searchTerm } = useNavigation()
 
@@ -59,13 +43,6 @@ useSeoMeta({
   twitterCard: 'summary_large_image',
   twitterSite: 'nuxt_js'
 })
-
-// watch(() => search.value?.commandPaletteRef?.query, debounce((query) => {
-//   if (typeof query !== 'string') {
-//     return
-//   }
-//   useTrackEvent('Search', { props: { query: `${query} - ${search.value?.commandPaletteRef?.results.length || 0} results` } })
-// }, 500))
 
 // Provide with non-null assertion since this is top level app setup
 provide('navigation', navigation!)
