6b0de1078988a01b3ba3ca128e8221130e2ee3d3
Benjamin Canac <canacb1@gmail.com>
2025-04-09 11:32:00 +0200

    fix(app): types (#1852)

--- app/types/filters.ts
+++ app/types/filters.ts
@@ -1,6 +1,6 @@
 import type { LinkProps } from '@nuxt/ui'
 
-export interface Filter extends LinkProps {
+export interface Filter extends Omit<LinkProps, 'type'> {
   key: string | number
   title?: string
   label?: string
