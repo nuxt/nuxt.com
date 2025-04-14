4d495bb6494e63e5d7fb00d97f7a17d3c139dd6e
Daniel Roe <daniel@roe.dev>
2025-04-14 22:55:08 +0100

    fix: use better practices for useBlog

--- app/composables/useBlog.ts
+++ app/composables/useBlog.ts
@@ -1,30 +1,18 @@
 import type { BlogArticle } from '~/types'
 
 export const useBlog = () => {
-  const articles = useState<BlogArticle[]>('articles', () => [])
-  // const featuredArticle: Ref<BlogArticle | {}> = useState('featured-article', () => ({}))
-
-  // Data fetching
+  const { data: articles, refresh } = useAsyncData<BlogArticle[]>('blog', async () => {
+    return queryCollection('blog')
+      .where('extension', '=', 'md')
+      /* .select('title', 'date', 'image', 'description', 'path', 'authors', 'category') */
+      .order('date', 'DESC')
+      .all()
+      .then(res => res.filter(article => article.path !== '/blog'))
+  }, { default: () => [] })
 
   async function fetchList() {
-    if (articles.value.length) {
-      return
-    }
-
-    try {
-      const { data: posts } = await useAsyncData('posts', async () => {
-        return queryCollection('blog')
-          .where('extension', '=', 'md')
-          /* .select('title', 'date', 'image', 'description', 'path', 'authors', 'category') */
-          .order('date', 'DESC')
-          .all()
-      })
-
-      articles.value = posts.value?.filter(article => article.path !== '/blog') || []
-      // featuredArticle.value = articles.value?.shift() || {}
-    } catch (e) {
-      articles.value = []
-      return e
+    if (!articles.value?.length) {
+      return refresh()
     }
   }
 
