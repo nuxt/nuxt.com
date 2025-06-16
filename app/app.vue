0f75adb88700ca572fcafe11b5d731f45e5c2b30
HugoRCD <hugo.richard@epitech.eu>
2025-06-16 16:34:17 +0200

    up

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-const route = useRoute()
 const colorMode = useColorMode()
 const { version } = useDocsVersion()
 const { searchGroups, searchLinks, searchTerm } = useNavigation()
