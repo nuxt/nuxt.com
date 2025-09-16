a5712103fae1687b288ba8b01c4eacfa33969e94
Hugo Richard <hugo.richard@epitech.eu>
2025-09-16 10:50:32 +0100

    up

---
+++ app/components/content/Carousel.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineProps<{
+  items: string[]
+}>()
+</script>
+
+<template>
+  <UCarousel v-slot="{ item, index }" :items="items" :ui="{ item: 'basis-1/3' }" dots>
+    <ProseImg :src="item" :alt="`Image ${index + 1}`" width="320" class="rounded-lg" />
+  </UCarousel>
+</template>
