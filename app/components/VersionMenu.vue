f965225f4de2861cd7651202d07e26cf24ce8ecc
Hugo Richard <hugo.richard@epitech.eu>
2025-09-19 14:12:28 +0100

    up

--- app/components/VersionMenu.vue
+++ app/components/VersionMenu.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 const { version, items } = useDocsVersion()
 const { tags } = useDocsTags()
+const route = useRoute()
 </script>
 
 <template>
@@ -11,11 +12,12 @@ const { tags } = useDocsTags()
     :content="{ align: 'start' }"
     :ui="{ content: 'min-w-fit' }"
     size="xs"
+    :disabled="!route.path.startsWith('/docs')"
   >
     <UBadge
       :label="`v${tags[version.shortTag]}`"
       variant="subtle"
-      trailing-icon="i-lucide-chevron-down"
+      :trailing-icon="route.path.startsWith('/docs') ? 'i-lucide-chevron-down' : undefined"
       size="sm"
       :color="version.tagColor"
       class="-mb-[2px] font-semibold text-[12px]/3 truncate"
