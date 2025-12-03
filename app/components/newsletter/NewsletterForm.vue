21bb11334fd985b455893fbe93a46c957c5570ed
Daniel Roe <daniel@roe.dev>
2025-12-03 15:08:10 +0000

    fix: do not try to parse error message from newsletter subscription

--- app/components/newsletter/NewsletterForm.vue
+++ app/components/newsletter/NewsletterForm.vue
@@ -36,8 +36,7 @@ async function onSubmit(event: FormSubmitEvent<Schema>) {
     toast.add({ title: 'Subscription pending', description: 'Please check your emails to confirm your subscription.', color: 'success' })
     state.email = ''
   }).catch((err) => {
-    const error = JSON.parse(err.data?.message)
-    const description = error[0].message || 'Something went wrong. Please try again later.'
+    const description = err.data?.message || 'Something went wrong. Please try again later.'
     toast.add({ title: 'Subscription failed', description, color: 'error' })
   })
   loading.value = false
