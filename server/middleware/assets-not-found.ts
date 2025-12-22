289306a4219c440826672ba4aa145360eb71b9b0
Sébastien Chopin <atinux@gmail.com>
2025-12-22 14:13:31 +0100

    ignore og_image

--- server/middleware/assets-not-found.ts
+++ server/middleware/assets-not-found.ts
@@ -3,6 +3,9 @@ import { defineEventHandler, setResponseStatus } from 'h3'
 const assetExtensions = new Set(['jpg', 'jpeg', 'png', 'gif', 'svg', 'ico', 'webp', 'avif', 'woff', 'woff2', 'ttf', 'eot', 'otf'])
 
 export default defineEventHandler((event) => {
+  if (event.path.includes('__og-image__')) {
+    return
+  }
   // if path has an asset extension, return 404
   const dotIndex = event.path.lastIndexOf('.')
   if (dotIndex !== -1 && assetExtensions.has(event.path.slice(dotIndex + 1))) {
