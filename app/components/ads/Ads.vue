97aa8cf2b5739a0a8fc7ca1df2cc8b414e7a6547
Sébastien Chopin <atinux@gmail.com>
2026-03-02 16:49:41 +0100

    chore: remove freeweekend offer

--- app/components/ads/Ads.vue
+++ app/components/ads/Ads.vue
@@ -1,16 +1,14 @@
 <script setup lang="ts">
-// const { $ads } = useNuxtApp()
-// const route = useRoute()
-// const nuxtApp = useNuxtApp()
-// const route = useRoute()
+const { $ads } = useNuxtApp()
+const route = useRoute()
 </script>
 
 <template>
   <div class="space-y-3">
     <!-- <AdsMN v-if="route.path.length % 2 === 0" /> -->
     <!-- <AdsNuxtCertificate v-else /> -->
-    <AdsFreeWeekend />
-    <!-- <AdsFallback v-if="$ads.adBlocked.value" />
-    <LazyAdsCarbon v-else :key="route.path" /> -->
+    <!-- <AdsFreeWeekend /> -->
+    <AdsFallback v-if="$ads.adBlocked.value" />
+    <LazyAdsCarbon v-else :key="route.path" />
   </div>
 </template>
