b54ec73f426d86de4aad52072dc10d0e8f6ce609
Daniel Roe <daniel@roe.dev>
2026-03-02 14:35:05 +0000

    chore: prerender 5.x docs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -114,6 +114,7 @@ export default defineNuxtConfig({
     '/404.html': { prerender: true },
     '/docs/3.x/getting-started/introduction': { prerender: true },
     '/docs/4.x/getting-started/introduction': { prerender: true },
+    '/docs/5.x/getting-started/introduction': { prerender: true },
     '/modules': { prerender: true },
     '/modules/**': { isr: 60 * 60 },
     // API
