75b5b579b2d92cc86e3feb1df5e27c7fc54cf141
Hugo Richard <hugo.richard@vercel.com>
2026-06-08 09:34:50 +0100

    feat: conditionally enable ipx.nuxt to get working images on preview

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -51,11 +51,7 @@ export default defineNuxtConfig({
         driver: 'vercel-runtime-cache'
       }
     },
-    image: {
-      ipx: {
-        baseURL: 'https://ipx.nuxt.com'
-      }
-    }
+    ...(process.env.VERCEL_ENV !== 'preview' && { image: { ipx: { baseURL: 'https://ipx.nuxt.com' } } })
   },
   devtools: {
     enabled: true
